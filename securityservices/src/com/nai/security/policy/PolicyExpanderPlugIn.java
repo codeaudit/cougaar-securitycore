f3b1c04fea320c5e233e682a776d2fcb449cfc1c
srosset <srosset>
2001-09-21 22:29:18 +0000

    *** empty log message ***

--- securityservices/src/com/nai/security/policy/PolicyExpanderPlugIn.java
+++ securityservices/src/com/nai/security/policy/PolicyExpanderPlugIn.java
@@ -1,3 +1,29 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2001 Networks Associates Inc
+ *  under sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).  
+ *  
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS 
+ *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR 
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF 
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT 
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT 
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL 
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS, 
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR 
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.  
+ * 
+ * </copyright>
+ *
+ * CHANGE RECORD
+ * - 
+ */
+
 package com.nai.security.policy;
 
 import org.cougaar.core.plugin.SimplePlugIn;
@@ -246,7 +272,7 @@ public class PolicyExpanderPlugIn extends SimplePlugIn
 	Document xmlContent = (Document) attributes.remove(XML_KEY);
 				// create a copy of the original message
 
-	XMLPolicyCreator policyCreator = new XMLPolicyCreator(xmlContent);
+	XMLPolicyCreator policyCreator = new XMLPolicyCreator(xmlContent, getClusterIdentifier().toAddress());
 	Policy[] policies = policyCreator.getPolicies();
 	if (policies == null) {
 	  return expandedPolicies;
