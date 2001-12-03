660da17fae07b7aea102506d0d4dd4add6a3387f
srosset <srosset>
2001-12-03 21:05:17 +0000

    *** empty log message ***

--- securityservices/src/com/nai/security/tools/PolicyGenerator.java
+++ securityservices/src/com/nai/security/tools/PolicyGenerator.java
@@ -30,21 +30,42 @@ import java.io.*;
 import java.util.*;
 import java.io.*;
 
+
+import org.w3c.dom.*;
+import org.apache.xml.serialize.*;
+import org.apache.xerces.parsers.*;
+import org.xml.sax.InputSource;
+
 public class PolicyGenerator
 {
-  private String filename = null;
-  private String outputfileprefix = null;
-
+  private String communityConfFile = null;
   private Hashtable communities = null;
+
+  private String policiesFile = null;
+  private Hashtable policies = null;
+
+  private String outputfileprefix = null;
+  private String templateFile = null;
+  private boolean debug = true;
+
+
+  private final String incommingSecureMethod = "IncomingSecureMethod";
+  private final String outgoingSecureMethod = "OutgoingSecureMethod";
 
   public PolicyGenerator()
   {
     communities = new Hashtable();
+    policies = new Hashtable();
   }
 
-  public void setInputFileName(String aFilename)
+  public void setCommunityConfigurationFileName(String aFilename)
   {
-    filename = aFilename;
+    communityConfFile = aFilename;
+  }
+
+  public void setPolicyFileName(String aFilename)
+  {
+    policiesFile = aFilename;
   }
 
   public void setOutputFilePrefix(String aPrefix)
@@ -52,7 +73,22 @@ public class PolicyGenerator
     outputfileprefix = aPrefix;
   }
 
-  public void parseFile()
+  public void setPolicyTemplate(String aPolicyTemplate)
+  {
+    templateFile = aPolicyTemplate;
+  }
+
+  public void parseCommunityFile()
+  {
+    parseFile(communityConfFile, communities);
+  }
+
+  public void parsePolicyFile()
+  {
+    parseFile(policiesFile, policies);
+  }
+
+  private void parseFile(String filename, Hashtable hashtable)
   {
     FileInputStream in = null;
     try {
@@ -80,21 +116,19 @@ public class PolicyGenerator
 
     int index = 0;
     ArrayList list = null;
-    String community = null;
+    String key = null;
 
     try {
       while(st.nextToken() != StreamTokenizer.TT_EOF) {
 	switch (st.ttype) {
 	case StreamTokenizer.TT_EOL:
 	  index = 0;
-	  //System.out.println("End of line");
 	  break;
 	case StreamTokenizer.TT_WORD:
-	  //System.out.println(st.sval + " - " + index);
 	  if (index == 0) {
-	    // Read community (enclave) name
-	    community = st.sval;
-	    list = (ArrayList) communities.get(community);
+	    // Read key name
+	    key = st.sval;
+	    list = (ArrayList) hashtable.get(key);
 	    if (list == null) {
 	      list = new ArrayList();
 	    }
@@ -102,12 +136,14 @@ public class PolicyGenerator
 	  else if (index == 1) {
 	    // Read agent name
 	    list.add(st.sval);
-	    communities.put(community, list);
+	    hashtable.put(key, list);
 	  }
 	  index++;
 	  break;
 	case StreamTokenizer.TT_NUMBER:
-	  System.out.println("Nb=" + st.nval);
+	  if (debug) {
+	    System.out.println("Nb=" + st.nval);
+	  }
 	  break;
 	}
       }
@@ -116,7 +152,10 @@ public class PolicyGenerator
       e.printStackTrace();
       return;
     }
+  }
 
+  public void printConfiguration()
+  {
     Enumeration e = communities.keys();
     while (e.hasMoreElements()) {
       String c = (String) e.nextElement();
@@ -129,11 +168,194 @@ public class PolicyGenerator
     }
   }
 
+  /** Generate one policy file for each community */
+  public void generatePolicyFiles()
+  {
+    Enumeration e = communities.keys();
+    while (e.hasMoreElements()) {
+      // Retrieve the name of the community
+      String aCommunity = (String) e.nextElement();
+      // Retrieve the list of all the agents in that community
+      ArrayList agentList = (ArrayList) communities.get(aCommunity);
+
+      // Generate the XML document
+      if (debug) {
+	System.out.println("Generating policy for community " + aCommunity);
+      }
+
+      Document document = readPolicyTemplate();
+
+      Enumeration pol = policies.keys();
+      while (pol.hasMoreElements()) {
+	String key = (String) pol.nextElement();
+	ArrayList list = (ArrayList) policies.get(key);
+	// The list should contain only one element.
+	String value = (String) list.get(0);
+	if (debug) {
+	  System.out.println("Modifying policy for key=" + key + " - value=" + value);
+	}
+	insertPolicyElements(document, agentList, key, value);
+      }
+
+      savePolicy(document, aCommunity);
+    }
+  }
+
+  public void insertPolicyElements(Document document, ArrayList agentList,
+				   String policyElementName,
+				   String policyValue)
+  {
+    Element rootElement = document.getDocumentElement();
+    String rootElementName = rootElement.getNodeName();
+
+    if (!rootElementName.equals("Policies")) {
+      throw new IllegalArgumentException("Expecting <Policies>, not "+ rootElementName);
+    }
+
+    NodeList rootNodeList = rootElement.getChildNodes();
+    int nRootNodes = rootNodeList.getLength();
+
+    for (int i = 0 ; i < nRootNodes ; i++) {
+      Node subNode = (Node) rootNodeList.item(i);
+      if (subNode.getNodeType() != Node.ELEMENT_NODE) {
+        continue;
+      }
+      String subNoodeName = subNode.getNodeName().trim();
+      if (!subNoodeName.equals("Policy")) {
+	throw new IllegalArgumentException("Expecting <Policy>, not "+ subNoodeName);
+      }
+      NodeList subNodeList = subNode.getChildNodes();
+      int nSubNode = subNodeList.getLength();
+      for (int j = 0 ; j < nSubNode ; j++) {
+	Node rule = (Node) subNodeList.item(j);
+	if (!(rule instanceof Element)) {
+	  continue;
+	}
+	Element policyElement = (Element) rule;
+	String ruleParamName = policyElement.getNodeName().trim();
+	if (ruleParamName.equals("RuleParam")) {
+	  String policyPredicate = policyElement.getAttribute("name");
+	  if (policyPredicate.equals(policyElementName)) {
+	    // This is where we insert specific policies
+	    if (debug) {
+	      System.out.println("Inserting specific policy for " + policyPredicate);
+	    }
+	    NodeList keysetList = rule.getChildNodes();
+	    int nKeysetList = keysetList.getLength();
+	    // Get first key
+	    // <KeySet value="signAndEncrypt"/>
+	    for (int k = 0 ; k < nKeysetList ; k++) {
+	      Node keyset = keysetList.item(k);
+	      if (!(keyset instanceof Element)) {
+		continue;
+	      }
+	      String defaultPolicy = ((Element)keyset).getAttribute("value");
+	      if (debug) {
+		System.out.println("Default Policy:" + defaultPolicy);
+	      }
+	      // Insert specific policies
+	      ListIterator it = agentList.listIterator();
+	      while (it.hasNext()) {
+		String agentName = (String) it.next();
+		System.out.println("\t" + agentName);
+		String keyValue = "KeyValue";
+		String attr1 = "key";
+		String attr2 = "value";
+
+		Element e = document.createElement(keyValue);
+		e.setAttribute(attr1, agentName);
+		e.setAttribute(attr2, policyValue);
+
+		// Insert element in document
+		keyset.insertBefore(e, null);
+	      }
+	    }
+	  }
+	}
+      }
+
+    }
+  }
+
+  private void savePolicy(Document document, String community)
+  {
+    OutputFormat of = new OutputFormat("xml", "UTF-8", true);
+    XMLSerializer serializer = new XMLSerializer(of);
+
+    String outputFileName = outputfileprefix + community + ".xml";
+
+    // Open output policy file
+    FileOutputStream out = null;
+    try {
+      out = new FileOutputStream(outputFileName);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      return;
+    }
+
+    serializer.setOutputByteStream(out);
+
+    try {
+      serializer.serialize(document);
+      out.close();
+    }
+    catch (Exception e) {
+    }
+  }
+
+  private Document readPolicyTemplate()
+  {
+    DOMParser parser = new DOMParser();
+    Document document = null;
+
+    // Open policy template
+    FileInputStream in = null;
+    try {
+      in = new FileInputStream(templateFile);
+    }
+    catch (Exception e) {
+      if (debug) {
+	System.out.println("Unable to open policy template file: " + e);
+      }
+      return null;
+    }
+
+    // Parse policy template
+    try {
+      InputSource inSource = new InputSource(in);
+      parser.parse(inSource);
+      in.close();
+      document = parser.getDocument();
+    } catch (Exception e) {
+      if (debug) {
+	System.out.println("Unable to parse policy template file:" + e);
+      }
+    }
+
+    return document;
+
+  }
+
   public static void main(String[] args) {
-    String filename = args[0];
+    String communityConfFile = args[0];
+    String policiesFile = args[1];
+    String policyTemplate = args[2];
+    String outputFilePrefix = args[3];
 
     PolicyGenerator pg = new PolicyGenerator();
-    pg.setInputFileName(filename);
-    pg.parseFile();
+    pg.setCommunityConfigurationFileName(communityConfFile);
+    pg.setPolicyFileName(policiesFile);
+
+    pg.setOutputFilePrefix(outputFilePrefix);
+    pg.setPolicyTemplate(policyTemplate);
+
+    pg.parseCommunityFile();
+    pg.parsePolicyFile();
+
+    System.out.println("======== Configuration file:");
+    pg.printConfiguration();
+    System.out.println("============================");
+    pg.generatePolicyFiles();
   }
 }
