262554b88c7616b312388c52b5845faf4b7b7437
rtripath <rtripath>
2001-12-07 01:53:17 +0000

    Split Crypto policy hasmap into 2 One for Outgoing and one for Incomming

--- securityservices/src/com/nai/security/crypto/CryptoPolicyServiceImpl.java
+++ securityservices/src/com/nai/security/crypto/CryptoPolicyServiceImpl.java
@@ -24,7 +24,8 @@
 package com.nai.security.crypto;
 
 import java.util.HashMap;
-
+import java.util.Set;
+import java.util.Iterator;
 import com.nai.security.policy.*;
 import org.cougaar.domain.planning.ldm.policy.*;
 
@@ -38,7 +39,8 @@ public class CryptoPolicyServiceImpl implements CryptoPolicyService {
     CryptoPolicyProxy cpp;
 
     //params look-up by name
-    static HashMap hm = new HashMap();
+  static HashMap send_hm = new HashMap();
+  static HashMap receive_hm = new HashMap();
 
     /** Creates new CryptoPolicyServiceImpl */
     public CryptoPolicyServiceImpl() {
@@ -48,25 +50,46 @@ public class CryptoPolicyServiceImpl implements CryptoPolicyService {
     }
 
     public synchronized SecureMethodParam getSendPolicy(String name) {
-        Object obj = hm.get(name);
+        String tag = name;
+	Object obj = send_hm.get(tag);
+	
         //if not found, try sender with default target
-        if(obj==null) obj = hm.get(name.substring(0, name.indexOf(':'))+":DEFAULT");
+        if(obj==null) {
+	    tag = "DEFAULT" + name.substring(name.indexOf(':'));
+	    obj = send_hm.get(tag);
+	}
+		
         //still not found, last try with default sender and default target
         //usually the case at bootstrap
-        if(obj==null) obj = hm.get("DEFAULT:DEFAULT");
+        if(obj==null) {
+		tag = "DEFAULT:DEFAULT";
+		obj = send_hm.get(tag);
+	}
+
 	if(dbg) {
-	  System.out.println("CryptoPolicyService: policy: " + name + " - Policy: " + obj);
+	  System.out.println("CryptoPolicyService: outgoing policy for " + tag + " - Policy: " + obj);
 	}
         return (SecureMethodParam)obj;
     }
 
     public synchronized SecureMethodParam getReceivePolicy(String name) {
-        Object obj = hm.get(name);
+	String tag = name;
+        Object obj = receive_hm.get(tag);
         //if not found, try sender with default target
-        if(obj==null) obj = hm.get("DEFAULT" + name.substring(name.indexOf(':')));
+        if(obj==null) {
+		tag = name.substring(0, name.indexOf(':'))+":DEFAULT";
+		obj = receive_hm.get(tag);
+	}
         //still not found, last try with default sender and default target
         //usually the case at bootstrap
-        if(obj==null) obj = hm.get("DEFAULT:DEFAULT");
+        if(obj==null) {
+		tag = "DEFAULT:DEFAULT";
+		obj = receive_hm.get(tag);
+	}
+	
+	if(dbg) {
+		System.out.println("CryptoPolicyService: incoming policy for " + tag + "--Policy: " + obj);
+	}
 
         return (SecureMethodParam)obj;
     }
@@ -88,23 +111,23 @@ public class CryptoPolicyServiceImpl implements CryptoPolicyService {
    * Merges an existing policy with a new policy.
    * @param policy the new policy to be added
    */
-  public void receivePolicyMessage(Policy policy,
-				   String policyID,
-				   String policyName,
-				   String policyDescription,
-				   String policyScope,
-				   String policySubjectID,
-				   String policySubjectName,
-				   String policyTargetID,
-				   String policyTargetName,
-				   String policyType) {
-    if (dbg) {
-      System.out.println("CryptoPolicyServiceImpl: " + policy);
-      RuleParameter[] param = policy.getRuleParameters();
-      for (int i = 0 ; i < param.length ; i++) {
-	System.out.println("Rule: " + param[i].getName() + " - " + param[i].getValue());
-      }
-    }
+      public void receivePolicyMessage(Policy policy,
+				       String policyID,
+				       String policyName,
+				       String policyDescription,
+				       String policyScope,
+				       String policySubjectID,
+				       String policySubjectName,
+				       String policyTargetID,
+				       String policyTargetName,
+				       String policyType) {
+	if (dbg) {
+	  System.out.println("CryptoPolicyServiceImpl: " + policy);
+	  RuleParameter[] param = policy.getRuleParameters();
+	  for (int i = 0 ; i < param.length ; i++) {
+	    System.out.println("Rule: " + param[i].getName() + " - " + param[i].getValue());
+	  }
+	}
 
 	if(policy == null)return;
         //whom is the policy for?
@@ -118,91 +141,178 @@ public class CryptoPolicyServiceImpl implements CryptoPolicyService {
           sub = "DEFAULT";
         }
         if(sub=="" || sub == null) return ;
-
+	//String defaultRuleParam=null;
+	String defaultInSymmetricAlg=null;
+	String defaultOutSymmetricAlg=null;
+	String defaultInAsymmetricAlg=null;
+	String defaultOutAsymmetricAlg=null;
+	String defaultOutSignAlg=null;
+	String defaultInSignAlg=null;
+	/*
+	  booleans incomming and outgoing gives information to functions 
+	  on which hash map to use  
+	 */
+	boolean incoming=true;
+	boolean outgoing=false;
         //for each RuleParameter
 	RuleParameter[] ruleParameters = policy.getRuleParameters();
         for (int j=0; j < ruleParameters.length; j++)
-        {
+	  {
             if(!(ruleParameters[j] instanceof KeyRuleParameter)) return;
             KeyRuleParameter krp = (KeyRuleParameter)ruleParameters[j];
             //process rules on all the parameters within secureMethodParam
             String name = krp.getName();
-            String value = (String)krp.getValue();
+	    Object valueobj=krp.getValue();
+	    String value=null;
+	    if(valueobj!=null) {
+	      value=(String)valueobj;
+	      //defaultRuleParam=value; 
+	    }
+	    else {
+	      if(dbg) {
+		System.out.println("Warning : No default value for KeyRule parameter "); 
+	      }
+	    }
+		
+	    //    String value = (String)krp.getValue();
             KeyRuleParameterEntry[] entry = krp.getKeys();
             if(name.endsWith("SecureMethod")){
-                if(name.startsWith("Outgoing")) {
-                    if(value!=null) updateSecureMethod(sub+":"+"DEFAULT",value);
-                    for(int i = 0; i < entry.length; i++) {
-                      updateSecureMethod(sub+":"+entry[i].getKey(), entry[i].getValue());
-                    }
-                }
-                if(name.startsWith("Incoming")) {
-                    if(value!=null) updateSecureMethod("DEFAULT"+":"+sub,value);
-                    if(value!=null&&isBoot) {
-                      updateSecureMethod("BOOT"+":"+sub,value);
-                    }
-                    for(int i = 0; i < entry.length; i++) {
-                      updateSecureMethod(entry[i].getKey()+":"+sub, entry[i].getValue());
-                    }
-                }
+	      if(name.startsWith("Outgoing")) {
+		if(value!=null) {
+		  updateSecureMethod(sub+":"+"DEFAULT",value,outgoing);
+		}
+		for(int i = 0; i < entry.length; i++) {
+		  updateSecureMethod(sub+":"+entry[i].getKey(), entry[i].getValue(),outgoing);
+		}
+	      }
+	      if(name.startsWith("Incoming")) {
+		if(value!=null) { 
+		  updateSecureMethod("DEFAULT"+":"+sub,value,incoming);
+		}
+		if(value!=null&&isBoot) {
+		  updateSecureMethod("BOOT"+":"+sub,value,incoming);
+		}
+		for(int i = 0; i < entry.length; i++) {
+		  updateSecureMethod(entry[i].getKey()+":"+sub, entry[i].getValue(),incoming);
+		}
+	      }
             }
 
             if(name.endsWith("SymmetricAlgorithm")){
-                if(name.startsWith("Outgoing")) {
-                    if(value!=null) updateSymmetricAlgorithm(sub+":"+"DEFAULT",value);
-                    for(int i = 0; i < entry.length; i++) {
-                      updateSymmetricAlgorithm(sub+":"+entry[i].getKey(), entry[i].getValue());
-                    }
-                }
-                if(name.startsWith("Incoming")) {
-                    if(value!=null) updateSymmetricAlgorithm("DEFAULT"+":"+sub,value);
-                    if(value!=null&&isBoot) updateSymmetricAlgorithm("BOOT"+":"+sub,value);
-                    for(int i = 0; i < entry.length; i++) {
-                      updateSymmetricAlgorithm(entry[i].getKey()+":"+sub, entry[i].getValue());
-                    }
-                }
+	      if(name.startsWith("Outgoing")) {
+		if(value!=null) {
+		  defaultOutSymmetricAlg=value;
+		  updateSymmetricAlgorithm(sub+":"+"DEFAULT",value,outgoing);
+		}
+		for(int i = 0; i < entry.length; i++) {
+		  updateSymmetricAlgorithm(sub+":"+entry[i].getKey(), entry[i].getValue(),outgoing);
+		}
+	      }
+	      if(name.startsWith("Incoming")) {
+		if(value!=null) {
+		  defaultInSymmetricAlg=value;
+		  updateSymmetricAlgorithm("DEFAULT"+":"+sub,value,incoming);
+		}
+		if(value!=null&&isBoot) updateSymmetricAlgorithm("BOOT"+":"+sub,value,incoming);
+		for(int i = 0; i < entry.length; i++) {
+		  updateSymmetricAlgorithm(entry[i].getKey()+":"+sub, entry[i].getValue(),incoming);
+		}
+	      }
             }
 
             if(name.endsWith("AsymmetricAlgorithm")){
-                if(name.startsWith("Outgoing")) {
-                    if(value!=null) updateAsymmetricAlgorithm(sub+":"+"DEFAULT",value);
-                    for(int i = 0; i < entry.length; i++) {
-                      updateAsymmetricAlgorithm(sub+":"+entry[i].getKey(), entry[i].getValue());
-                    }
-                }
-                if(name.startsWith("Incoming")) {
-                    if(value!=null) updateAsymmetricAlgorithm("DEFAULT"+":"+sub,value);
-                    if(value!=null&&isBoot) updateAsymmetricAlgorithm("BOOT"+":"+sub,value);
-                    for(int i = 0; i < entry.length; i++) {
-                      updateAsymmetricAlgorithm(entry[i].getKey()+":"+sub, entry[i].getValue());
-                    }
-                }
+	      if(name.startsWith("Outgoing")) {
+		if(value!=null) {
+		  defaultOutAsymmetricAlg=value;
+		  updateAsymmetricAlgorithm(sub+":"+"DEFAULT",value,outgoing);
+		}
+		for(int i = 0; i < entry.length; i++) {
+		  updateAsymmetricAlgorithm(sub+":"+entry[i].getKey(), entry[i].getValue(),outgoing);
+		}
+	      }
+	      if(name.startsWith("Incoming")) {
+		if(value!=null) {
+		  defaultInAsymmetricAlg=value;
+		  updateAsymmetricAlgorithm("DEFAULT"+":"+sub,value,incoming);
+		}
+		if(value!=null&&isBoot) updateAsymmetricAlgorithm("BOOT"+":"+sub,value,incoming);
+		for(int i = 0; i < entry.length; i++) {
+		  updateAsymmetricAlgorithm(entry[i].getKey()+":"+sub, entry[i].getValue(),incoming);
+		}
+	      }
             }
 
             if(name.endsWith("SigningAlgorithm")){
-                if(name.startsWith("Outgoing")) {
-                    if(value!=null) updateSigningAlgorithm(sub+":"+"DEFAULT",value);
-                    for(int i = 0; i < entry.length; i++) {
-                      updateSigningAlgorithm(sub+":"+entry[i].getKey(), entry[i].getValue());
-                    }
-                }
-                if(name.startsWith("Incoming")) {
-                    if(value!=null) updateSigningAlgorithm("DEFAULT"+":"+sub,value);
-                    if(value!=null&&isBoot) updateSigningAlgorithm("BOOT"+":"+sub,value);
-                    for(int i = 0; i < entry.length; i++) {
-                      updateSigningAlgorithm(entry[i].getKey()+":"+sub, entry[i].getValue());
-                    }
-                }
+	      if(name.startsWith("Outgoing")) {
+		if(value!=null) {
+		  defaultOutSignAlg=value;
+		  updateSigningAlgorithm(sub+":"+"DEFAULT",value,outgoing);
+		}
+		for(int i = 0; i < entry.length; i++) {
+		  updateSigningAlgorithm(sub+":"+entry[i].getKey(), entry[i].getValue(),outgoing);
+		}
+	      }
+	      if(name.startsWith("Incoming")) {
+		if(value!=null) {
+		  defaultInSignAlg=value;
+		  updateSigningAlgorithm("DEFAULT"+":"+sub,value,incoming);
+		}
+		if(value!=null&&isBoot) updateSigningAlgorithm("BOOT"+":"+sub,value,incoming);
+		for(int i = 0; i < entry.length; i++) {
+		  updateSigningAlgorithm(entry[i].getKey()+":"+sub, entry[i].getValue(),incoming);
+		}
+	      }
             }
-        }
+	  }
+	Set set=send_hm.keySet();
+	Iterator iter=set.iterator();
+	SecureMethodParam param=null;
+	String key=null;
+	
+	for(;iter.hasNext();) {
+	  key=(String)iter.next();
+	  param=(SecureMethodParam)send_hm.get(key);
+	  if((param.symmSpec==null)||(param.symmSpec.equals(""))) {
+	    param.symmSpec=defaultOutSymmetricAlg;
+	  }
+	  if((param.asymmSpec==null)||(param.asymmSpec.equals(""))) {
+	    param.asymmSpec=defaultOutAsymmetricAlg;
+	  } 
+	  if((param.signSpec==null)||(param.signSpec.equals(""))) {
+	    param.signSpec=defaultOutSignAlg;
+	  } 
+	  send_hm.put(key,param);	 
+	}
+	set=receive_hm.keySet();
+	iter=set.iterator();
+	param=null;
+	key=null;
+	for(;iter.hasNext();) {
+	  key=(String)iter.next();
+	  param=(SecureMethodParam)receive_hm.get(key);
+	  if((param.symmSpec==null)||(param.symmSpec.equals(""))) {
+	    param.symmSpec=defaultInSymmetricAlg;
+	  }
+	  if((param.asymmSpec==null)||(param.asymmSpec.equals(""))) {
+	    param.asymmSpec=defaultInAsymmetricAlg;
+	  } 
+	  if((param.signSpec==null)||(param.signSpec.equals(""))) {
+	    param.signSpec=defaultInSignAlg;
+	  } 
+	  receive_hm.put(key,param);	 
+	}
+		       
+      }
 
-
-    }
-
-    private synchronized void updateSecureMethod(String key, String value){
+    private synchronized void updateSecureMethod(String key, String value,boolean incoming){
         //entry in the hash map
         SecureMethodParam smp;
-        smp = (SecureMethodParam)hm.get(key);
+	if(incoming) {
+	  smp = (SecureMethodParam)receive_hm.get(key);
+	}
+	else {
+	   smp = (SecureMethodParam)send_hm.get(key);
+	}
         if(smp==null) smp=new SecureMethodParam();
         if(value.equalsIgnoreCase("plain")){
           smp.secureMethod = SecureMethodParam.PLAIN;
@@ -213,32 +323,73 @@ public class CryptoPolicyServiceImpl implements CryptoPolicyService {
         }else if(value.equalsIgnoreCase("signAndEncrypt")){
           smp.secureMethod = SecureMethodParam.SIGNENCRYPT;
         }
-        hm.put(key, smp);
+	if(incoming) {
+	  receive_hm.put(key, smp);
+	}
+	else {
+	   send_hm.put(key, smp);
+	}
     }
 
-    private synchronized void updateSymmetricAlgorithm(String key, String value){
+    private synchronized void updateSymmetricAlgorithm(String key, String value,boolean incoming){
         //entry in the hash map
         SecureMethodParam smp;
-        smp = (SecureMethodParam)hm.get(key);
+	if(incoming) {
+	  smp = (SecureMethodParam)receive_hm.get(key);
+	}
+	else {
+	   smp = (SecureMethodParam)send_hm.get(key);
+	}
+	// smp = (SecureMethodParam)hm.get(key);
         if(smp==null) smp=new SecureMethodParam();
         smp.symmSpec = value;
-        hm.put(key, smp);
+	if(incoming) {
+	  receive_hm.put(key, smp);
+	}
+	else {
+	   send_hm.put(key, smp);
+	}
+	// hm.put(key, smp);
     }
-    private synchronized void updateAsymmetricAlgorithm(String key, String value){
+    private synchronized void updateAsymmetricAlgorithm(String key, String value, boolean incoming){
         //entry in the hash map
         SecureMethodParam smp;
-        smp = (SecureMethodParam)hm.get(key);
+	if(incoming) {
+	  smp = (SecureMethodParam)receive_hm.get(key);
+	}
+	else {
+	   smp = (SecureMethodParam)send_hm.get(key);
+	}
+        //smp = (SecureMethodParam)hm.get(key);
         if(smp==null) smp=new SecureMethodParam();
         smp.asymmSpec = value;
-        hm.put(key, smp);
+	if(incoming) {
+	  receive_hm.put(key, smp);
+	}
+	else {
+	   send_hm.put(key, smp);
+	}
+	// hm.put(key, smp);
     }
-    private synchronized void updateSigningAlgorithm(String key, String value){
+    private synchronized void updateSigningAlgorithm(String key, String value, boolean incoming){
         //entry in the hash map
         SecureMethodParam smp;
-        smp = (SecureMethodParam)hm.get(key);
+        if(incoming) {
+	  smp = (SecureMethodParam)receive_hm.get(key);
+	}
+	else {
+	   smp = (SecureMethodParam)send_hm.get(key);
+	}
+	//smp = (SecureMethodParam)hm.get(key);
         if(smp==null) smp=new SecureMethodParam();
         smp.signSpec = value;
-        hm.put(key, smp);
+	if(incoming) {
+	  receive_hm.put(key, smp);
+	}
+	else {
+	   send_hm.put(key, smp);
+	}
+	// hm.put(key, smp);
     }
 
 }
