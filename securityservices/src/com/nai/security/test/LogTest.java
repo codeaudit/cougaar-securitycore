e393bceba195a5b18fa0943b8ee2e22236a585bf
srosset <srosset>
2002-02-14 17:25:18 +0000

    Added two classes to test the logging mechanism with Security Manager

---
+++ securityservices/src/com/nai/security/test/LogTest.java
@@ -0,0 +1,56 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2001 Networks Associates Technology, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ * Created on September 12, 2001, 10:55 AM
+ */
+
+package com.nai.security.test;
+
+import java.util.*;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.Level;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+ 
+public class LogTest {
+
+  static Logger logger = Logger.getLogger(LogTest.class.getName());
+        
+  public static void main(String[] args) {
+    LogTest tc = new LogTest();
+
+    tc.log(args);
+  }
+
+  public void log(String[] args)
+  {
+    // BasicConfigurator replaced with PropertyConfigurator.
+    PropertyConfigurator.configure(args[0]);     
+
+    System.out.println("Start logging data");
+
+    logger.info("Entering application.");
+    LoggingFacility bar = new LoggingFacility();
+    bar.doIt();
+    logger.info("Exiting application.");    
+
+    System.out.println("Done logging data");
+  }
+}
