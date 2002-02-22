ceb7a4fb42c337e3399e842a9183dbe134d63344
srosset <srosset>
2002-02-22 17:52:56 +0000

    Skeleton implementation

--- securityservices/src/org/cougaar/core/security/crypto/AgentMobilityServiceImpl.java
+++ securityservices/src/org/cougaar/core/security/crypto/AgentMobilityServiceImpl.java
@@ -27,17 +27,102 @@
 package org.cougaar.core.security.crypto;
 
 import java.lang.*;
+import java.security.cert.X509Certificate;
+import java.security.PrivateKey;
+
 import org.cougaar.core.component.Service;
 import org.cougaar.core.security.services.crypto.AgentMobilityService;
 
+import com.nai.security.crypto.*;
+import com.nai.security.util.CryptoDebug;
+
 public class AgentMobilityServiceImpl implements AgentMobilityService {
 
-  public Object moveAgent(String agentName, String targetNodeAgent)
+  public Object moveAgentTo(String agentName, String targetNodeAgent)
   {
-    return null;
+    /* Package the agent private and public keys.
+     */
+    byte[] pkcs12 = createPkcs12(agentName, targetNodeAgent);
+
+    /* Remove the agent keys from the local keystore.
+     * There should be a transaction here, otherwise we may end
+     * up deleting keys before they have reached their destination.
+     */
+
+    return pkcs12;
   }
 
-  public void setupAgent(Object cryptoAgentData, String agentName)
+  public void moveAgentFrom(String agentName, Object cryptoAgentData)
   {
+    /* Extract the keys from the PKCS12 envelope.
+     */
+    PrivateKeyCert[] kcert = extractPkcs12(agentName, cryptoAgentData);
+
+    /* Install keys in the local keystore.
+     */
+  }
+
+  /**
+   */
+  private byte[] createPkcs12(String agentName, String targetNodeAgent)
+  {
+    String nodeAgentName = null;
+
+    /* Get the private key of the signer: the node agent.
+     */
+    if (CryptoDebug.debug) {
+      System.out.println("========= Looking up key for sender node");
+    }
+    PrivateKey signerPrivKey = KeyRing.findPrivateKey(nodeAgentName);
+
+    if (CryptoDebug.debug) {
+      System.out.println("========= Looking up certificate for sender node");
+    }
+    X509Certificate signerCertificate =
+      (X509Certificate)KeyRing.findCert(nodeAgentName);
+
+    if (CryptoDebug.debug) {
+      System.out.println("======== Looking up agent's key to be wrapped");
+    }
+    PrivateKey privKey = KeyRing.findPrivateKey(agentName);
+    X509Certificate cert =
+      (X509Certificate)KeyRing.findCert(agentName);
+
+    if (CryptoDebug.debug) {
+      System.out.println("======== Looking up key for target node");
+    }
+    PrivateKey rcvrPrivKey = KeyRing.findPrivateKey(targetNodeAgent);
+    X509Certificate rcvrCert =
+      (X509Certificate)KeyRing.findCert(targetNodeAgent);
+
+    if (CryptoDebug.debug) {
+      System.out.println("======== Creating PKCS#12 envelope");
+    }
+    byte[] pkcs12 = KeyRing.protectPrivateKey(privKey,
+					      cert,
+					      signerPrivKey,
+					      signerCertificate,
+					      rcvrCert);
+    return pkcs12;
+  }
+
+  private PrivateKeyCert[] extractPkcs12(String agentName,
+					 Object cryptoAgentData)
+  {
+    String nodeAgentName = null;
+    byte[] pkcs12 = (byte[]) cryptoAgentData;
+
+    PrivateKey rcvrPrivKey = KeyRing.findPrivateKey(nodeAgentName);
+    X509Certificate rcvrCert =
+      (X509Certificate)KeyRing.findCert(nodeAgentName);
+
+    if (CryptoDebug.debug) {
+      System.out.println("======== Extracting PKCS#12 envelope");
+    }
+
+    PrivateKeyCert[] pkey = KeyRing.getPfx(pkcs12,
+					   rcvrPrivKey,
+					   rcvrCert);
+    return pkey;
   }
 }
