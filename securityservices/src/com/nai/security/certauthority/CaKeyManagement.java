fabfd76a8a51820eef3579f6201cc1560131488e
srosset <srosset>
2002-02-28 21:11:04 +0000

    New servlet for management of keys of the Certificate Authority

---
+++ securityservices/src/com/nai/security/certauthority/CaKeyManagement.java
@@ -0,0 +1,95 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2001 Networks Associates Technology, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).  
+ *  
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS 
+ *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR 
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF 
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT 
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT 
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL 
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS, 
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR 
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.  
+ * 
+ * </copyright>
+ *
+ * CHANGE RECORD
+ * - 
+ */
+
+package com.nai.security.certauthority;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.security.cert.X509Certificate;
+
+import sun.security.x509.*;
+
+import com.nai.security.crypto.ConfParser;
+import com.nai.security.crypto.CertificateUtility;
+import com.nai.security.crypto.ldap.CertDirectoryServiceClient;
+import com.nai.security.crypto.ldap.CertDirectoryServiceFactory;
+import com.nai.security.crypto.ldap.LdapEntry;
+import com.nai.security.policy.CaPolicy;
+
+public class CaKeyManagement extends  HttpServlet
+{
+  private ConfParser confParser = null;
+  private CaPolicy caPolicy = null;            // the policy of the CA
+  private javax.servlet.ServletContext context=null;
+
+  public void init(ServletConfig config) throws ServletException
+  {
+    context=config.getServletContext();
+    String confpath=
+      (String)context.getAttribute("org.cougaar.security.crypto.config");
+
+    confParser = new ConfParser(confpath, true);
+  }
+
+  public void doPost (HttpServletRequest  req, HttpServletResponse res)
+    throws ServletException,IOException
+  {
+    PrintWriter out=res.getWriter();
+
+    out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>Certificate Authority Key Management</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("</body></html>");
+    out.flush();
+    out.close();
+  }
+  
+  protected void doGet(HttpServletRequest req,HttpServletResponse res)
+    throws ServletException, IOException
+  {
+    res.setContentType("Text/HTML");
+    PrintWriter out=res.getWriter();
+    out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>Certificate Authority Key Management</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("</body></html>");
+    out.flush();
+    out.close();
+  }
+  
+  public String getServletInfo()
+  {
+    return("Manage keys of Certificate Authority");
+  }
+}
