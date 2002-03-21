37ded0c01dc1745f1a1fe01ab5412d2c376ab207
rliao1 <rliao1>
2002-03-21 00:27:47 +0000

    interface for TomcatServletEngine to use.

---
+++ securityservices/src/org/cougaar/core/security/services/identity/WebserverIdentityService.java
@@ -0,0 +1,63 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2001 Networks Associates Technology, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ *
+ * </copyright>
+ *
+ * CHANGE RECORD
+ * -
+ */
+
+package org.cougaar.core.security.services.identity;
+
+import org.cougaar.core.component.Service;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.apache.catalina.net.SSLServerSocketFactory;
+
+public interface WebserverIdentityService extends Service {
+  /**
+   * This function should be called at Tomcat startup, it will
+   * perform the following:
+   *
+   * 1. check whether there is a certificate for tomcat server, if not get one
+   * 2. check whether the server cert has expired, if so get one
+   * 3. initialize SSLContext with our own keystore and truststore
+   * 4. return a catalina SSLServerSocketFactory instance
+   *
+   * The SSLServerSocketFactory is either a subclass of catalina.SSLServerSocketFactory
+   * or the catalina factory itself, with context settings using SecurityServices'
+   * keystore and trust store.
+   *
+   */
+  SSLServerSocketFactory getServerSocketFactory();
+
+  /**
+   * This function is to set HttpsConfig parameters, Cougaar may have its own
+   * config policy requirements.
+   *
+   * @parameter:    the HttpsConfig passed from Tomcat configuration
+   * @return:       the modified HttpsConfig, could change keystore, keystore password,
+   *                etc.
+   *
+   */
+  HttpsConfig setHttpsConfig(HttpsConfig httpsC);
+}
+
