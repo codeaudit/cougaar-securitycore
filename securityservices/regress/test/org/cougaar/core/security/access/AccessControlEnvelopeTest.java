2377d5514698372a052f3ec0ddf08be98d88a341
tom <tom>
2002-04-26 19:42:56 +0000

    initial check in of JUnit test case.

---
+++ securityservices/regress/test/org/cougaar/core/security/access/AccessControlEnvelopeTest.java
@@ -0,0 +1,35 @@
+package test.org.cougaar.core.security.access;
+
+import junit.framework.*;
+import org.cougaar.core.mts.Message;
+import org.cougaar.core.mts.MessageEnvelope;
+import org.cougaar.core.security.acl.trust.*;
+import org.cougaar.core.security.access.*;
+
+public class AccessControlEnvelopeTest extends TestCase
+{
+	public AccessControlEnvelopeTest(String name)
+	{
+		super(name);
+	}
+
+	public void testSetTrustSet()
+	{
+		AccessControlEnvelope ace = new AccessControlEnvelope(null);
+		TrustSet ts = new TrustSet();
+		ace.setTrustSet(ts);
+		assertEquals(ace.getTrustSet(), ts);
+	}
+	
+	public void testSetTrustSets()
+	{
+		AccessControlEnvelope ace = new AccessControlEnvelope(null);
+		TrustSet[] ts = new TrustSet[2];
+		ts[0] = new TrustSet();
+		ts[1] = new TrustSet();
+		ace.setTrustSets(ts);
+		TrustSet[] ts2 = ace.getTrustSets();
+		assertEquals(ts2[0], ts[0]);
+		assertEquals(ts2[1], ts[1]);
+	}
+}
