da0f4dc085982dd211aa5f66833f302c33428a45
srosset <srosset>
2002-04-26 19:32:20 +0000

    *** empty log message ***

---
+++ securityservices/src/org/cougaar/core/security/monitoring/blackboard/CapabilitiesObject.java
@@ -0,0 +1,36 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2001 Network Associates
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+
+package org.cougaar.core.security.monitoring.blackboard;
+
+import java.io.Serializable;
+
+// Cougaar core services
+import org.cougaar.core.blackboard.Publishable;
+import java.util.Hashtable;
+
+public class CapabilitiesObject extends Hashtable implements Publishable {
+    public boolean isPersistable() {
+    return true;
+  }  
+   
+}
