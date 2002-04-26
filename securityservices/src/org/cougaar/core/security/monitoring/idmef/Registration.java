67990d89edbae5c20ff27af1280630fc6a5c6261
rtripath <rtripath>
2002-04-26 02:13:48 +0000

    maked this as a marker interface and old Registration code to RegistrationAlert.java

--- securityservices/src/org/cougaar/core/security/monitoring/idmef/Registration.java
+++ securityservices/src/org/cougaar/core/security/monitoring/idmef/Registration.java
@@ -19,51 +19,10 @@
  * </copyright>
  */
 package org.cougaar.core.security.monitoring.idmef;
-
-import edu.jhuapl.idmef.AdditionalData;
-import edu.jhuapl.idmef.Alert;
-import edu.jhuapl.idmef.Analyzer;
-import edu.jhuapl.idmef.Classification;
-import edu.jhuapl.idmef.CreateTime;
-import edu.jhuapl.idmef.Source;
-import edu.jhuapl.idmef.Target;
-
-/**
- * Registration subclasses Alert, and is used to distinguish
- * the difference been an Alert message and a Registration message
- * avoiding the need to determine the message type via the AdditionalData
- * object.
+/*
+ *  This is just a marker interface for Registration Event  
+ *
  */
-public class Registration extends Alert {
-    
-    public static String TYPE = "sensor-registration";
-    /**
-     * Creates a message for a sensor to register its capabilities.
-     * Can only be create through IdmefMessageFactory
-     */
-    Registration( Analyzer analyzer,
-                  Source []sources,
-                  Target []targets,
-                  Classification []capabilities,
-                  AdditionalData []data,
-                  String ident ){
-        super( analyzer, 
-               new CreateTime(), 
-               null,  // detection time
-               null,  // don't think we need AnalyzerTime
-               sources, // sources
-               targets, // targets
-               capabilities,
-               null,    // assessment 
-               data, 
-               ident );
-    }
-    
-    /**
-     * Creates an empty message for a sensor to register its capabilities.
-     * Can only be create through IdmefMessageFactory
-     */
-    Registration(){
-        super();
-    }
+
+public interface Registration {
 }
