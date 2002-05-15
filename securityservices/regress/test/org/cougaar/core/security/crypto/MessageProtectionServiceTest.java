cbfcda1c74fb9209e38caedca703083c85c7e960
srosset <srosset>
2002-05-15 23:57:18 +0000

    * Added new test

--- securityservices/regress/test/org/cougaar/core/security/crypto/MessageProtectionServiceTest.java
+++ securityservices/regress/test/org/cougaar/core/security/crypto/MessageProtectionServiceTest.java
@@ -20,48 +20,52 @@
  * Created on September 12, 2001, 10:55 AM
  */
 
-package test.org.cougaar.core.security.access;
+package test.org.cougaar.core.security.crypto;
 
 import junit.framework.*;
-/*
-import org.cougaar.core.mts.Message;
-import org.cougaar.core.mts.MessageEnvelope;
-import org.cougaar.core.security.acl.trust.*;
-import org.cougaar.core.security.access.*;
-*/
 
-public class AccessControlEnvelopeTest
+// Cougaar core services
+import org.cougaar.core.service.*;
+
+// Cougaar security services
+import org.cougaar.core.security.crypto.*;
+import org.cougaar.core.security.provider.SecurityServiceProvider;
+
+public class MessageProtectionServiceTest
   extends TestCase
 {
-  public AccessControlEnvelopeTest(String name) {
+
+  public MessageProtectionServiceTest(String name) {
     super(name);
   }
 
-  public void testFoo() {
-    // the tests below were just added
-    // as an example.  Since they're not compiling now,
-    // I've commented them out...
+  public static Test suite() {
+    return new TestSuite(MessageProtectionServiceTest.class);
   }
 
-/*
-  public void testSetTrustSet()
-  {
-  AccessControlEnvelope ace = new AccessControlEnvelope(null);
-  TrustSet ts = new TrustSet();
-  ace.setTrustSet(ts);
-  assertEquals(ace.getTrustSet(), ts);
+  /**
+   * A test case method consists of:
+   * 1) Code which creates the objects we will interact with during the test.
+   *    This testing context is commonly referred to as a test's fixture.
+   * 2) Code which exercises the objects in the fixture.
+   * 3) Code which verifies the result.
+   */
+  public void testEncryption() {
+
+    /** 1) */
+
+    SecurityServiceProvider secProvider = null;
+    MessageProtectionService mps = null;
+    secProvider = new SecurityServiceProvider();
+
+    mps =
+      (MessageProtectionService)secProvider.getService(null,
+						       this,
+						       MessageProtectionService.class);
+
+    /** 2) */
+
+    /** 3) */
+    Assert.assertTrue(false);
   }
-	
-  public void testSetTrustSets()
-  {
-  AccessControlEnvelope ace = new AccessControlEnvelope(null);
-  TrustSet[] ts = new TrustSet[2];
-  ts[0] = new TrustSet();
-  ts[1] = new TrustSet();
-  ace.setTrustSets(ts);
-  TrustSet[] ts2 = ace.getTrustSets();
-  assertEquals(ts2[0], ts[0]);
-  assertEquals(ts2[1], ts[1]);
-  }
-*/
 }
