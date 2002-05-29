faea5b522e663fe8457f5cd9ad0f335fcd12035c
rliao1 <rliao1>
2002-05-29 19:17:36 +0000

    for multiple CAs, identity the root CA

--- securityservices/src/org/cougaar/core/security/policy/CryptoClientPolicy.java
+++ securityservices/src/org/cougaar/core/security/policy/CryptoClientPolicy.java
@@ -3,25 +3,25 @@
  *  Copyright 1997-2001 Networks Associates Technology, Inc.
  *  under sponsorship of the Defense Advanced Research Projects
  *  Agency (DARPA).
- * 
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the Cougaar Open Source License as published by
- *  DARPA on the Cougaar Open Source Website (www.cougaar.org).  
- *  
- *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS 
- *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR 
- *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF 
- *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT 
- *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT 
- *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL 
- *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS, 
- *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR 
- *  PERFORMANCE OF THE COUGAAR SOFTWARE.  
- * 
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ *
  * </copyright>
  *
  * CHANGE RECORD
- * - 
+ * -
  */
 
 package org.cougaar.core.security.policy;
@@ -32,6 +32,8 @@ public class CryptoClientPolicy
   extends SecurityPolicy
 {
   private boolean isCertificateAuthority;
+
+  private boolean isRootCA;
 
   /** The file name of the keystore containing keys for this node
    */
@@ -64,6 +66,10 @@ public class CryptoClientPolicy
     return isCertificateAuthority;
   }
 
+  public boolean isRootCA() {
+    return isRootCA;
+  }
+
   public String getKeystoreName() {
     return keystoreName;
   }
@@ -89,6 +95,10 @@ public class CryptoClientPolicy
   // Set methods
   public void setIsCertificateAuthority(boolean isCertAuth) {
     isCertificateAuthority = isCertAuth;
+  }
+
+  public void setIsRootCA(boolean isRoot) {
+    isRootCA = isRoot;
   }
 
   public void setKeystoreName(String keystoreName) {
