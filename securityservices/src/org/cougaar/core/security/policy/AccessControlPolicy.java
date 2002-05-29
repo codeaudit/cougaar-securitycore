5d0fc98bff7a92cace353273b6146f00a8765dbe
ychen <ychen>
2002-05-29 16:59:27 +0000

    reverted last change. i.e. this version equals 1.3

--- securityservices/src/org/cougaar/core/security/policy/AccessControlPolicy.java
+++ securityservices/src/org/cougaar/core/security/policy/AccessControlPolicy.java
@@ -26,70 +26,109 @@
 
 package org.cougaar.core.security.policy;
 
-import java.util.HashMap;
+import org.cougaar.planning.ldm.policy.*;
 
 /**
  * Access control policy class instance and policy contstants. The constants
  * are specific to access control policy. An instance should have a specific 
- * target agent (or possibly community).
+ * target agent (or possibly a node).
  */
-public class AccessControlPolicy extends SecurityPolicy {
+public class AccessControlPolicy extends TypedPolicy {
+
+    /**
+     * Name of rule parameter containing the agent's name.
+     */
+    private static final String AGENT_NAME_RULE = "agentName";
+
+    // XML policy file constants used as keys for looking up policy
+
+    // incoming message policy sections
+
+    /**
+     * Incoming message integrity level (Integer value)
+     */
+    public static String IN_MSG_INTEGRITY = "IncomingMessageIntegrity";
+    /**
+     * Incoming message criticality (Integer value)
+     */
+    public static String IN_MSG_CRITICALITY = "IncomingMessageCriticality";
+    /**
+     * Incoming message action { SET_ASIDE, FORWARD, ACCEPT }
+     */
+    public static String IN_MSG_ACTION = "IncomingMessageAction";
+
+    /**
+     * Incoming agent action { SET_ASIDE, FORWARD, ACCEPT }
+     */
+    public static String IN_AGENT_ACTION = "IncomingAgentAction";
+
+    // outgoing message action policy sections
+
+    /**
+     * Outgoing message integrity level (Integer value)
+     */
+    public static String OUT_MSG_INTEGRITY = "OutgoingMessageIntegrity";
+    /**
+     * Outgoing message mission criticality (Integer value)
+     */
+    public static String OUT_MSG_CRITICALITY = "OutgoingMessageCriticality";
+    /**
+     * Outgoing message action { SET_ASIDE, FORWARD, ACCEPT }
+     */
+    public static String OUT_MSG_ACTION = "OutgoingMessageAction";
+    /**
+     * Outgoing message action { SET_ASIDE, FORWARD, ACCEPT }
+     */
+    public static String OUT_AGENT_ACTION = "OutgoingAgentAction";
+    
+
+    // Message action constants
+
+    /**
+     * A message action constant to redirect a message to another agent or
+     * node. This can be used for incoming or outgoing messages.
+     */
+    public static String FORWARD = "ForwardTo";
+    /**
+     * A message action constant to allow a message to be accepted. Incoming
+     * messages will be received by the agent and outgoing messages will be 
+     * sent to the message transport service.
+     */
+    public static String ACCEPT = "AcceptMessage";
+    /**
+     * A message action constant to allow a message to be accepted. Incoming
+     * messages will be received by the agent and outgoing messages will be 
+     * sent to the message transport service.
+     */
+
+    public static String SET_ASIDE = "SetAside";
+
+    /**
+     * Constant used to determine a rule's default value.
+     */
+    public static final String DEFAULT = "DEFAULT";
 
   /**
-   * to whom this policy is applied to.
+   * default contructors which calls TypedPolicy constructor and name policy type
    */
-  public String NAME = "UNKNOWN";
+  public AccessControlPolicy() {
+    super("org.cougaar.core.security.policy.AccessControlPolicy");
 
-  public static final int AGENT = 0;
-  public static final int COMMUNITY = 1;
-  public static final int SOCIETY = 2;
-  public int TYPE = AGENT;
-
-  public static final int INCOMING = 0;
-  public static final int OUTGOING = 1;
-  public static final int BOTH = 2;
-  public int DIRECTION = BOTH;
-
-  public static final int ACCEPT = 0;
-  public static final int SET_ASIDE = 1;
-  private HashMap actions = new HashMap();
-  public Object getAction(String key){
-    return actions.get(key);
-  }
-  public void setAction(String key, Object value){
-    actions.put(key, value);
-    return;
-  }
-  
-  public static final int INTEGRITY0 = 0;
-  public static final int INTEGRITY1 = 1;
-  public static final int INTEGRITY2 = 2;
-  public static final int INTEGRITY3 = 3;
-  public static final int INTEGRITY4 = 4;
-  public static final int INTEGRITY5 = 5;
-  private HashMap trusts = new HashMap();
-  public Object getTrust(String key){
-    return trusts.get(key);
-  }
-  public void setTrust(String key, Object value){
-    trusts.put(key, value);
-    return;
   }
 
-  private HashMap verbs = new HashMap();
-  public Object getVerb(String key){
-    return verbs.get(key);
-  }
-  public void setVerb(String key, Object value){
-    verbs.put(key, value);
-    return;
-  }
-
-  public String toString() {
-  return "NAME:" + NAME +
-        " TYPE:" + TYPE +
-        " DIRECTION:" + DIRECTION 
-  ;
+  /**
+   * Returns the name for which this access control policy pertains.
+   */
+  public String getAgentName() 
+  {
+      RuleParameter nameRule = Lookup(AGENT_NAME_RULE);
+      if(nameRule == null) {
+        String name = (String)nameRule.getValue();
+        if(name instanceof String) {
+            return name;
+        }
+      }
+      return "UNKNOWN";
   }
 
 }
