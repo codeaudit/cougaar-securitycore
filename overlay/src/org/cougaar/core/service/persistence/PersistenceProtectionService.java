e4ae72a6c5f7eacd2c3ab13948fc8b96c7139363
srosset <srosset>
2002-06-11 22:07:34 +0000

    Protected Persistence Service

---
+++ overlay/src/org/cougaar/core/service/persistence/PersistenceProtectionService.java
@@ -0,0 +1,76 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2001 Networks Associates Technology, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ *
+ * </copyright>
+ *
+ * CHANGE RECORD
+ * -
+ */
+
+package org.cougaar.core.service.persistence;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+// Cougaar core services
+import org.cougaar.core.component.Service;
+import org.cougaar.core.mts.MessageAddress;
+
+public interface PersistenceProtectionService
+  extends Service
+{
+
+  /** Protects a data stream by signing and/or encrypting the stream.
+   *  The service client should create an output stream to which the
+   *  encrypted and/or signed data should be persisted.
+   *
+   *  This service will return an OutputStream that the client should
+   *  use to write the unprotected data, as well as an object containing
+   *  an encrypted key, which must be used to decrypt the data.
+   *  The encrypted key is usually a symmetric key encrypted with the public
+   *  key of the agent.
+   *
+   *  This service must be able to re-encrypt symmetric keys at any time.
+   *  For instance, keys may be re-encrypted if the certificate containing
+   *  the public key is about to expire, or if the certificate is revoked.
+   *
+   *  In order to get access to keys at any time, the client must implement
+   *  the PersistenceProtectionServiceClient interface, which provides an
+   *  iterator over all the keys.
+   *  The client is responsible for adding the PersistedKey to the collection
+   *  of keys, so that it is available in the Iterator.
+   *
+   *  @param ai  an identifier for the agent using the persisted service
+   *  @param os  the output stream containing the encrypted and/or signed data
+   *  @return    An output stream that the client uses to protect data.
+   */
+  public PersistedOutputStream getOutputStream(MessageAddress ai, OutputStream os);
+
+  /** Unprotects a data stream by verifying and/or decrypting the stream.
+   *
+   *  The client should provide the same key that was used to encrypt
+   *  the data.
+   *  @param ai  an identifier for the agent using the persisted service.
+   *  @param is  the input stream containing the encrypted and/or signed data as
+   *             well as the key used to encrypt the stream.
+   *  @return    An input stream containing the un-encrypted and/or verified data.
+   */
+  public InputStream getInputStream(MessageAddress ai, PersistedInputStream is);
+}
