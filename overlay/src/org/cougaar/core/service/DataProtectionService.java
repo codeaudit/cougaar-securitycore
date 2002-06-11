b6699dda793dde1388b5c1c73e1250e53470e9fe
srosset <srosset>
2002-06-11 01:00:48 +0000

    Data protection service

--- overlay/src/org/cougaar/core/service/DataProtectionService.java
+++ overlay/src/org/cougaar/core/service/DataProtectionService.java
@@ -26,105 +26,25 @@
 
 package org.cougaar.core.service;
 
-import java.io.Serializable;
-import javax.crypto.SealedObject;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 // Cougaar core services
 import org.cougaar.core.component.Service;
-import org.cougaar.core.security.coreservices.crypto.EncryptAttributes;
 
-public interface DataProtectionService extends Service {
+public interface DataProtectionService
+  extends Service
+{
 
-  /** Encrypt a data stream, attribTag is used to specify the
-   *  policy driven attributes (encryption type, key strength, etc) to
-   *  retrieve (or produce) the secret key used to encrypt the
-   *  stream.
-   *  @param  agentName   name of the agent which requests the service
-   *  @param  encryptAttr The encryption information tagged as attributes.
-   *                        i.e. blackboard, trust attribute, ...
-   *  @param  out         Stream to be written for output.
-   *  @param  in          Stream to be encrypted
+  /** Protects a data stream by signing and/or encrypting the stream.
+   *  @param os  the output stream containing the encrypted and/or signed data
+   *  @return    An output stream that the client uses to protect data.
    */
-  public void encryptStream(String agentName,
-                            EncryptAttributes encryptAttr,
-                            OutputStream out,
-                            InputStream in)
-    throws RuntimeException,
-            java.io.IOException;
+  public OutputStream getOutputStream(OutputStream os);
 
-  /** Issues/Comments: putting attributes on a string may be
-   *  a bit of an overkill
-   *  @param  agentName   name of the agent which requests the service
-   *  @param  encryptAttr The encryption information tagged as attributes.
-   *                        i.e. blackboard, trust attribute, ...
-   *  @param  plainString the string to be encrypted
-   *  @return the encrypted string
+  /** Unprotects a data stream by verifying and/or decrypting the stream.
+   *  @param is  the input stream containing the unencrypted and/or verified data
+   *  @return    An input stream containing the encrypted data.
    */
-  public String encryptString(String agentName,
-                            EncryptAttributes encryptAttr,
-                            String plainString)
-    throws RuntimeException;
-
-  /**
-   *  @param  agentName   name of the agent which requests the service
-   *  @param  encryptAttr The encryption information tagged as attributes.
-   *                        i.e. blackboard, trust attribute, ...
-   *  @param  inputObject the object to be encrypted
-   *  @return encrypted object
-   */
-  public SealedObject encryptObject(String agentName,
-                            EncryptAttributes encryptAttr,
-                            Serializable inputObject)
-    throws RuntimeException;
-
-  /** For security reason, files are decrypted directly into
-   * memory instead of a temporary file for storage, therefore
-   * the file size should not be too large, ie, 1M.
-   * For places where BufferedReader is used, instead use
-   * ByteArrayOutputStream, convert output to string, then
-   * use Stream reader to read the decrypted data.
-   */
-
-  /**
-   *  @param  agentName   name of the agent which requests the service
-   *  @param  encryptAttr The encryption information tagged as attributes.
-   *                        i.e. blackboard, trust attribute, ...
-   *  @param  in          the stream to be decrypted
-   *  @param  out         the stream to be written
-   */
-  public void decryptStream(String agentName,
-                            EncryptAttributes encryptAttr,
-                            OutputStream out,
-                            InputStream in)
-    throws RuntimeException,
-            java.io.IOException;
-
-  /**
-   *  @param  agentName   name of the agent which requests the service
-   *  @param  encryptAttr The encryption information tagged as attributes.
-   *                        i.e. blackboard, trust attribute, ...
-   *  @param  encryptedString
-   *                      The string to be decrypted
-   *  @return the decrypted string
-   */
-  public String decryptString(String agentName,
-                            EncryptAttributes encryptAttr,
-                            String encryptedString)
-    throws RuntimeException;
-
-  /**
-   *  @param  agentName   name of the agent which requests the service
-   *  @param  encryptAttr The encryption information tagged as attributes.
-   *                        i.e. blackboard, trust attribute, ...
-   *  @param  encryptedObj
-   *                      The object to be decrypted
-   *  @return the decrypted object
-   */
-  public Object decryptObject(String agentName,
-                            EncryptAttributes encryptAttr,
-                            SealedObject encryptedObj)
-    throws RuntimeException;
-
+  public InputStream getInputStream(InputStream is);
 }
