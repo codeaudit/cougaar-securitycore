5bb8201264f6a22df21ef6802990162a0ab8c735
srosset <srosset>
2002-06-12 00:52:07 +0000

    Using the new AgentBinder interface

--- securityservices/src/org/cougaar/core/security/access/JaasAgentBinder.java
+++ securityservices/src/org/cougaar/core/security/access/JaasAgentBinder.java
@@ -30,6 +30,7 @@ import org.cougaar.core.component.BinderWrapper;
 import org.cougaar.core.component.BinderFactory;
 import org.cougaar.core.component.BindingSite;
 import org.cougaar.core.component.ComponentDescription;
+import org.cougaar.core.mts.MessageAddress;
 import org.cougaar.core.agent.AgentManagerForBinder;
 import org.cougaar.core.agent.Agent;
 import org.cougaar.core.agent.AgentBinder;
@@ -44,7 +45,7 @@ import org.cougaar.core.security.securebootstrap.JaasClient;
 
 public class JaasAgentBinder
   extends BinderWrapper
-  implements AgentManagerForBinder
+  implements AgentManagerForBinder, AgentBinder
 {
   private ServiceBroker serviceBroker;
   private LoggingService log;
@@ -53,73 +54,104 @@ public class JaasAgentBinder
   public JaasAgentBinder(BinderFactory bf, Object child) {
     super(bf,child);
   }
-    //child binder
-    protected final AgentBinder getAgentBinder() { 
-        return (AgentBinder)getChildBinder(); 
-    }    
-    //parent
-    protected final AgentManagerForBinder getAgentManager() { 
-        return (AgentManagerForBinder)getContainer(); 
-    }    
+
+  /* ********************************************************************************
+   * AgentBinder interface
+   */
+  /**
+   * Get the agent's message address.
+   */
+  public MessageAddress getAgentIdentifier() {
+    AgentBinder ab = (AgentBinder) getChildBinder();
+    MessageAddress ret = ab.getAgentIdentifier();
+    if (log.isDebugEnabled()) {
+      log.debug("Agent "+ret+" wrapper: get agent-id from binder "+ab);
+    }
+    return ret;
+  }
+
+  /**
+   * Obtain direct access to the agent.
+   * <p>
+   * This method may be removed from the binder API due to
+   * security concerns.
+   */
+  public Agent getAgent() {
+    AgentBinder ab = (AgentBinder) getChildBinder();
+    MyypyypessageAddress addr = ab.getAgentIdentifier();
+    Agent ret = ab.getAgent();
+    if (log.isDebugEnabled()) {
+      log.debug("Agent "+addr+" wrapper: get agent from binder "+ab);
+    }
+    return ret;
+  }
+  
+  /* ********************************************************************************
+   * End AgentBinder interface
+   */
+
+  //child binder
+  protected final AgentBinder getAgentBinder() { 
+    return (AgentBinder)getChildBinder(); 
+  }    
+  //parent
+  protected final AgentManagerForBinder getAgentManager() { 
+    return (AgentManagerForBinder)getContainer(); 
+  }    
     
    
-    public String toString() {
-        return "JaasAgentBinder for "+getAgentManager();
-    }
-    public String getName() {return getAgentManager().getName(); }
-/*    
-    public void initialize() {
-        log.debug("#####test:"+getAgentBinder().toString());
-        super.initialize();
-    }
-*/
-    private String getAgentName(){
-        String tmp = getAgentBinder().toString();
-        return tmp.substring(9, tmp.indexOf('>'));
-    }
+  public String toString() {
+    return "JaasAgentBinder for "+getAgentManager();
+  }
+  public String getName() {return getAgentManager().getName(); }
 
-    private void doLoad() { super.load();}
+  private String getAgentName(){
+    String tmp = getAgentBinder().toString();
+    return tmp.substring(9, tmp.indexOf('>'));
+  }
 
-    private void doStart() { super.start();}
-
-    public void load() {
-      log = (LoggingService)
-	getServiceBroker().getService(this,
-				      LoggingService.class, null);
-
-        JaasClient jc = new JaasClient();
-        jc.doAs(getAgentName(),
+  private void doLoad() { super.load();}
+  
+  private void doStart() { super.start();}
+  
+  public void load() {
+    log = (LoggingService)
+      getServiceBroker().getService(this,
+				    LoggingService.class, null);
+    
+    JaasClient jc = new JaasClient();
+    jc.doAs(getAgentName(),
             new java.security.PrivilegedAction() {
                 public Object run() {
                   log.debug("Agent manager is loading: "
-                                     + getAgentName()
-                                     + " security context is:");
+			    + getAgentName()
+			    + " security context is:");
                   JaasClient.printPrincipals();
                   doLoad();
                   return null;
                 }
               });
-    }
+  }
 
    
-    public void start() {
-        JaasClient jc = new JaasClient();
-        jc.doAs(getAgentName(),
+  public void start() {
+    JaasClient jc = new JaasClient();
+    jc.doAs(getAgentName(),
             new java.security.PrivilegedAction() {
                 public Object run() {
                   log.debug("Agent manager is starting: "
-                                     + getAgentName()
-                                     + " security context is:");
+			    + getAgentName()
+			    + " security context is:");
                   JaasClient.printPrincipals();
                   doStart();
                   return null;
                 }
               });
-    }
+  }
    
-    public void registerAgent(Agent agent) {
-        //just passing through
-        getAgentManager().registerAgent(agent);
-    }
+  public void registerAgent(Agent agent) {
+    //just passing through
+    getAgentManager().registerAgent(agent);
+  }
     
 }
