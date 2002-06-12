6cf2ff5c66dc027d2296f0b32c9cffe5d9e5b9db
rliao1 <rliao1>
2002-06-12 21:40:26 +0000

    get node information from NodeIdentification service instead of from environment.

--- securityservices/src/org/cougaar/core/security/util/NodeInfo.java
+++ securityservices/src/org/cougaar/core/security/util/NodeInfo.java
@@ -3,25 +3,25 @@
  *  Copyright 1997-2001 Networks Associates Technology, Inc.
  *  under sponsorship of the Defense Advanced Research Projects
  *  Agency (DARPA).
- * 
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the Cougaar Open Source License as published by
- *  DARPA on the Cougaar Open Source Website (www.cougaar.org).  
- *  
- *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS 
- *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR 
- *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF 
- *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT 
- *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT 
- *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL 
- *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS, 
- *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR 
- *  PERFORMANCE OF THE COUGAAR SOFTWARE.  
- * 
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ *
  * </copyright>
  *
  * CHANGE RECORD
- * - 
+ * -
  */
 
 package org.cougaar.core.security.util;
@@ -30,12 +30,19 @@ import java.io.*;
 import java.util.*;
 import java.lang.reflect.Field;
 
+import org.cougaar.core.component.ServiceBroker;
+import org.cougaar.core.node.NodeIdentificationService;
+
 public class NodeInfo
 {
+  private static String nodeName = null;
 
   /** Returns the name of the node. Deals with backward compatibility issues */
   public static String getNodeName()
   {
+    if (nodeName != null)
+      return nodeName;
+
     int major = 0;
     int minor = 0;
     String version = null;
@@ -44,7 +51,7 @@ public class NodeInfo
       Field vf = vc.getField("version");
       version = (String) vf.get(null);
     } catch (Exception e) {}
-    
+
     StringTokenizer st = new StringTokenizer(version, ".");
     for (int i = 0 ; st.hasMoreTokens() ; i++) {
       String s = st.nextToken();
@@ -61,7 +68,6 @@ public class NodeInfo
     // Cougaar 8.4: node name is in org.cougaar.core.society.Node.name
     // Cougaar >=8.6: node name is in org.cougaar.node.name
 
-    String nodeName = "";
     if (major <= 8 && minor <=4) {
       nodeName = System.getProperty("org.cougaar.core.society.Node.name");
     }
@@ -70,5 +76,11 @@ public class NodeInfo
     }
     //System.out.println("Version: " + major + "." + minor + " - Node name:" + nodeName);
     return nodeName;
+  }
+
+  public void setNodeName(ServiceBroker sb) {
+    NodeIdentificationService nis = (NodeIdentificationService)
+      sb.getService(this, NodeIdentificationService.class, null);
+    nodeName = nis.getNodeIdentifier().toString();
   }
 }
