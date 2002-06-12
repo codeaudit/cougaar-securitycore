cc634d22f76a9091e86f9d6334c07797faebb492
gmount <gmount>
2002-06-12 22:57:33 +0000

    Use ant to choose which boot policy to use instead of SecureNode

---
+++ securityservices/test/bin/chooseBootPolicy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+naiaddr=`/sbin/ifconfig eth0 | /bin/egrep 'addr:161.69.57.|addr:162.10.1.'`
+
+if [ "$naiaddr" != "" ] ; then
+    mv $1/BootPolicy.ldm.NAI.xml $1/BootPolicy.ldm.xml
+fi
