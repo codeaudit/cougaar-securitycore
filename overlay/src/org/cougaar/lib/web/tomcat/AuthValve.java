4be9241fec6de3d88e502e3fc8c672afd303d8fd
ahelsing <ahelsing>
2002-06-14 18:05:22 +0000

    update copyright to include 2002

--- overlay/src/org/cougaar/lib/web/tomcat/AuthValve.java
+++ overlay/src/org/cougaar/lib/web/tomcat/AuthValve.java
@@ -1,6 +1,6 @@
 /*
  * <copyright>
- *  Copyright 1997-2001 Networks Associates Technology, Inc.
+ *  Copyright 1997-2002 Networks Associates Technology, Inc.
  *  under sponsorship of the Defense Advanced Research Projects
  *  Agency (DARPA).
  * 
