aa5254a0d20f45af78fe6c9fec280ede4fa4caf9
rliao1 <rliao1>
2002-06-19 21:38:27 +0000

    provider for cert validity service

---
+++ securityservices/src/org/cougaar/core/security/provider/CertValidityServiceProvider.java
@@ -0,0 +1,82 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2001 Networks Associates Technology, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ *
+ * </copyright>
+ *
+ * CHANGE RECORD
+ * -
+ */
+
+package org.cougaar.core.security.provider;
+
+// Cougaar core infrastructure
+import org.cougaar.core.component.*;
+import org.cougaar.util.*;
+import org.cougaar.core.service.LoggingService;
+import org.cougaar.core.component.ServiceBroker;
+
+// Cougaar security services
+import org.cougaar.core.security.services.crypto.*;
+import org.cougaar.core.security.crypto.*;
+
+public class CertValidityServiceProvider
+  implements ServiceProvider {
+
+  static private CertificateValidityMonitor service = null;
+
+  /**
+   * Get a service.
+   * @param sb a Service Broker
+   * @param requestor the requestor of the service
+   * @param serviceClass a Class, usually an interface, which extends Service.
+   * @return a service
+   */
+  public Object getService(ServiceBroker sb,
+			   Object requestor,
+			   Class serviceClass) {
+
+    LoggingService log = (LoggingService)
+      sb.getService(this,
+			       LoggingService.class, null);
+    if (service != null)
+      return service;
+
+    try {
+      service = new CertificateValidityMonitor(sb);
+    }
+    catch (Exception e) {
+      log.debug("Failed to initialize CertificateValidityMonitor! " + e.toString());
+      e.printStackTrace();
+    }
+    return service;
+  }
+
+  /** Release a service.
+   * @param sb a Service Broker.
+   * @param requestor the requestor of the service.
+   * @param serviceClass a Class, usually an interface, which extends Service.
+   * @param service the service to be released.
+   */
+  public void releaseService(ServiceBroker sb,
+			     Object requestor,
+			     Class serviceClass,
+			     Object service) {
+  }
+}
