9a0016f05f0a76ff70541cd90b43af3dcc2dfb76
rtripath <rtripath>
2002-06-26 23:09:29 +0000

    Fix for agent certificate Revocation

--- securityservices/src/org/cougaar/core/security/crlextension/x509/extensions/CertificateIssuerExtension.java
+++ securityservices/src/org/cougaar/core/security/crlextension/x509/extensions/CertificateIssuerExtension.java
@@ -40,55 +40,51 @@ import sun.security.util.ObjectIdentifier;
 import sun.security.x509.Extension;
 import sun.security.x509.CertAttrSet;
 import sun.security.x509.GeneralNames;
+import sun.security.x509.GeneralNameInterface;
 import sun.security.x509.GeneralNamesException;
 import sun.security.x509.AttributeNameEnumeration;
 
 public class CertificateIssuerExtension extends Extension
-  implements CertAttrSet
-{
-
+  implements CertAttrSet {
+  
   private void encodeThis()
-    throws IOException
-  {
-    if(issuerName == null || issuerName.isEmpty())
-      {
-	super.extensionValue = null;
-	return;
-      }
+    throws IOException {
+    if(issuerName == null || issuerName.isEmpty()) {
+      super.extensionValue = null;
+      return;
+    }
+    System.out.println(" in encode of CertificateIssuerExtension");
     DerOutputStream deroutputstream = new DerOutputStream();
-    try
-      {
-	
-	issuerName.encode(deroutputstream);
-      }
-    catch(GeneralNamesException generalnamesexception)
-      {
-	throw new IOException(generalnamesexception.toString());
-      }
+    try {
+      issuerName.encode(deroutputstream);
+    }
+    catch(GeneralNamesException generalnamesexception)  {
+      generalnamesexception.printStackTrace();
+      throw new IOException(generalnamesexception.toString());
+    }
     super.extensionValue = deroutputstream.toByteArray();
   }
 
-  public CertificateIssuerExtension(GeneralNames generalnames)
-    throws IOException
-  {
+  public CertificateIssuerExtension(CougaarGeneralNames generalnames)
+    throws IOException {
     issuerName = null;
     issuerName = generalnames;
+    //System.out.println(" In constructor of CertificateIssuerExtension:");
     super.extensionId=new ObjectIdentifier(certIssuerOID);
     super.critical = false;
     encodeThis();
   }
 
-  public CertificateIssuerExtension()  throws IOException
-  {
+  public CertificateIssuerExtension()  
+    throws IOException {
     issuerName = null;
     super.extensionId=new ObjectIdentifier(certIssuerOID);
     super.critical = false;
-    issuerName = new GeneralNames();
+    issuerName = new CougaarGeneralNames();
   }
 
   public CertificateIssuerExtension(Boolean boolean1, Object obj)
-    throws IOException
-  {
+    throws IOException  {
     issuerName = null;
     super.extensionId =new ObjectIdentifier(certIssuerOID);
     super.critical = boolean1.booleanValue();
@@ -99,38 +95,33 @@ public class CertificateIssuerExtension extends Extension
 
     super.extensionValue = abyte0;
     DerValue dervalue = new DerValue(abyte0);
-    try
-      {
-	  
-	issuerName = new GeneralNames(dervalue);
-      }
-    catch(GeneralNamesException generalnamesexception)
-      {
-	throw new IOException("IssuerAlternativeNameExtension" + generalnamesexception.toString());
-      }
+    try {
+      issuerName = new CougaarGeneralNames(dervalue);
+    }
+    catch(GeneralNamesException generalnamesexception){
+      throw new IOException("IssuerAlternativeNameExtension" + generalnamesexception.toString());
+    }
   }
 
-  public String toString()
-  {
-    if(issuerName == null)
-      {
-	return "";
-      } else
-        {
-	  StringBuffer buffer=new StringBuffer(); 
-	  buffer.append( super.toString()); 
-	  buffer.append( "Certificate Issuer Name [\n" + issuerName.toString() + "]\n");
-	  return buffer.toString();
-        }
+  public String toString()  {
+    if(issuerName == null)  {
+      return "";
+    } 
+    else  {
+      StringBuffer buffer=new StringBuffer(); 
+      buffer.append( super.toString()); 
+      buffer.append( "Certificate Issuer Name [\n" + issuerName.toString() + "]\n");
+      return buffer.toString();
+    }
   }
 
   public void set(String s, Object obj)
-    throws IOException
-  {
+    throws IOException  {
     if(s.equalsIgnoreCase(ISSUERNAME))  {
-      if(!(obj instanceof GeneralNames))
+      if(!(obj instanceof CougaarGeneralNames)) {
 	throw new IOException("Attribute value should be of type GeneralNames.");
-      issuerName = (GeneralNames)obj;
+      }
+      issuerName = (CougaarGeneralNames)obj;
     }
     else  {
       throw new IOException("Attribute name not recognized by CertAttrSet:certificateIssuer.");
@@ -139,18 +130,16 @@ public class CertificateIssuerExtension extends Extension
   }
 
   public Object get(String s)
-    throws IOException
-  {
+    throws IOException {
     if(s.equalsIgnoreCase(ISSUERNAME))
       return issuerName;
     else
       throw new IOException("Attribute name not recognized by CertAttrSet:certificateIssuer.");
-
+    
   }
 
   public void delete(String s)
-    throws IOException
-  {
+    throws IOException {
     if(s.equalsIgnoreCase(ISSUERNAME))
       issuerName=null;
     else
@@ -158,15 +147,14 @@ public class CertificateIssuerExtension extends Extension
     encodeThis();
   }
 
-  public Enumeration getElements()
-  {
+  public Enumeration getElements()  {
     AttributeNameEnumeration attributenameenumeration = new AttributeNameEnumeration();
     attributenameenumeration.addElement(ISSUERNAME);
     return attributenameenumeration.elements();
   }
+  
   public void encode(OutputStream outputstream)
-    throws IOException
-  {
+    throws IOException  {
     DerOutputStream deroutputstream = new DerOutputStream();
     if(super.extensionValue == null)
       {
@@ -179,15 +167,12 @@ public class CertificateIssuerExtension extends Extension
   }
   
   public void decode(InputStream inputstream)
-    throws IOException
-  {
+    throws IOException  {
     throw new IOException("Method not to be called directly.");
   }
 
-  public String getName()
-  {
+  public String getName()  {
     return  "CertificateIssuer";
-
   }
   
 
@@ -195,7 +180,7 @@ public class CertificateIssuerExtension extends Extension
   public final String certIssuerOID="2.5.29.29";  
   public static final String NAME ="CertificateIssuer";
   public static final String ISSUERNAME="certificateIssuer"; 
-  private GeneralNames issuerName=null; 
+  private CougaarGeneralNames issuerName=null; 
 
   
 }
