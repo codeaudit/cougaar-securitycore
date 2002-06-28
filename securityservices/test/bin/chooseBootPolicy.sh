c8dce75fa728a6ef191f19a47a6818289a1ac359
gmount <gmount>
2002-06-28 22:57:40 +0000

    Boot policies have been moved out of BootPolicy.ldm.xml. Now the difference between the TIC and NAI are encapsulated in BootPolicy.UserDB*.xml

--- securityservices/test/bin/chooseBootPolicy.sh
+++ securityservices/test/bin/chooseBootPolicy.sh
@@ -2,6 +2,6 @@
 
 naiaddr=`/sbin/ifconfig eth0 | /bin/egrep 'addr:161.69.57.|addr:162.10.1.'`
 
-if [ "$naiaddr" != "" ] ; then
-    mv $1/BootPolicy.ldm.NAI.xml $1/BootPolicy.ldm.xml
+if [ "$naiaddr" == "" ] ; then
+    mv $1/BootPolicy.UserDB.TIC.xml $1/BootPolicy.UserDB.xml
 fi
