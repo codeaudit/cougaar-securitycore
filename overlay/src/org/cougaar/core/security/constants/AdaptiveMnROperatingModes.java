7adf6b3b453c20c7f3ec3dbcf67fae2ed5db5f75
mluu <mluu>
2002-07-11 18:41:38 +0000

    centralize operating mode and idmef classification constants

---
+++ overlay/src/org/cougaar/core/security/constants/AdaptiveMnROperatingModes.java
@@ -0,0 +1,69 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2002 Networks Associates Technology, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ *
+ * </copyright>
+ *
+ * CHANGE RECORD
+ * -
+ */
+ 
+package org.cougaar.core.security.constants;
+
+/**
+ * This class specifies the operating modes subscribed by various sensors
+ * and analyzers developed by the Ultra*Log project teams.  These operating
+ * modes are also used to define plays in an adaptivity playbook.
+ *
+ * Place all operating mode name constants in this class.
+ */
+public class AdaptiveMnROperatingModes {
+  /**
+   * number of login failures before locking out a user
+   * possible value: { N, where N is a number }
+   */
+  public final static String MAX_LOGIN_FAILURES = 
+    "org.cougaar.core.security.monitoring.MAX_LOGIN_FAILURES";
+  /**
+   * the lockout duration
+   * possible value: { N, where N is a number in minutes }
+   */
+  public final static String LOCKOUT_DURATION = 
+    "org.cougaar.core.security.monitoring.LOCKOUT_DURATION";
+  /**
+   * policy preventive measure
+   * possible values: { LOW | HIGH }
+   */
+  public final static String PREVENTIVE_MEASURE_POLICY = 
+    "org.cougaar.core.security.policy.PREVENTIVE_MEASURE_POLICY";
+  /**
+   * telcordia's adaptive filter scope
+   * possible values: 
+   * { LOGIN_FAILURES | LOGIN_JAR_SECURITYMGR_FAILURES | 
+   *   LOGIN_JAR_SECURITYMGR_CRYPTO_FAILURES }
+   */
+  public final static String ADAPTIVE_FILTER_SCOPE = 
+    "com.telcordia.mode.AdaptiveFilterOperatingModeScope";
+  /**
+   *  telcordia's adaptive filter reporting rate
+   *  possible values: { MODERATE | RAPID }
+   */
+  public final static String ADAPTIVE_FILTER_REPORTING_RATE = 
+    "com.telcordia.mode.AdaptiveFilterOperatingModeReportingRate";
+}
---
+++ overlay/src/org/cougaar/core/security/constants/IdmefClassifications.java
@@ -0,0 +1,59 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2002 Networks Associates Technology, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ *
+ * </copyright>
+ *
+ * CHANGE RECORD
+ * -
+ */
+package org.cougaar.core.security.constants;
+
+/**
+ * This class defines all of the Idmef classification constants.
+ *
+ * Place all Idmef classification name constants here.
+ */
+public class IdmefClassifications {
+  /**
+   * cougaar classification name prefix
+   */
+  public final static String COUGAAR_PREFIX = "org.cougaar.core.security.monitoring.";
+  /**
+   * message failure
+   * @see MessageFailureEvent
+   */
+  public final static String MESSAGE_FAILURE = 
+    COUGAAR_PREFIX + "MESSAGE_FAILURE";
+  /**
+   * login failure
+   */
+  public final static String LOGIN_FAILURE = 
+    COUGAAR_PREFIX + "LOGIN_FAILURE";
+  /**
+   * security manager exception
+   */
+  public final static String SECURITY_MANAGER_EXCEPTION = 
+    COUGAAR_PREFIX + "SECURITY_MANAGER_EXCEPTION";
+  /**
+   * jar verification failure
+   */
+  public final static String JAR_VERIFICATION_FAILURE = 
+    COUGAAR_PREFIX + "JAR_VERIFICATION_FAILURE";
+}
