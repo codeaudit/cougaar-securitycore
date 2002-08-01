05fe0a6f96457228d60684e4bef1d9ea1b551c9c
gmount <gmount>
2002-08-01 23:30:54 +0000

    Now it creates an assessment IDMEF message.

--- securityservices/src/org/cougaar/core/security/monitoring/plugin/UserLockoutPlugin.java
+++ securityservices/src/org/cougaar/core/security/monitoring/plugin/UserLockoutPlugin.java
@@ -21,7 +21,10 @@
 package org.cougaar.core.security.monitoring.plugin;
 
 import org.cougaar.util.UnaryPredicate;
+import org.cougaar.multicast.AttributeBasedAddress;
 import org.cougaar.core.agent.ClusterIdentifier;
+import org.cougaar.core.service.community.CommunityService;
+import org.cougaar.core.service.AgentIdentificationService;
 import org.cougaar.core.service.LoggingService;
 import org.cougaar.core.service.DomainService;
 import org.cougaar.core.service.BlackboardService;
@@ -34,26 +37,39 @@ import org.cougaar.core.adaptivity.OMCRange;
 import org.cougaar.core.adaptivity.OMCRangeList;
 import org.cougaar.core.adaptivity.OMCThruRange;
 import org.cougaar.core.blackboard.IncrementalSubscription;
+import org.cougaar.core.mts.MessageAddress;
 import org.cougaar.core.security.services.crypto.LdapUserService;
 import org.cougaar.core.security.crypto.ldap.KeyRingJNDIRealm;
 import org.cougaar.core.security.monitoring.idmef.IdmefMessageFactory;
+import org.cougaar.core.security.monitoring.idmef.RegistrationAlert;
 import org.cougaar.core.security.monitoring.blackboard.Event;
 import org.cougaar.core.security.monitoring.blackboard.MRAgentLookUp;
 import org.cougaar.core.security.monitoring.blackboard.CmrRelay;
 import org.cougaar.core.security.monitoring.blackboard.MRAgentLookUpReply;
 import org.cougaar.core.security.monitoring.blackboard.CmrFactory;
+import org.cougaar.core.security.monitoring.blackboard.NewEvent;
+import org.cougaar.core.security.monitoring.plugin.SensorInfo;
 
 // Cougaar overlay
 import org.cougaar.core.security.constants.IdmefClassifications;
 import org.cougaar.core.security.constants.AdaptiveMnROperatingModes;
+import org.cougaar.core.security.constants.IdmefAssessments;
 
 import edu.jhuapl.idmef.Target;
 import edu.jhuapl.idmef.Classification;
 import edu.jhuapl.idmef.IDMEF_Message;
+import edu.jhuapl.idmef.IDMEF_Node;
+import edu.jhuapl.idmef.IDMEF_Process;
+import edu.jhuapl.idmef.Source;
 import edu.jhuapl.idmef.Alert;
 import edu.jhuapl.idmef.AdditionalData;
 import edu.jhuapl.idmef.User;
 import edu.jhuapl.idmef.UserId;
+import edu.jhuapl.idmef.DetectTime;
+import edu.jhuapl.idmef.Assessment;
+import edu.jhuapl.idmef.Impact;
+import edu.jhuapl.idmef.Confidence;
+import edu.jhuapl.idmef.Action;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -65,6 +81,8 @@ import java.util.NoSuchElementException;
 import java.util.TimerTask;
 import java.util.Enumeration;
 import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
 
 /**
  * This class queries login failures and will lockout users who
@@ -91,12 +109,31 @@ public class UserLockoutPlugin extends ComponentPlugin {
 
   FailureCache _failures       = new FailureCache();
   private LoggingService  _log;
+  private DomainService  _domainService;
   private LdapUserService _userService;
   private IncrementalSubscription _maxLoginFailureSubscription;
   private IncrementalSubscription _lockoutDurationSubscription;
 
   private OperatingMode _maxLoginFailureOM = null;
   private OperatingMode _lockoutDurationOM = null;
+  private static final SensorInfo _sensor = new ULSensor();
+  private IdmefMessageFactory  _idmefFactory;
+  private CmrFactory _cmrFactory;
+  private String         _managerRole   = "SecurityMnRManager-Enclave";
+
+  private final static Action[] USER_LOCKOUT_ACTION = new Action[] {
+    new Action(Action.OTHER, IdmefAssessments.USER_LOCKOUT)
+  };
+  private final static Confidence USER_LOCKOUT_CONFIDENCE = 
+    new Confidence(Confidence.MEDIUM, null);
+  public static final Classification LOGIN_FAILURE = 
+    new Classification(IdmefClassifications.LOGIN_FAILURE, "", 
+                       Classification.VENDOR_SPECIFIC);
+  private final static Assessment USER_LOCKOUT_ASSESSMENT = 
+    new Assessment(new Impact(Impact.MEDIUM, Impact.SUCCEEDED,
+                              Impact.USER, IdmefAssessments.USER_LOCKOUT),
+                   USER_LOCKOUT_ACTION,
+                   USER_LOCKOUT_CONFIDENCE);
 
   /**
    * Subscription to the login failures on the local blackboard
@@ -122,7 +159,7 @@ public class UserLockoutPlugin extends ComponentPlugin {
                   if (ad != null) {
                     for (int j = 0; j < ad.length; j++) {
                       if (KeyRingJNDIRealm.FAILURE_REASON.equals(ad[j].getMeaning())) {
-                        return ("the user has entered the wrong password".equals(ad[j].getAdditionalData()));
+                        return (KeyRingJNDIRealm.FAILURE_REASONS[KeyRingJNDIRealm.LF_PASSWORD_MISMATCH].equals(ad[j].getAdditionalData()));
                       }
                     }
                   }
@@ -176,6 +213,42 @@ public class UserLockoutPlugin extends ComponentPlugin {
     };
 
   /**
+   * Used by the binding utility through reflection to set my DomainService
+   */
+  public void setDomainService(DomainService aDomainService) {
+    _domainService = aDomainService;
+  }
+
+  /**
+   * Used by the binding utility through reflection to get my DomainService
+   */
+  public DomainService getDomainService() {
+    return _domainService;
+  }
+
+  /**
+   * Produce the Assessment alert
+   */
+  private void alertAssessment(String user) {
+    ArrayList cfs = new ArrayList();
+    cfs.add(LOGIN_FAILURE);
+    Alert alert = _idmefFactory.createAlert(_sensor, new DetectTime(),
+                                            null, null, cfs, null);
+    
+    // set the target
+    UserId uid = new UserId( user, null, null, UserId.TARGET_USER );
+    UserId uids[] = new UserId[] { uid };
+    User u = new User( uids, null, User.UNKNOWN );
+    Target t = new Target(null, u, null, null, null, null,
+                          Target.UNKNOWN, null);
+    alert.setTargets( new Target[] {t} );
+    alert.setAssessment(USER_LOCKOUT_ASSESSMENT);
+    NewEvent event = _cmrFactory.newEvent(alert);
+
+    getBlackboardService().publishAdd(event);
+  }
+
+  /**
    * For the lockout duration OperatingMode
    */
   private static final UnaryPredicate LOCKOUT_DURATION_PREDICATE = 
@@ -213,6 +286,7 @@ public class UserLockoutPlugin extends ComponentPlugin {
       paramName = "failure memory";
       param = iter.next().toString();
       _rememberTime = Long.parseLong(param) * 1000;
+      
     } catch (NoSuchElementException e) {
       throw new IllegalArgumentException("You must provide a " +
                                         paramName +
@@ -228,13 +302,23 @@ public class UserLockoutPlugin extends ComponentPlugin {
                                          "clean interval and failure memory " +
                                          "arguments");
     }
+    if (iter.hasNext()) {
+      _managerRole = (String) iter.next();
+    } // end of if (iter.hasNext())
+    
   }
 
   /**
    * Lockout a given user for the lockout duration
    */
   public void lock(String user) throws NamingException {
-    _log.debug("locking out user (" + user + ")");
+    if (_log.isDebugEnabled()) {
+      _log.debug("locking out user (" + user + ")");
+    } // end of if (_log.isDebugEnabled())
+
+    // send an IDMEF alert
+    alertAssessment(user);
+    
     if (_lockoutTime < 0) {
       _userService.disableUser(user);
     } else {
@@ -272,6 +356,77 @@ public class UserLockoutPlugin extends ComponentPlugin {
       getService(this, ThreadService.class, null);
     ts.schedule(_failures,
                 0, ((long)_cleanInterval) * 1000);
+
+    setupFailureSensor();
+  }
+
+  private void setupFailureSensor() {
+    BlackboardService    bbs          = getBlackboardService();
+    DomainService        ds           = getDomainService(); 
+    _cmrFactory                       = (CmrFactory) ds.getFactory("cmr");
+    _idmefFactory                     = _cmrFactory.getIdmefMessageFactory();
+    ServiceBroker        sb           = getBindingSite().getServiceBroker();
+    AgentIdentificationService ais    = (AgentIdentificationService)
+      sb.getService(this, AgentIdentificationService.class, null);
+    String               agentName    = ais.getName();
+    MessageAddress       myAddress    = ais.getMessageAddress();
+    CommunityService     cs           = (CommunityService)
+      sb.getService(this, CommunityService.class,null);
+
+    if (cs == null) {
+      _log.error("You must have CommunityPlugin in this agent for the UserLockoutPlugin to work");
+    } // end of if (cs == null)
+    
+    List capabilities = new ArrayList();
+    capabilities.add(KeyRingJNDIRealm.LOGINFAILURE);
+      
+    RegistrationAlert reg = 
+      _idmefFactory.createRegistrationAlert( _sensor, capabilities,
+                                             _idmefFactory.newregistration ,
+                                             _idmefFactory.SensorType );
+    
+    IDMEF_Node node = _idmefFactory.getNodeInfo();
+    IDMEF_Process process = _idmefFactory.getProcessInfo();
+    Source source = new Source(node, null, process, null, null, null, null);
+    reg.setSources(new Source[] { source });
+    
+    NewEvent regEvent = _cmrFactory.newEvent(reg);
+    Collection communities = cs.listParentCommunities(agentName);
+    Iterator iter = communities.iterator();
+    boolean addedOne = false;
+    while (iter.hasNext()) {
+      String community = iter.next().toString();
+      Attributes attrs = cs.getCommunityAttributes(community);
+      boolean isSecurityCommunity = false;
+      if (attrs != null) {
+        Attribute  attr  = attrs.get("CommunityType");
+        if (attr != null) {
+          try {
+            for (int i = 0; !isSecurityCommunity && i < attr.size(); i++) {
+              if ("Security".equals(attr.get(i).toString())) {
+                isSecurityCommunity = true;
+              }
+            }
+          } catch (NamingException e) {
+            // error reading value, so it can't be a Security community
+          }
+        }
+      }
+      if (isSecurityCommunity) {
+        AttributeBasedAddress messageAddress = 
+          new AttributeBasedAddress(community, "Role", _managerRole);
+        CmrRelay relay = _cmrFactory.newCmrRelay(regEvent, messageAddress);
+        if (_log.isInfoEnabled()) {
+          _log.info("Sending sensor capabilities to community '" + 
+                    community + "'");
+        }
+        bbs.publishAdd(relay);
+        addedOne = true;
+      }
+    }
+    if (!addedOne) {
+      _log.warn("This agent does not belong to any community. Login failures won't be reported.");
+    }
   }
 
   public void execute() {
@@ -384,5 +539,28 @@ public class UserLockoutPlugin extends ComponentPlugin {
   protected static class CacheNode {
     int  failureCount = 0;
     long lastFailure;
+  }
+
+  private static class ULSensor implements SensorInfo {
+
+    public String getName() {
+      return "User Lockout Analyzer";
+    }
+
+    public String getManufacturer() {
+      return "NAI Labs";
+    }
+
+    public String getModel() {
+      return "Servlet Login Failure";
+    }
+    
+    public String getVersion() {
+      return "1.0";
+    }
+
+    public String getAnalyzerClass() {
+      return "org.cougaar.core.security.monitoring.plugin.UserLockoutPlugin";
+    }
   }
 }
