3c0c64e39ddffa917a451b63c13c48eabfff9bcf
srosset <srosset>
2002-08-10 00:15:32 +0000

    *** empty log message ***

--- securityservices/test/configs/recipes/MnREventQueryPluginMessageFailures3-export.sql
+++ securityservices/test/configs/recipes/MnREventQueryPluginMessageFailures3-export.sql
@@ -23,7 +23,7 @@ REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VAL
 REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-MnREventQueryPluginMessageFailures3','Number of Arguments',0.000000000000000000000000000000,'2');
 REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-MnREventQueryPluginMessageFailures3','Target Component Selection Query',2.000000000000000000000000000000,'recipeQueryEnclave3SecurityMnRAgent');
 REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-MnREventQueryPluginMessageFailures3','Type of Insertion',4.000000000000000000000000000000,'plugin');
-REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-MnREventQueryPluginMessageFailures3','Value 1',6.000000000000000000000000000000,'Enclave2SecurityMnRManager');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-MnREventQueryPluginMessageFailures3','Value 1',6.000000000000000000000000000000,'Enclave3SecurityMnRManager');
 REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-MnREventQueryPluginMessageFailures3','Value 2',7.000000000000000000000000000000,'org.cougaar.core.security.monitoring.plugin.AllMessageFailures');
 UNLOCK TABLES;
 
