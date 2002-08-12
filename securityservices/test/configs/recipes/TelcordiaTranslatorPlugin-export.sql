53203cbcea064d203f02a375b2f061fc6cc3270c
art <art>
2002-08-12 18:20:59 +0000

    replacing component collection with old style recipes

---
+++ securityservices/test/configs/recipes/TelcordiaTranslatorPlugin-export.sql
@@ -0,0 +1,144 @@
+-- MySQL dump 8.21
+--
+-- Host: localhost    Database: tempcopy
+---------------------------------------------------------
+-- Server version	3.23.49-nt
+
+--
+-- Dumping data for table 'alib_component'
+--
+
+
+LOCK TABLES alib_component WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'asb_agent'
+--
+
+
+LOCK TABLES asb_agent WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'asb_agent_pg_attr'
+--
+
+
+LOCK TABLES asb_agent_pg_attr WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'asb_agent_relation'
+--
+
+
+LOCK TABLES asb_agent_relation WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'asb_assembly'
+--
+
+
+LOCK TABLES asb_assembly WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'asb_component_arg'
+--
+
+
+LOCK TABLES asb_component_arg WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'asb_component_hierarchy'
+--
+
+
+LOCK TABLES asb_component_hierarchy WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'asb_oplan'
+--
+
+
+LOCK TABLES asb_oplan WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'asb_oplan_agent_attr'
+--
+
+
+LOCK TABLES asb_oplan_agent_attr WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'community_attribute'
+--
+
+
+LOCK TABLES community_attribute WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'community_entity_attribute'
+--
+
+
+LOCK TABLES community_entity_attribute WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'lib_agent_org'
+--
+
+
+LOCK TABLES lib_agent_org WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'lib_component'
+--
+
+
+LOCK TABLES lib_component WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'lib_mod_recipe'
+--
+
+
+LOCK TABLES lib_mod_recipe WRITE;
+REPLACE INTO lib_mod_recipe (MOD_RECIPE_LIB_ID, NAME, JAVA_CLASS, DESCRIPTION) VALUES ('RECIPE-0021TelcordiaTranslatorPlugin','TelcordiaTranslatorPlugin-cpy','org.cougaar.tools.csmart.recipe.SpecificInsertionRecipe','No description available');
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'lib_mod_recipe_arg'
+--
+
+
+LOCK TABLES lib_mod_recipe_arg WRITE;
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0021TelcordiaTranslatorPlugin','Class Name',3.000000000000000000000000000000,'com.telcordia.adFilterTranslator.TranslatorPlugin');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0021TelcordiaTranslatorPlugin','Component Name',6.000000000000000000000000000000,'com.telcordia.adFilterTranslator.TranslatorPlugin');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0021TelcordiaTranslatorPlugin','Component Priority',8.000000000000000000000000000000,'COMPONENT');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0021TelcordiaTranslatorPlugin','Number of Arguments',1.000000000000000000000000000000,'3');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0021TelcordiaTranslatorPlugin','Target Component Selection Query',5.000000000000000000000000000000,'recipeQuerySecurityEnclaveManagerAgents');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0021TelcordiaTranslatorPlugin','Type of Insertion',7.000000000000000000000000000000,'plugin');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0021TelcordiaTranslatorPlugin','Value 1',4.000000000000000000000000000000,'REAL');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0021TelcordiaTranslatorPlugin','Value 2',2.000000000000000000000000000000,'SecurityManager');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0021TelcordiaTranslatorPlugin','Value 3',0.000000000000000000000000000000,'AdFilter1');
+UNLOCK TABLES;
+
+--
+-- Dumping data for table 'lib_pg_attribute'
+--
+
+
+LOCK TABLES lib_pg_attribute WRITE;
+UNLOCK TABLES;
+
