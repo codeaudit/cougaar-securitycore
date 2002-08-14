2efbe6a64f0eeddd8ba7c7e33671499b1f47f329
art <art>
2002-08-14 14:23:46 +0000

    Simpler startup script

--- securityservices/test/bin/securityconsole/seccon.sh
+++ securityservices/test/bin/securityconsole/seccon.sh
@@ -21,60 +21,29 @@
 
 node="umseccon"
 
-LIBPATHS="$COUGAAR_INSTALL_PATH/lib/overlay.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/core.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/util.jar"
 
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/bootstrap.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/securebootstrapper.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/CougaarCRLextensions.jar"
-
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/planserver.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/glm.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/aggagent.jar"
-
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/idmef.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/securityservices.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/kaos.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/safe.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/jas.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/mail.jar"
-
-
-
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/jcchart451K.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/xerces.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/servlet.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/jpython.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/junit.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/log4j.jar"
-
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/xml4j.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/xmlparserv2.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/ibmpkcs.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/jce1_2_1.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/servlet.jar"
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/sys/tomcat_40.jar"
-
-LIBPATHS="$LIBPATHS:$COUGAAR_INSTALL_PATH/lib/umemphis.jar"
-
+PROPFILE="$COUGAAR_INSTALL_PATH/configs/security/data/MainApplication.properties"
+# Search and replace properties to customize for current environment
+${COUGAAR_SECURITY_SERVICES}/test/bin/sarep -v '\.\./\.\./' ${COUGAAR_INSTALL_PATH}/configs/security/  ${PROPFILE}
 
 MYDOMAINS=""
+MYMEMORY="-Xms384m -Xmx448m"
+
+MYCONFIGPATH="-Dorg.cougaar.config.path=$COUGAAR_INSTALL_PATH/csmart/data/common/\;$COUGAAR_INSTALL_PATH/configs/\;"
+
+MYCLASSPATH="${COUGAAR_INSTALL_PATH}/lib/bootstrap.jar"
 
 MYPROPERTIES="$MYDOMAINS  -Dorg.cougaar.install.path=$COUGAAR_INSTALL_PATH -Duser.timezone=GMT -Dorg.cougaar.core.cluster.startTime=08/10/2005 -Dorg.cougaar.domain.planning.ldm.lps.ComplainingLP.level=0 -Dorg.cougaar.core.cluster.SharedPlugInManager.watching=false"
-
 MYPROPERTIES="$MYPROPERTIES -Dorg.cougaar.security.crypto.config=cryptoPolicy.xml"
 MYPROPERTIES="$MYPROPERTIES -Dorg.cougaar.security.role=$USER"
 MYPROPERTIES="$MYPROPERTIES -Dorg.cougaar.workspace=$COUGAAR_INSTALL_PATH/workspace"
 MYPROPERTIES="$MYPROPERTIES -Dorg.cougaar.node.name=$node"
 MYPROPERTIES="$MYPROPERTIES -Dorg.cougaar.core.security.standalone=true"
-
 MYPROPERTIES="$MYPROPERTIES -Dorg.cougaar.core.logging.config.filename=loggingConfig.conf"
 MYPROPERTIES="$MYPROPERTIES -Dorg.cougaar.core.logging.log4j.appender.SECURITY.File=kpat.log"
+MYPROPERTIES="$MYPROPERTIES -Dlog4j.configuration=$COUGAAR_INSTALL_PATH/configs/security/data/MainApp-log4j.properties"
 
+JAVAARGS="$MYPROPERTIES $MYMEMORY $MYCONFIGPATH -cp $MYCLASSPATH"
 
-PROPFILE="$COUGAAR_INSTALL_PATH/configs/security/data/MainApplication.properties"
-${COUGAAR_SECURITY_SERVICES}/test/bin/sarep -v '\.\./\.\./' ${COUGAAR_INSTALL_PATH}/configs/security/  ${PROPFILE}
-JAVARGS="$MYPROPERTIES  -Dlog4j.configuration=$COUGAAR_INSTALL_PATH/configs/security/data/MainApp-log4j.properties"
-
-exec java $JAVARGS -cp $LIBPATHS edu.memphis.issrl.snapingui.MainApplication $PROPFILE
+#echo exec java $JAVAARGS org.cougaar.bootstrap.Bootstrapper edu.memphis.issrl.snapingui.MainApplication $PROPFILE
+exec java $JAVAARGS org.cougaar.bootstrap.Bootstrapper edu.memphis.issrl.snapingui.MainApplication $PROPFILE
