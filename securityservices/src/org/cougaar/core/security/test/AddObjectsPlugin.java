8c546d8262b6d9fed85de0759cd7e2ff6f8e1397
gmount <gmount>
2002-08-20 18:14:28 +0000

    test to add objects to blackboard

---
+++ securityservices/src/org/cougaar/core/security/test/AddObjectsPlugin.java
@@ -0,0 +1,161 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2001 Network Associates
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+package org.cougaar.core.security.test;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.Enumeration;
+import java.util.Collection;
+import java.util.HashSet;
+
+import java.io.Serializable;
+import java.io.StringReader;
+import java.io.IOException;
+
+import edu.jhuapl.idmef.IDMEFTime;
+import edu.jhuapl.idmef.Classification;
+import edu.jhuapl.idmef.IDMEF_Message;
+import edu.jhuapl.idmef.Alert;
+
+// Cougaar core services
+import org.cougaar.core.mts.MessageAddress;
+
+import org.cougaar.core.service.LoggingService;
+import org.cougaar.core.service.ThreadService;
+import org.cougaar.core.service.DomainService;
+import org.cougaar.core.service.AgentIdentificationService;
+import org.cougaar.core.service.BlackboardService;
+import org.cougaar.core.service.community.CommunityService;
+
+import org.cougaar.core.util.UID;
+import org.cougaar.core.component.ServiceBroker;
+import org.cougaar.core.blackboard.IncrementalSubscription;
+import org.cougaar.core.plugin.ComponentPlugin;
+import org.cougaar.core.adaptivity.OMCRangeList;
+import org.cougaar.core.adaptivity.Condition;
+import org.cougaar.core.agent.ClusterIdentifier;
+
+import org.cougaar.core.security.crypto.ldap.KeyRingJNDIRealm;
+import org.cougaar.core.security.monitoring.idmef.IdmefMessageFactory;
+import org.cougaar.core.security.monitoring.idmef.RegistrationAlert;
+import org.cougaar.core.security.monitoring.blackboard.Event;
+import org.cougaar.core.security.monitoring.blackboard.CmrFactory;
+import org.cougaar.core.security.monitoring.blackboard.MRAgentLookUp;
+import org.cougaar.core.security.monitoring.blackboard.CmrRelay;
+import org.cougaar.core.security.monitoring.blackboard.MRAgentLookUpReply;
+import org.cougaar.core.security.monitoring.blackboard.EventImpl;
+
+// Cougaar overlay
+import org.cougaar.core.security.constants.IdmefClassifications;
+
+import org.cougaar.multicast.AttributeBasedAddress;
+import org.cougaar.util.UnaryPredicate;
+import org.cougaar.util.ConfigFinder;
+
+import org.cougaar.lib.aggagent.query.AlertDescriptor;
+import org.cougaar.lib.aggagent.query.AggregationQuery;
+import org.cougaar.lib.aggagent.query.QueryResultAdapter;
+import org.cougaar.lib.aggagent.query.ResultSetDataAtom;
+import org.cougaar.lib.aggagent.query.ScriptSpec;
+import org.cougaar.lib.aggagent.query.AggregationResultSet;
+
+import org.cougaar.lib.aggagent.session.UpdateDelta;
+import org.cougaar.lib.aggagent.session.SessionManager;
+import org.cougaar.lib.aggagent.session.XMLEncoder;
+import org.cougaar.lib.aggagent.session.SubscriptionAccess;
+import org.cougaar.lib.aggagent.session.IncrementFormat;
+
+import org.cougaar.lib.aggagent.util.Enum.QueryType;
+import org.cougaar.lib.aggagent.util.Enum.Language;
+import org.cougaar.lib.aggagent.util.Enum.AggType;
+import org.cougaar.lib.aggagent.util.Enum.ScriptType;
+import org.cougaar.lib.aggagent.util.Enum.UpdateMethod;
+import org.cougaar.lib.aggagent.util.Enum.XmlFormat;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+
+/**
+ * This class just adds some objects to the blackboard. 
+ */
+public class AddObjectsPlugin extends ComponentPlugin {
+
+  private LoggingService  _log;
+  private int             _maxObjects = 50000;
+  private int             _objSize    = 1000;
+
+  public void setParameter(Object o) {
+    if (!(o instanceof List)) {
+      throw new IllegalArgumentException("Expecting a List parameter " +
+                                         "instead of " + 
+                                         ( (o == null)
+                                           ? "null" 
+                                           : o.getClass().getName() ));
+    }
+
+    List l = (List) o;
+    if (l.size() != 0) {
+      _maxObjects = Integer.parseInt(l.remove(0).toString());
+    } // end of if (l.size() != 0)
+    if (l.size() != 0) {
+      _objSize = Integer.parseInt(l.remove(0).toString());
+    } // end of if (l.size() != 0)
+  }
+
+  protected void execute() {
+  }
+
+  /**
+   * Sets up the AggregationQuery and event subscriptions.
+   */
+  protected void setupSubscriptions() {
+    _log = (LoggingService)
+	getServiceBroker().getService(this, LoggingService.class, null);
+
+    System.out.println("adding objects to blackboard service");
+    BlackboardService bbs = getBlackboardService();
+    for (int i = 0; i < _maxObjects; i++) {
+      bbs.publishAdd(new BigObject(_objSize));
+      if (i % 1000 == 999) {
+        System.out.println("Added " + (i+1) + " " + _objSize + " byte objects");
+      }
+    } // end of for (int i = 0; i < _maxObjects; i++)
+    
+  }
+
+  private static class BigObject implements Serializable {
+    byte buf[];
+    public BigObject(int size) {
+      buf = new byte[size];
+      for (int i = 0; i < buf.length; i++) {
+        buf[i] = (byte) i;
+      } // end of for (int i = 0; i < buf.length; i++)
+    }
+  }
+}
