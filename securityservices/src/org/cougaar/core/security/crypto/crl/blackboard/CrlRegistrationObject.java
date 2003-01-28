65fe484f40675d00ea1dc9fadbc9c0b5cc3ca72a
rtripath <rtripath>
2003-01-28 20:05:50 +0000

    Files have been added for dynamic CRL updates

---
+++ securityservices/src/org/cougaar/core/security/crypto/crl/blackboard/CrlRegistrationObject.java
@@ -0,0 +1,126 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2001 Network Associates
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+package org.cougaar.core.security.crypto.crl.blackboard;
+
+
+import java.util.*;
+import java.security.cert.*;
+import java.io.Serializable;
+import java.io.*;
+// Cougaar core services
+import org.cougaar.core.mts.MessageAddress;
+
+import org.cougaar.core.blackboard.Publishable;
+
+public class CrlRegistrationObject implements Serializable,Publishable { 
+
+  private Vector messageAddress;
+  public String ldapUrl;
+  public int ldapType;
+  private byte[] derEncodedCrl;
+  private String modifiedTimestamp;
+  public String dnName;
+
+  public CrlRegistrationObject (String dnname,String ldapURL,int ldaptype) {
+    dnName=dnname;
+    ldapUrl=ldapURL;
+    ldapType=ldaptype;
+    modifiedTimestamp=null;
+    messageAddress=new Vector();
+  }
+  
+  public void addAgent(MessageAddress agentAddress) throws CRLAgentRegistrationException{
+    if(agentAddress!=null) {
+      MessageAddress msgAddress=null;
+      Enumeration enum =messageAddress.elements();
+      while(enum.hasMoreElements()) {
+	msgAddress=(MessageAddress)enum.nextElement();
+	if(msgAddress.equals(agentAddress)) {
+	  throw new CRLAgentRegistrationException(" Agent " +agentAddress.toString()+
+					     "alredy registered");
+	}
+      }
+      messageAddress.add(agentAddress);
+    }
+  }
+  
+  public void setModifiedTime(String modifiedTime){
+    modifiedTimestamp=modifiedTime;
+  }
+  
+  public void setCRL(byte[] encodedCrl) {
+   derEncodedCrl=encodedCrl;
+  }
+  
+  public X509CRL  getCRL() {
+    X509CRL crl =null;
+    if(derEncodedCrl!=null){
+      try {
+	InputStream inStream = new ByteArrayInputStream(derEncodedCrl);
+	CertificateFactory cf = CertificateFactory.getInstance("X.509");
+	crl = (X509CRL)cf.generateCRL(inStream);
+	inStream.close();
+      }
+      catch (Exception exp){
+	return crl;
+      }
+    }
+    return crl;
+  }
+  public String getModifiedTimeStamp() {
+    return modifiedTimestamp;
+  }
+  
+  public Vector getRegisteredAgents() {
+    return messageAddress;
+  }
+
+  public boolean isPersistable() {
+    return true;
+  }
+
+  public String toString(){
+    StringBuffer buffer=new StringBuffer();
+    if(ldapUrl!=null) {
+      buffer.append("ldap URL :"+ldapUrl+"\n");
+    }
+    buffer.append("ldap type :"+ldapType+"\n");
+    if(dnName!=null){
+       buffer.append("DN Name :"+dnName+"\n");
+    }
+    if(modifiedTimestamp!=null){
+       buffer.append("Modified time stamp :"+modifiedTimestamp+"\n");
+    }
+    if(!messageAddress.isEmpty()) {
+       buffer.append("Registered Addressers are  :"+"\n");
+       MessageAddress msgAddress=null;
+      for(int i=0;i<messageAddress.size();i++) {
+	msgAddress=(MessageAddress)messageAddress.elementAt(i);
+	 buffer.append(msgAddress.toString()+"\n");
+      }
+    }
+    return buffer.toString();
+    
+  }
+  
+  
+}
---
+++ securityservices/src/org/cougaar/core/security/crypto/crl/blackboard/CrlRelay.java
@@ -0,0 +1,173 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2001 Network Associates
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+package org.cougaar.core.security.crypto.crl.blackboard;
+
+import java.util.Set;
+import java.util.Collections;
+import org.cougaar.core.relay.*;
+import org.cougaar.core.mts.MessageAddress;
+import org.cougaar.core.util.UID;
+import org.cougaar.core.util.XMLizable;
+import org.cougaar.core.util.XMLize;
+
+/**
+ * This class implements a Relay capable of transmitting registration for CRL to
+ * CRL Management agent .  */
+public class CrlRelay
+  implements Relay.Source, Relay.Target, XMLizable
+{
+  private MessageAddress source;
+  private MessageAddress target;
+  private UID uid;
+
+  private Object content;
+  private Object response;
+
+  private transient Set _targets;
+
+  /**
+   * @param content initial content
+   * @param response initial response
+   */
+  public CrlRelay(UID uid, MessageAddress source, MessageAddress target,
+		  Object content, Object response) {
+    this.uid = uid;
+    this.source = source;
+    this.target = target;
+
+    this.content = content;
+    this.response = response;
+
+    this._targets = 
+     ((target != null) ?
+      Collections.singleton(target) :
+      Collections.EMPTY_SET);
+  }
+
+  // UniqueObject interface
+
+  public void setUID(UID uid) {
+    throw new RuntimeException("Attempt to change UID");
+  }
+
+  public UID getUID() {
+    return uid;
+  }
+
+  // Source interface
+
+  public Set getTargets() {
+    return _targets;
+  }
+
+  public Object getContent() {
+    return content;
+  }
+
+  private static final class SimpleRelayFactory
+  implements TargetFactory, java.io.Serializable {
+
+    public static final SimpleRelayFactory INSTANCE = 
+      new SimpleRelayFactory();
+
+    private SimpleRelayFactory() {}
+
+    public Relay.Target create(
+        UID uid, 
+        MessageAddress source, 
+        Object content,
+        Token token) {
+      return new CrlRelay( uid, source, null, content, null);
+    }
+
+    private Object readResolve() {
+      return INSTANCE;
+    }
+  };
+
+  public TargetFactory getTargetFactory() {
+    return SimpleRelayFactory.INSTANCE;
+  }
+
+  public int updateResponse(
+      MessageAddress t, Object response) {
+    // assert response != null
+    if (!(response.equals(this.response))) {
+      this.response = response;
+      return Relay.RESPONSE_CHANGE;
+    }
+    return Relay.NO_CHANGE;
+  }
+
+  // Target interface
+
+  public MessageAddress getSource() {
+    return source;
+  }
+
+  public Object getResponse() {
+    return response;
+  }
+
+  public int updateContent(Object content, Token token) {
+    // assert content != null
+    if (!(content.equals(this.content))) {
+      this.content = content;
+      return CONTENT_CHANGE;
+    }
+    return NO_CHANGE;
+  }
+
+  // XMLizable method for UI, other clients
+  public org.w3c.dom.Element getXML(org.w3c.dom.Document doc) {
+    return XMLize.getPlanObjectXML(this, doc);
+  }
+
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    } else if (!(o instanceof CrlRelay)) {
+      return false;
+    } else {
+      UID u = ((CrlRelay) o).getUID();
+      return uid.equals(u);
+    }
+  }
+
+  public int hashCode() {
+    return uid.hashCode();
+  }
+
+  public String toString() {
+    return "("+uid+", "+content+", "+response+")";
+  }
+
+  /** Get the target address.
+   * Not part of any interface, but allows the object to be displayed with the target
+   * in the /task servlet.
+   */
+  public MessageAddress getTarget() {
+    return target;
+  }
+
+}
+
