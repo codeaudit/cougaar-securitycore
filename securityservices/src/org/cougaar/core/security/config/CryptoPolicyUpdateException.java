b7a1a0e3e760f07620ff5253e07f730a129efb57
mluu <mluu>
2003-03-07 00:59:36 +0000

    *** empty log message ***

---
+++ securityservices/src/org/cougaar/core/security/config/CryptoPolicyUpdateException.java
@@ -0,0 +1,39 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2003 Networks Associates Technology, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ * 
+ */
+package org.cougaar.core.security.config;
+
+import java.security.GeneralSecurityException;
+
+/**
+ * This exception is thrown when a component tries to update a crypto client policy
+ * that is different than the internal crypto client policy
+ */
+public class CryptoPolicyUpdateException extends GeneralSecurityException {
+  
+  public CryptoPolicyUpdateException(Throwable cause) { 
+    super(cause.getMessage());
+    setStackTrace(cause.getStackTrace());
+  }
+  public CryptoPolicyUpdateException(String message) {
+    super(message);
+  }
+}
