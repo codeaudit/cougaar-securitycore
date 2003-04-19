0825594b29a1cdb26e1afa38f5d85b297e50f973
gmount <gmount>
2003-04-19 01:57:38 +0000

    *** empty log message ***

---
+++ securityservices/src/org/cougaar/core/security/crypto/MessageDumpedException.java
@@ -0,0 +1,7 @@
+package org.cougaar.core.security.crypto;
+
+public class MessageDumpedException extends java.io.IOException {
+  public MessageDumpedException(String message) {
+    super(message);
+  }
+}
