199d4a126a73086f9844a108eea48bb6b346744f
srosset <srosset>
2003-06-05 01:43:10 +0000

    Changed copyright information

--- securityservices/src/org/cougaar/core/security/monitoring/plugin/DataProtectionSensor.java
+++ securityservices/src/org/cougaar/core/security/monitoring/plugin/DataProtectionSensor.java
@@ -1,6 +1,6 @@
 /*
  * <copyright>
- *  Copyright 1997-2001 Network Associates
+ *  Copyright 1997-2003 Cougaar Software, Inc.
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
  * 
  *  This program is free software; you can redistribute it and/or modify
