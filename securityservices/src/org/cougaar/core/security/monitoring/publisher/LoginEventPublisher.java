39d282fff5993403e6c99e1c910e6db54a346df9
rliao1 <rliao1>
2003-06-06 22:41:29 +0000

    report Login failure events which has different format than normal FailureEvents (non-Agent and multiple targets)

---
+++ securityservices/src/org/cougaar/core/security/monitoring/publisher/LoginEventPublisher.java
@@ -0,0 +1,182 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2002 Network Associates
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+package org.cougaar.core.security.monitoring.publisher;
+
+// cougaar core classes
+import org.cougaar.core.component.ServiceBroker;
+import org.cougaar.core.service.BlackboardService;
+import org.cougaar.core.service.LoggingService;
+
+// securityservices classes
+import org.cougaar.core.security.monitoring.blackboard.CmrFactory;
+import org.cougaar.core.security.monitoring.blackboard.CmrRelay;
+import org.cougaar.core.security.monitoring.blackboard.Event;
+import org.cougaar.core.security.monitoring.blackboard.NewEvent;
+import org.cougaar.core.security.monitoring.idmef.Agent;
+import org.cougaar.core.security.monitoring.idmef.IdmefMessageFactory;
+import org.cougaar.core.security.monitoring.event.FailureEvent;
+import org.cougaar.core.security.monitoring.event.LoginFailureEvent;
+import org.cougaar.core.security.monitoring.plugin.UnknownSensorInfo;
+import org.cougaar.core.security.monitoring.plugin.SensorInfo;
+
+import edu.jhuapl.idmef.Alert;
+import edu.jhuapl.idmef.AdditionalData;
+import edu.jhuapl.idmef.Address;
+import edu.jhuapl.idmef.Classification;
+import edu.jhuapl.idmef.Source;
+import edu.jhuapl.idmef.Target;
+import edu.jhuapl.idmef.XMLUtils;
+import edu.jhuapl.idmef.DetectTime;
+import edu.jhuapl.idmef.Service;
+import edu.jhuapl.idmef.IDMEF_Node;
+import edu.jhuapl.idmef.IDMEF_Process;
+import edu.jhuapl.idmef.User;
+import edu.jhuapl.idmef.UserId;
+
+// java classes
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class LoginEventPublisher extends IdmefEventPublisher {
+
+  public LoginEventPublisher(BlackboardService bbs, CmrFactory cmrFactory,
+    LoggingService logger, SensorInfo info) {
+    super(bbs, cmrFactory, logger, info);
+  }
+
+  private List createClassifications() {
+    ArrayList cfs = new ArrayList();
+    cfs.add(LoginFailureEvent.LOGINFAILURE);
+    return cfs;
+  }
+
+  private List createSources(String remoteAddr) {
+    List addrs = new ArrayList();
+    Address addr = m_idmefFactory.createAddress( remoteAddr, null,
+                                                Address.IPV4_ADDR );
+    addrs.add(addr);
+    IDMEF_Node node = m_idmefFactory.createNode( null, addrs );
+
+    Source src = m_idmefFactory.createSource(node, null, null, null, null);
+
+    List srcs = new ArrayList();
+    srcs.add(src);
+    return srcs;
+  }
+
+  private List createTargets(String url, int serverPort, String protocol,
+                             String userName) {
+    IDMEF_Node node = m_idmefFactory.getNodeInfo();
+    List addrs = new ArrayList();
+    if (node.getAddresses() != null) {
+      Address[] a = node.getAddresses();
+      for (int i = 0; i < a.length; i++) {
+        addrs.add(a[i]);
+      } // end of for (int i = 0; i < a.length; i++)
+    }
+
+    addrs.add(m_idmefFactory.createAddress(url, null, Address.URL_ADDR));
+
+    node = m_idmefFactory.createNode(node.getName(), addrs);
+
+    IDMEF_Process process = m_idmefFactory.getProcessInfo();
+    Service service = m_idmefFactory.createService("Cougaar Web Server",
+                                                  new Integer(serverPort),
+                                                  protocol);
+
+    User user = null;
+    List uids = new ArrayList();
+    if (userName != null) {
+      uids.add(m_idmefFactory.createUserId( userName ));
+    }
+    if (uids.size() > 0) {
+      user = m_idmefFactory.createUser( uids );
+    }
+    Target target = m_idmefFactory.createTarget(node, user, process, service,
+                                               null, null);
+    List targets = new ArrayList();
+    targets.add(target);
+    return targets;
+  }
+
+  private List createAdditionalData(String reason, String targetIdent,
+                                    String data) {
+    Agent agentinfo = m_idmefFactory.getAgentInfo();
+    String [] ref=null;
+    if (agentinfo.getRefIdents()!=null) {
+      String[] originalref=agentinfo.getRefIdents();
+      ref=new String[originalref.length+1];
+      System.arraycopy(originalref,0,ref,0,originalref.length);
+      ref[originalref.length] = targetIdent;
+    } else {
+      ref=new String[1];
+      ref[0] = targetIdent;
+    }
+    agentinfo.setRefIdents(ref);
+
+    AdditionalData additionalData =
+      m_idmefFactory.createAdditionalData(Agent.TARGET_MEANING, agentinfo);
+    List addData = new ArrayList();
+    addData.add(m_idmefFactory.
+                createAdditionalData(AdditionalData.STRING, LoginFailureEvent.FAILURE_REASON,
+                                     reason));
+    addData.add(additionalData);
+    if (data != null) {
+      addData.add(m_idmefFactory.
+                  createAdditionalData(AdditionalData.STRING,
+                                       "Exception", data));
+    }
+    return addData;
+  }
+
+  protected Event createIDMEFAlert(FailureEvent event){
+    LoginFailureEvent loginEvent = (LoginFailureEvent)event;
+    String remoteAddr = loginEvent.getEventSource()[0];
+    String [] eventTargets = loginEvent.getEventTarget();
+    String url = eventTargets[0];
+    int serverPort = 0;
+    try {
+      serverPort = Integer.parseInt(eventTargets[1]);
+    } catch (Exception ex) {}
+    String protocol = eventTargets[2];
+    String userName = eventTargets[3];
+
+    List sources = createSources(remoteAddr);
+    List targets = createTargets(url, serverPort, protocol,
+                                 userName);
+    List classifications = createClassifications();
+    String targetIdent = ((Target) targets.get(0)).getIdent();
+    List additionalData = createAdditionalData(
+      event.getReasonIdentifier(), targetIdent,
+      event.getDataIdentifier());
+    Alert alert = m_idmefFactory.createAlert(m_sensorInfo, new DetectTime(),
+                                            sources, targets,
+                                            classifications,
+                                            additionalData);
+
+    return m_cmrFactory.newEvent(alert);
+  }
+}
