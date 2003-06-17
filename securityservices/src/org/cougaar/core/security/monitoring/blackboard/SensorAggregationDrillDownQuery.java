58c25ea6dbcde8d09e4ad2c8295cf3e6c0c1e453
rtripath <rtripath>
2003-06-17 01:12:56 +0000

    * Agg Query mechanism publlished at sensor to report events that satisfy the predicate satified in the query od sensorDrillQuery

---
+++ securityservices/src/org/cougaar/core/security/monitoring/blackboard/SensorAggregationDrillDownQuery.java
@@ -0,0 +1,52 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2003 CougaarSoftware Inc
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+
+
+package org.cougaar.core.security.monitoring.blackboard;
+
+// Core classes
+import org.cougaar.core.util.UID;
+import org.cougaar.lib.aggagent.util.Enum.QueryType;
+import org.cougaar.lib.aggagent.util.Enum;
+import org.cougaar.core.util.UniqueObject;
+import org.cougaar.lib.aggagent.query.AggregationQuery;
+import org.cougaar.lib.aggagent.query.QueryResultAdapter;
+
+public class  SensorAggregationDrillDownQuery extends QueryResultAdapter implements java.io.Serializable,UniqueObject {
+  private UID _uid;
+
+  public SensorAggregationDrillDownQuery(AggregationQuery query  ){
+    super(query);
+   
+    // this.setUID(iuid);
+  }
+  
+
+  public void setUID  (UID uid){
+    _uid=uid;
+  }
+  
+  public UID getUID (){
+    return _uid;
+  }
+ 
+}
