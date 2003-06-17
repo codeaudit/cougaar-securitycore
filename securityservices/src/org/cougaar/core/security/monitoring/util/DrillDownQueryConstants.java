b1d885c68c4ef90103e437c5f31bdf1cb13b542a
rtripath <rtripath>
2003-06-17 01:29:26 +0000

    * Defines constants used by DrillDown Mechanism

---
+++ securityservices/src/org/cougaar/core/security/monitoring/util/DrillDownQueryConstants.java
@@ -0,0 +1,35 @@
+
+
+/*
+ * <copyright>
+ *  Copyright 1997-2003 CougaarSoftware Inc.
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+
+
+package org.cougaar.core.security.monitoring.util;
+
+public class DrillDownQueryConstants {
+  public static final String ORIGINATORS_UID="ORIGINATORS_UID";
+  public static final String PARENT_UID="PARENT_UID";
+  public static final String TOTAL_CURRENT_EVENTS="TOTAL_CURRENT_EVENTS";
+  public static final String TOTAL_EVENTS="TOTAL_EVENTS";
+  public static final String RATE="RATE";
+   public static final String CONSOLIDATED_EVENTS="CONSOLIDATED_EVENTS";
+}
