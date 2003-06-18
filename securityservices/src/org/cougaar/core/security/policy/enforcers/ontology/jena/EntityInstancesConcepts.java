928889fe9ee92c15a36f7433c93a9df31a8ca126
tredmond <tredmond>
2003-06-18 19:35:44 +0000

    My old strategy with otherblackboardinstances didnot work.

--- securityservices/src/org/cougaar/core/security/policy/enforcers/ontology/jena/EntityInstancesConcepts.java
+++ securityservices/src/org/cougaar/core/security/policy/enforcers/ontology/jena/EntityInstancesConcepts.java
@@ -5,7 +5,8 @@ final public class EntityInstancesConcepts
     final public static String EntityInstancesDamlURL = "http://ontology.coginst.uwf.edu/Ultralog/Names/EntityInstances.daml#";
 
 	// Concepts
-    final public static String _OtherBlackBoardObjects_ = EntityInstancesDamlURL + "OtherBlackBoardObjects";
+    final public static String _PluginsInRolePolicyServletRole_ = EntityInstancesDamlURL + "PluginsInRolePolicyServletRole";
+    final public static String _BlackboardObjects_ = EntityInstancesDamlURL + "BlackboardObjects";
 
 
 	// Properties
