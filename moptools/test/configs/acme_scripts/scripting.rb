d77a8fd840e0d0d8614d9a5df4560cc196fa82cd
srosset <srosset>
2003-06-26 22:02:42 +0000

    *** empty log message ***

--- moptools/test/configs/acme_scripts/scripting.rb
+++ moptools/test/configs/acme_scripts/scripting.rb
@@ -15,7 +15,7 @@ module Cougaar
 
                                         @run.society.each_agent(true) do |agent|
                                                 url = "http://#{ agent.node.host.host_name}:#{@run.society.cougaar_port}/$#{agent.name}/testBlackboardManager?do=start&exp=#{@run.name}"
-						puts url
+						#puts url
                                                 result = Cougaar::Communications::HTTP.get(url)
 
 
@@ -39,7 +39,7 @@ module Cougaar
                                 begin
                                         @run.society.each_agent(true) do |agent|
                                                 url ="http://#{agent.node.host.host_name}:#{@run.society.cougaar_port}/$#{agent.name}/testBlackboardManager?do=end&exp=#{@run.name}"
-						puts url
+						#puts url
                                                 req=Cougaar::Communications::HTTP.get(url)
                                         end
                                 rescue
