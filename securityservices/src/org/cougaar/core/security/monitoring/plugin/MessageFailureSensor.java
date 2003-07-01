e3daf229124bf052436c939b7d4cc778ccd03bc8
gmount <gmount>
2003-07-01 20:27:48 +0000

    Changed copyright statement

--- securityservices/src/org/cougaar/core/security/monitoring/plugin/MessageFailureSensor.java
+++ securityservices/src/org/cougaar/core/security/monitoring/plugin/MessageFailureSensor.java
@@ -1,6 +1,6 @@
 /*
  * <copyright>
- *  Copyright 1997-2002 Networks Associates Technology, Inc.
+ *  Copyright 1997-2003 Cougaar Software, Inc.
  *  under sponsorship of the Defense Advanced Research Projects
  *  Agency (DARPA).
  *
