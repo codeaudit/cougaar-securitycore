889ac9abf087998702284de3469a6bf15518cff5
tredmond <tredmond>
2003-07-05 01:15:19 +0000

    Automatic update.

--- securityservices/src/org/cougaar/core/security/policy/enforcers/ontology/jena/EntityInstancesConcepts.java
+++ securityservices/src/org/cougaar/core/security/policy/enforcers/ontology/jena/EntityInstancesConcepts.java
@@ -6,7 +6,13 @@ final public class EntityInstancesConcepts
 
 	// Concepts
     final public static String _PluginsInRolePolicyServletRole_ = EntityInstancesDamlURL + "PluginsInRolePolicyServletRole";
+    final public static String _PluginsInRoleOrgActivityQueryNoReadRole_ = EntityInstancesDamlURL + "PluginsInRoleOrgActivityQueryNoReadRole";
     final public static String _BlackboardObjects_ = EntityInstancesDamlURL + "BlackboardObjects";
+    final public static String _PluginsInRoleOrgActivityAllRole_ = EntityInstancesDamlURL + "PluginsInRoleOrgActivityAllRole";
+    final public static String _PluginsInRoleOrgActivityQueryRole_ = EntityInstancesDamlURL + "PluginsInRoleOrgActivityQueryRole";
+    final public static String _PluginsInRoleOrgActivityRemoveRole_ = EntityInstancesDamlURL + "PluginsInRoleOrgActivityRemoveRole";
+    final public static String _PluginsInRoleOrgActivityAddRole_ = EntityInstancesDamlURL + "PluginsInRoleOrgActivityAddRole";
+    final public static String _PluginsInRoleOrgActivityChangeRole_ = EntityInstancesDamlURL + "PluginsInRoleOrgActivityChangeRole";
 
 
 	// Properties
