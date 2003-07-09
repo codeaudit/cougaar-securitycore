b4d8bff535f96658464bdfdfb02aef0c11c0cf70
tredmond <tredmond>
2003-07-09 01:43:21 +0000

    I needed java access to the servlet concept.

---
+++ securityservices/src/org/cougaar/core/security/policy/enforcers/ontology/jena/UltralogEntityConcepts.java
@@ -0,0 +1,19 @@
+package org.cougaar.core.security.policy.enforcers.ontology.jena;
+
+final public class UltralogEntityConcepts
+{
+    final public static String UltralogEntityDamlURL = "http://ontology.coginst.uwf.edu/Ultralog/UltralogEntity.daml#";
+
+	// Concepts
+    final public static String _Servlet_ = UltralogEntityDamlURL + "Servlet";
+    final public static String _BlackBoardObjects_ = UltralogEntityDamlURL + "BlackBoardObjects";
+    final public static String _AuthenticationLevel_ = UltralogEntityDamlURL + "AuthenticationLevel";
+    final public static String _ULContentValue_ = UltralogEntityDamlURL + "ULContentValue";
+    final public static String _ProtectionLevel_ = UltralogEntityDamlURL + "ProtectionLevel";
+    final public static String _PlugInRoles_ = UltralogEntityDamlURL + "PlugInRoles";
+    final public static String _BlackBoardAccessMode_ = UltralogEntityDamlURL + "BlackBoardAccessMode";
+    final public static String _UltralogEntity_ = UltralogEntityDamlURL + "UltralogEntity";
+
+
+	// Properties
+}
