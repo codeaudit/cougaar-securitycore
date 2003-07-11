1a1b6b903f985ed97531847612720ed0228072d7
mabrams <mabrams>
2003-07-11 13:38:41 +0000

    set up to run on remote machine

--- moptools/test/configs/acme_scripts/networkanalysis.rb
+++ moptools/test/configs/acme_scripts/networkanalysis.rb
@@ -8,43 +8,44 @@
 #
 #
 module Cougaar
-  module Actions
-    class StartEtherealAnalysis < Cougaar::Action
-      def initialize(run)
-        super(run)
-        @run = run
-      end
-      def perform
-        outputfilename="#{@run.name}_analysis.in"
-        puts "Output of raw network data #{outputfilename}"
-        puts "Removing old file if present"
-        system "rm -f #{outputfilename}"
-        commandsyntax="tethereal -z io,phs -w #{outputfilename}"
-	puts "Executing #{commandsyntax}"
-	$pid = fork{
-	  puts "Executing #{commandsyntax}"
-	  exec(commandsyntax)
-	}
-	puts "PID: #{$pid}"
-      end
-    end
+        module Actions
+         class StartEtherealAnalysis < Cougaar::Action
+                         def initialize(run)
+                                super(run)
+                                print "Enter Hostname: "
+                                $hostname = gets.chomp
+                                @run = run
+                     end
+                     def perform
+                        outputfilename="#{@run.name}_analysis.in"
+                        puts "Output of raw network data #{outputfilename}"
+                        puts "Removing old file if present"
+                        commandsyntax="ssh #{hostname} ./runTethereal.sh #{outputfilename}"
+                        puts "Executing #{commandsyntax}"
+                        $pid = fork{
+                                 puts "Executing #{commandsyntax}"
+                                 exec(commandsyntax)
+                        }
+                        puts "PID: #{$pid}"
+                     end
+                end
 
-    class StopEtherealAnalysis  < Cougaar::Action
-      def initialize(run)
-	super(run)
-	@run = run
-      end
-      def perform
-	system "kill #{$pid}"
-	infile="#{@run.name}_analysis.in"
-	outfile="#{@run.name}_analysis.out"
-	puts "Analyze file: #{infile}"
-	puts "Create file: #{outfile}"
-	commandsyntax="ethereal -r #{infile} -w #{outfile}"
-	exec(commandsyntax)
-	puts "Output written to #{outfile}"
-      end
-    end
-    
-  end
+                class StopEtherealAnalysis  < Cougaar::Action
+                         def initialize(run)
+                                super(run)
+                                @run = run
+                     end
+                     def perform
+                        system "ssh -t #{hostname} ./stopTehtereal"
+                        infile="#{@run.name}_analysis.in"
+                        outfile="#{@run.name}_analysis.out"
+                        puts "Analyze file: #{infile}"
+                        puts "Create file: #{outfile}"
+                        commandsyntax="ssh -t #{hostname} ./readTethreal.sh #{infile} #{outfile}"
+                        exec(commandsyntax)
+                        puts "Output written to #{outfile} on #{hostname}"
+                     end
+                end
+        
+        end
 end
