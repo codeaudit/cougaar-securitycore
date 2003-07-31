77ff1f0d382f784cf9c17448a53dac3ddda45cd5
tredmond <tredmond>
2003-07-31 20:35:05 +0000

    The new blackboard policy template.

---
+++ securityservices/src/org/cougaar/core/security/policy/builder/BlackboardParsedPolicy.java
@@ -0,0 +1,162 @@
+/*
+ * <copyright>
+ *  Copyright 2003 Cougaar Software, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects Agency *  (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+package org.cougaar.core.security.policy.builder;
+
+import java.util.*;
+
+import kaos.ontology.util.ClassNameNotSet;
+import kaos.ontology.util.KAoSClassBuilderImpl;
+import kaos.ontology.util.RangeIsBasedOnAClass;
+import kaos.policy.util.DAMLPolicyBuilderImpl;
+
+import org.cougaar.core.security.policy.enforcers.ontology.jena.*;
+
+
+public class BlackboardParsedPolicy extends ParsedPolicy
+{
+  private Set    _accessModes;
+  private Set    _objectTypes;
+   
+  public BlackboardParsedPolicy(String  policyName,
+                                String  plugInRole,
+                                Set     accessModes,
+                                Set     objectTypes)
+    throws PolicyCompilerException
+  {
+    super(policyName, 
+          2,
+          true,
+          PolicyUtils.pluginsInRoleClassPrefix + plugInRole,
+          UltralogActionConcepts._BlackBoardAccess_);
+    _accessModes = accessModes;
+    _objectTypes  = objectTypes;
+    makeDescription(plugInRole);
+    enforceBlackboardConstraints();
+  }
+
+  /**
+   * build the description of the policy from the arguments.  For readability,
+   * it should probably  be called before enforceBlackboardContraints.
+   */
+  private void makeDescription(String plugInRole)
+  {
+    _description = "A plugin in the role " + plugInRole + " can ";
+    {
+      Iterator accessModesIt = _accessModes.iterator();
+      String   accessMode    = (String) accessModesIt.next();
+      _description += accessMode;
+      while (accessModesIt.hasNext()) {
+        accessMode = (String) accessModesIt.next();
+        _description += (", " + accessMode);
+      }
+    }
+    _description += " objects of type ";
+    {
+      Iterator objectTypesIt = _objectTypes.iterator();
+      String   objectType    = (String) objectTypesIt.next();
+      _description += objectType;
+      while (objectTypesIt.hasNext()) {
+        objectType = (String) objectTypesIt.next();
+        _description += (", " + objectType);
+      }
+    }
+  }
+
+
+  /**
+   * This routine ensures certain consistency requirements involving which 
+   * access modes imply which other access modes.
+   */
+  public void enforceBlackboardConstraints()
+    throws PolicyCompilerException
+  {
+    if (_accessModes.contains("Add")) {
+      _accessModes.add("Read");
+      _accessModes.add("Write");
+      _accessModes.add("Create");
+    }
+    if (_accessModes.contains("Change")) {
+      _accessModes.add("Read");
+      _accessModes.add("Write");
+    }
+    if (_accessModes.contains("Query")) {
+      _accessModes.add("Read");
+    }
+    if (_accessModes.contains("Remove") 
+        && !_accessModes.contains("Add")
+        && !_accessModes.contains("Query")) {
+      throw new PolicyCompilerException("Remove blackboard access implies " +
+                                        "either Add or Query access");
+    }
+    if (_accessModes.contains("Change") 
+        && !_accessModes.contains("Add")
+        && !_accessModes.contains("Query")) {
+      throw new PolicyCompilerException("Remove blackboard access implies " +
+                                        "either Add or Query access");
+    }
+  }
+
+  /**
+   * This routine does the core work of constructing the policy defined by 
+   * the Blackboard Access Policy.
+   */
+  public DAMLPolicyBuilderImpl buildPolicy(OntologyConnection ontology)
+    throws PolicyCompilerException
+  {
+    try {
+      Set    jenaAccessModes = new HashSet();
+      ontology.verifySubClass(getActor(), 
+                              UltralogActorConcepts._UltralogPlugins_);
+      // The stuff we gave to super is valid...
+      initiateBuildPolicy(ontology);
+
+      for (Iterator accessModesIt = _accessModes.iterator();
+           accessModesIt.hasNext(); ) {
+        String accessMode = (String) accessModesIt.next();
+        String jenaAccessMode 
+          =  EntityInstancesConcepts.EntityInstancesDamlURL
+                    + "BlackBoardAccess" + accessMode;
+        ontology.verifyInstanceOf(jenaAccessMode, 
+                                  UltralogEntityConcepts._BlackBoardAccessMode_);
+        _controls.addPropertyRangeInstance
+              (UltralogActionConcepts._blackBoardAccessMode_, jenaAccessMode);
+      }
+      for (Iterator objectTypeIt = _objectTypes.iterator();
+           objectTypeIt.hasNext(); ) {
+        String objectType = (String) objectTypeIt.next();
+        String jenaObjectClass = EntityInstancesConcepts.EntityInstancesDamlURL
+                                          + objectType;
+        ontology.verifyInstanceOf(jenaObjectClass,
+                                  UltralogEntityConcepts._BlackBoardObjects_);
+        _controls.addPropertyRangeInstance(
+                           UltralogActionConcepts._blackBoardAccessObject_,
+                           jenaObjectClass);
+      }
+      return _pb;
+    } catch ( ClassNameNotSet e ) {
+      throw new PolicyCompilerException(e);
+    } catch ( RangeIsBasedOnAClass e) {
+      throw new PolicyCompilerException(e);
+    }
+  }
+
+}
