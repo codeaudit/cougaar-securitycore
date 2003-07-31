6d2e7a14fdf3c702f9745e211250b864a686965d
srosset <srosset>
2003-07-31 23:16:30 +0000

    *** empty log message ***

--- moptools/csrc/bufferoverflow/remote_attack_script.sh
+++ moptools/csrc/bufferoverflow/remote_attack_script.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 echo "====> In attack script - Doing a bad thing now"
 echo "This is a bad configuration file" > "temp-`date +%b-%d-%X`" 
+
+# Kill existing java processes
+killall -9 java
+
+# Search startup file
+
+# Search and replace -D properties
+
+
+# Enable user authentication over HTTP (or HTTPS)
+# org.cougaar.lib.web.tomcat.enableAuth=false
+
