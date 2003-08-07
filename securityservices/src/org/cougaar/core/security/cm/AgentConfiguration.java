2432698b09ecef36ef97d57bf1ad66c65849c1dc
ttschampel <ttschampel>
2003-08-07 00:09:22 +0000

    *** empty log message ***

--- securityservices/src/org/cougaar/core/security/cm/AgentConfiguration.java
+++ securityservices/src/org/cougaar/core/security/cm/AgentConfiguration.java
@@ -25,18 +25,11 @@
  */
 
 
-/*
- * Created on Jul 29, 2003
- *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
- */
+
 package org.cougaar.core.security.cm;
 
 
 import java.io.Serializable;
-
-import java.util.List;
 
 
 /**
