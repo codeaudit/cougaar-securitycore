19bb618728862a2c6786786af4eef5d070bd0dce
srosset <srosset>
2003-08-21 23:00:40 +0000

    Added comments to clarify the usage of host names. Added debug statements

--- securityservices/src/org/cougaar/core/security/util/NodeInfo.java
+++ securityservices/src/org/cougaar/core/security/util/NodeInfo.java
@@ -1,6 +1,6 @@
 /*
  * <copyright>
- *  Copyright 1997-2001 Networks Associates Technology, Inc.
+ *  Copyright 1997-2003 Cougaar Software, Inc.
  *  under sponsorship of the Defense Advanced Research Projects
  *  Agency (DARPA).
  *
@@ -28,6 +28,8 @@ package org.cougaar.core.security.util;
 
 import org.cougaar.core.component.ServiceBroker;
 import org.cougaar.core.node.NodeIdentificationService;
+import org.cougaar.util.log.Logger;
+import org.cougaar.util.log.LoggerFactory;
 
 import java.lang.reflect.Field;
 import java.net.InetAddress;
@@ -38,6 +40,11 @@ public class NodeInfo
 {
   private static String nodeName = null;
   private static String hostName = null;
+  private static Logger _log;
+
+  static {
+    _log = LoggerFactory.getInstance().createLogger(NodeInfo.class);
+  }
 
   /** Returns the name of the node. Deals with backward compatibility issues */
   public static String getNodeName()
@@ -86,6 +93,33 @@ public class NodeInfo
     nodeName = nis.getMessageAddress().toString();
   }
 
+  /**
+   * Returns the name of the local host.
+   * Care must be taken when using the host name for certificates.
+   * The host name is used to set the Common Name (CN) attribute
+   * of the host X.509 certificate.
+   * Most clients (e.g. browsers or agents using HTTPs) verify that
+   * the host name matches the CN of the X.509 certificate.
+   * Usually, clients perform a reverse lookup of the server IP address
+   * using DNS. The returned host name is matched against the CN
+   * in the X.509 certificate provided in the SSL handshake.
+   * In a normal deployment, fully qualified domain names should be used.
+   * However, there are scenarios when another name resolution service
+   * is being used. For example, hosts could use NIS, NIS+, WINS,
+   * or host files. Care must be taken that the client and the server
+   * will see the same name.
+   * Another example is load balancing, when an array of servers
+   * serve the same externally visible DNS name, but they have
+   * different names internally.
+   * The org.cougaar.core.security.hostname property can be used
+   * to override the host name.
+   *
+   * Issues:
+   * - getCanonicalHostName() may return the textual representation
+   *   of the IP address if a reverse DNS lookup fails.
+   * - getHostName() may return the FQDN if there is a matching
+   *   entry in DNS, not just the host name.
+   */
   public static String getHostName() {
     if (hostName == null) {
       // is it set in a system parameter?
@@ -94,10 +128,14 @@ public class NodeInfo
         return hostName;
       }
       try {
+        //hostName = InetAddress.getLocalHost().getCanonicalHostName();
         hostName = InetAddress.getLocalHost().getHostName();
       } catch (UnknownHostException ex) {
 	System.err.println("Unable to get my host name: " + ex.toString());
       }
+    }
+    if (_log.isDebugEnabled()) {
+      _log.debug("hostname:" + hostName);
     }
     return hostName;
   }
