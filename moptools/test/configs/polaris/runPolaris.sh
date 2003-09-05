4655f20fd4a830fbaa6776a20a56b6c56e1dc7e2
srosset <srosset>
2003-09-05 16:53:23 +0000

    *** empty log message ***

--- moptools/test/configs/polaris/runPolaris.sh
+++ moptools/test/configs/polaris/runPolaris.sh
@@ -8,9 +8,9 @@ fi
 sed -e s/\"u180/\"polaris.ultralog.net/ $CIP/csmart/acme_scripting/bin/p-config.rb.ori > \
    $CIP/csmart/acme_scripting/bin/p-config.rb
 
-exit
 xmlPolarisConfiguration=as-10.4.xml
 rundir=~/CSI/polaris/automation/scripts03/security
 cp ${xmlPolarisConfiguration} ${rundir}
 cd ${rundir}
-$CIP/csmart/acme_scripting/bin/p-run.rb JTG-Security-1a.rb ${xmlPolarisConfiguration}
+#$CIP/csmart/acme_scripting/bin/p-run.rb JTG-Security-1a.rb ${xmlPolarisConfiguration}
+$CIP/csmart/acme_scripting/bin/p-run.rb JTG-Security-3a101.rb ${xmlPolarisConfiguration}
