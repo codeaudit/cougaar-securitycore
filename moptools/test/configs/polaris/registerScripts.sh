f0d067f632c1e6fe51485828e11942d635dd4878
srosset <srosset>
2003-09-05 03:26:09 +0000

    *** empty log message ***

--- moptools/test/configs/polaris/registerScripts.sh
+++ moptools/test/configs/polaris/registerScripts.sh
@@ -8,9 +8,7 @@ fi
 sed -e s/\"u180/\"polaris.ultralog.net/ $CIP/csmart/acme_scripting/bin/p-config.rb.ori > \
    $CIP/csmart/acme_scripting/bin/p-config.rb
 
-exit
-xmlPolarisConfiguration=as-10.4.xml
-rundir=~/CSI/polaris/automation/scripts03/security
-cp ${xmlPolarisConfiguration} ${rundir}
-cd ${rundir}
-$CIP/csmart/acme_scripting/bin/p-run.rb JTG-Security-1a.rb ${xmlPolarisConfiguration}
+
+# Register scripts
+
+
