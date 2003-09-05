0f404665152f9585d6c90b8d4e301af0d1edeb61
srosset <srosset>
2003-09-05 00:41:51 +0000

    *** empty log message ***

--- moptools/test/configs/polaris/runPolaris.sh
+++ moptools/test/configs/polaris/runPolaris.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 set xmlPolarisConfiguration="as-10.4.xml"
+
+#cd ~/CSI/polaris/automation/scripts03
+#ruby runTestScript.rb 1a ${xmlPolarisConfiguration}
+
 cd ~/CSI/polaris/automation/scripts03
-ruby runTestScript.rb 1a ${xmlPolarisConfiguration}
+p-run.rb security/JTG-Security-1a.rb ${xmlPolarisConfiguration}
