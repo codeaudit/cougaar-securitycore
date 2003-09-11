326f78dbb2c8831220296ec3e1283b9e7306bb13
mabrams <mabrams>
2003-09-11 19:26:06 +0000

    *** empty log message ***

--- securityservices/src/org/cougaar/core/security/test/wp/WPTestServlet.java
+++ securityservices/src/org/cougaar/core/security/test/wp/WPTestServlet.java
@@ -1,4 +1,32 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2003 Cougaar Software, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ *
+ * </copyright>
+ *
+ * CHANGE RECORD
+ * -
+ */
+
+
 package org.cougaar.core.security.test.wp;
+
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,23 +36,34 @@ import org.cougaar.planning.ldm.PlanningFactory;
 import org.cougaar.planning.ldm.plan.NewTask;
 import org.cougaar.planning.ldm.plan.Verb;
 
-public class WPTestServlet extends AbstractServletComponent{
 
-	
-	protected String getPath() {
-		return "/wptest";
-	}
+/**
+ * DOCUMENT ME!
+ *
+ * @version $Revision: 1.2 $
+ * @author $author$
+ */
+public class WPTestServlet extends AbstractServletComponent {
+  /**
+   * DOCUMENT ME!
+   *
+   * @return DOCUMENT ME!
+   */
+  protected String getPath() {
+    return "/wptest";
+  }
 
-	/* (non-Javadoc)
-	 * @see org.cougaar.core.security.test.AbstractServletComponent#execute(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-	 */
-	protected void execute(HttpServletRequest request, HttpServletResponse response) {
-		this.blackboardService.openTransaction();
-		PlanningFactory pf = (PlanningFactory)domainService.getFactory("planning");
-		NewTask task = pf.newTask();
-		task.setVerb(Verb.getVerb("WPTEST"));
-		this.blackboardService.closeTransactionDontReset();
-		
-	}
 
+  /* (non-Javadoc)
+   * @see org.cougaar.core.security.test.AbstractServletComponent#execute(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+   */
+  protected void execute(HttpServletRequest request, HttpServletResponse response) {
+    this.blackboardService.openTransaction();
+    PlanningFactory pf = (PlanningFactory) domainService.getFactory("planning");
+    NewTask task = pf.newTask();
+    task.setVerb(Verb.getVerb("WPTEST"));
+    this.blackboardService.publishAdd(task);
+    this.blackboardService.closeTransactionDontReset();
+
+  }
 }
