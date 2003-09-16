6cead596d1504570217f1c20d6c611a7ada755fe
tredmond <tredmond>
2003-09-16 22:37:36 +0000

    The relatively complex uri matching code has been replaced with a call to a regular expression matcher.

--- securityservices/src/org/cougaar/core/security/policy/enforcers/util/DAMLServletMapping.java
+++ securityservices/src/org/cougaar/core/security/policy/enforcers/util/DAMLServletMapping.java
@@ -22,25 +22,31 @@
 package org.cougaar.core.security.policy.enforcers.util;
 
 import org.cougaar.core.component.ServiceBroker;
+import org.cougaar.core.service.LoggingService;
+
+import org.cougaar.core.security.policy.enforcers.ontology.jena.EntityInstancesConcepts;
+
 
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
-
 
 /**
  * The purpose of this class is to facilitate the mapping between DAML
  * concepts and the UltraLog concepts.  For now I am using
  * configuration files but some of this will change later...
  */
-public class DAMLServletMapping extends StringPairMapping{
-
-  private boolean _initialized = false;
-  private List _uriMap;
+public class DAMLServletMapping 
+{
+  private boolean             _initialized = false;
+  private RegexpStringMapping _uriMap;
+  private ServiceBroker        _sb;
+  private LoggingService       _log;
 
   public DAMLServletMapping(ServiceBroker sb)
   {
-    super(sb);
+    _sb = sb;
+    _log = (LoggingService) _sb.getService(this, LoggingService.class, null);
     if (_log.isDebugEnabled()) {
       _log.debug("Initializing DAML Servlet Mapper");
     }
@@ -51,121 +57,21 @@ public class DAMLServletMapping extends StringPairMapping{
   {
     try {
       _log.debug("loading uri mappings...");
-      _uriMap = loadPairs("DamlUriMap");
-    } catch (IOException e) {
-      _log.error("IO Exception reading DAML <-> uri configuration file", e);
+      _uriMap = new RegexpStringMapping(_sb, "DamlUriMap");
+    } catch (Exception e) {
+      _log.error("Exception reading DAML <-> uri configuration file", e);
     }
   }
 
   public String ulUriToKAoSUri(String uri)
   {
     try {
-      _log.debug("Converting " + uri + " to KAoS uri");
-      AgentUri agUri = new AgentUri(uri);
-      for (Iterator uriIt = _uriMap.iterator();
-           uriIt.hasNext();) {
-        StringPair pair = (StringPair) uriIt.next();
-        String pattern = pair._first;
-        String kaosUri = pair._second;
-        _log.debug("Matching against pattern " + pattern);
-        AgentUri agUriPattern = new AgentUri(pattern);
-        if (agUri.match(agUriPattern)) {
-          _log.debug("Found Match");
-          String ret = 
-            org.cougaar.core.security.policy.enforcers.ontology.jena
-            .EntityInstancesConcepts.EntityInstancesDamlURL
-            + (String) (kaosUri);
-          _log.debug("Returning " + ret);
-          return ret;
-        }
-      }
-      return null;
+      return EntityInstancesConcepts.EntityInstancesDamlURL
+                         + _uriMap.functionalGet(uri);
     } catch (Exception e) {
       _log.warn("This is probably not good - " + 
                 "some URI is malformed...", e);
       return null;
     }
   }
-
-  private class AgentUri 
-  {
-    private String _agent;
-    private String _uri;
-
-    /**
-     * Constructor for AgentUri which takes a uri parameter.
-     *
-     * Splits a URI like /$EnclaveOneWorkerNode/myuri into the agent part
-     * (EnclaveOneWorkerNode) and the rest (/myuri).
-     */
-
-    public AgentUri(String uri)
-    {
-      String agent;
-      if (uri.startsWith("/$")) {
-        int index = uri.indexOf("/", 2);
-	if (index == -1) {
-	    _agent = uri.substring(2);
-	    uri = "/";
-	} else {
-	    _agent = uri.substring(2,index);
-	    uri = uri.substring(index);
-	}
-      } else {
-        _agent = null;
-      }
-      _uri=uri;
-    }
-
-    public String getAgent() { return _agent; }
-
-    public String getURI() { return _uri; }
-
-    public boolean rootURI() { return _agent == null; }
-
-    /**
-     * This function matches a uri against a uri from the policy.
-     */
-    public boolean match(AgentUri pattern)
-    {
-      _log.debug("Does the uri with agent " + _agent + " and uri " + _uri);
-      _log.debug("match the pattern with agent " + pattern._agent + 
-                 " and uri " + pattern._uri + "?");
-
-      if (!matchAgentPart(pattern)) {
-        _log.debug("agent match failed");
-        return false;
-      } else if (pattern._uri.equals("/*")) {
-        _log.debug("matched because the uri is universal");
-        return true;
-      } else if (_uri.equals(pattern._uri)) {
-        _log.debug("matched because the uri's are equal");
-        return true;
-      }
-      _log.debug("no match");
-      return false;
-    }
-
-    private boolean matchAgentPart(AgentUri pattern)
-    {
-      _log.debug("do the agents match?");
-      if (rootURI() ^ pattern.rootURI()) {
-        _log.debug("one agent is root the other isn't");
-        return false;
-      } else if (rootURI() && pattern.rootURI()) {
-        _log.debug("both agents are root");
-        return true;
-      } else if (pattern._agent.equals("*")) {
-        _log.debug("the agent pattern is universal");
-        return true;
-      } else if (_agent.equals(pattern._agent)) {
-        _log.debug("the agent is the same as the pattern");
-        return true;
-      }
-      _log.debug("The agents don't match");
-      return false;
-    }
-
-  }
-
 }
