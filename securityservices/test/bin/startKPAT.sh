d4e5a9926fd9d68be041cc9fca81493a54bc7228
tredmond <tredmond>
2003-09-21 00:06:34 +0000

    This is cleaned up a little so that we won't get those missing classes exceptions when people run things in a different environment.  Also startKPAT.sh now doesn't use the cougaar bootstrapper.  I may be hallucinating but I think that this is noticeably faster (because I am not checkign signatures?).

--- securityservices/test/bin/startKPAT.sh
+++ securityservices/test/bin/startKPAT.sh
@@ -24,8 +24,6 @@ if [ "$COUGAAR_INSTALL_PATH" = "" ]; then
   exit;
 fi
 
-LIBPATHS=$COUGAAR_INSTALL_PATH/lib/bootstrap.jar
-
 host=$1
 port=$2
 
@@ -34,12 +32,29 @@ if [ "$3" == "" ]; then
   agentName="PolicyDomainManager1ServletAgent";
 fi
 
-MYPROPERTIES="-Dorg.cougaar.bootstrap.Bootstrapper.loud=shout -Dorg.cougaar.system.path=$COUGAAR_INSTALL_PATH/sys -Dorg.cougaar.install.path=$COUGAAR_INSTALL_PATH -Dorg.cougaar.core.servlet.enable=true -Dorg.cougaar.lib.web.scanRange=100 -Dorg.cougaar.lib.web.http.port=$port -Dorg.cougaar.lib.web.https.port=-1 -Dorg.cougaar.lib.web.https.clientAuth=true -Xbootclasspath/p:$COUGAAR_INSTALL_PATH/lib/javaiopatch.jar"
+for i in ${COUGAAR_INSTALL_PATH}/lib/*.jar
+do
+  CP=$CP:$i
+done
+for i in ${COUGAAR_INSTALL_PATH}/sys/*.jar
+do
+  CP=$CP:$i
+done
+
+#MYPROPERTIES="-Dlog4j.rootCategory=FATAL,Junk -Dlog4j.appender.Junk=org.apache.log4j.FileAppender -Dlog4j.appender.Junk.File=/tmp/KPAT.log"
+
+#
+# The above attempt did not work.  The disadvantage of the approach below is 
+# that it leaves those stupid node.log files distributed all around your 
+# directories...
+#
+MYPROPERTIES="-Dlog4j.configuration=${COUGAAR_INSTALL_PATH}/configs/common/loggingConfig.conf"
+MYPROPERTIES="${MYPROPERTIES} -Dorg.cougaar.system.path=$COUGAAR_INSTALL_PATH/sys -Dorg.cougaar.install.path=$COUGAAR_INSTALL_PATH -Dorg.cougaar.core.servlet.enable=true -Dorg.cougaar.lib.web.scanRange=100 -Dorg.cougaar.lib.web.http.port=$port -Dorg.cougaar.lib.web.https.port=-1 -Dorg.cougaar.lib.web.https.clientAuth=true"
 
 MYMEMORY=""
-MYCLASSES="org.cougaar.bootstrap.Bootstrapper kaos.kpat.applet.KPATAppletMain"
+MYCLASSES="kaos.kpat.applet.KPATAppletMain"
 MYARGUMENTS="http://$host:$port/\$$agentName/policyAdmin true"
 
-echo java $MYPROPERTIES $MYMEMORY -classpath $LIBPATHS $MYCLASSES $MYARGUMENTS
-java $MYPROPERTIES $MYMEMORY -classpath $LIBPATHS $MYCLASSES $MYARGUMENTS
+#echo java -classpath $CP $MYPROPERTIES $MYMEMORY $MYCLASSES $MYARGUMENTS
+      java -classpath $CP $MYPROPERTIES $MYMEMORY $MYCLASSES $MYARGUMENTS
 
