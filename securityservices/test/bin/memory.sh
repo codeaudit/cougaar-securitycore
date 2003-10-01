a62e91e555fbe4971a82d91a95a91ac05a6d61d4
srosset <srosset>
2003-10-01 19:49:49 +0000

    Using runallparallel

--- securityservices/test/bin/memory.sh
+++ securityservices/test/bin/memory.sh
@@ -2,7 +2,7 @@
 
 rm topOut
 while true; do
-$CIP/operator/runall 'top -b -n 1 | grep java | sort' | tee -a topOut
+$CIP/operator/runallparallel 'top -b -n 1 | grep java | sort &' | tee -a topOut
 date | tee -a topOut
-sleep 300
+sleep 10
 done
