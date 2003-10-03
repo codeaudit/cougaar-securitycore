d41a840773531ab4ad7213f5ea4827cc7478e055
rliao1 <rliao1>
2003-10-03 03:19:21 +0000

    add new interface addAvailabilityListener

--- securityservices/src/org/cougaar/core/security/services/crypto/CertValidityService.java
+++ securityservices/src/org/cougaar/core/security/services/crypto/CertValidityService.java
@@ -30,12 +30,19 @@ import org.cougaar.core.component.Service;
 import org.cougaar.core.security.crypto.CertValidityListener;
 
 public interface CertValidityService extends Service {
+// will make the certificate if it is not present, this should only be used for local agents!
   public void addValidityListener(CertValidityListener listener);
 
   public void updateCertificate(String commonName);
 
   public void invalidate(String cname);
 
+// only listen when cert becomes invalid, does not check and make cert, there is a possibility that the agent is NOT local
+// used for messaging or ssl
   public void addInvalidateListener(CertValidityListener listener);
+
+// only listens when a cert becomes available, does not check make the cert if it is not available. there is a possibility that the agent is NOT local
+// used for name server certificate update
+  public void addAvailabilityListener(CertValidityListener listener);
 }
 
