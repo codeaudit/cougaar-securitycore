35ea526ed6561e80303f5246e165b150e72ba86f
rliao1 <rliao1>
2003-10-29 19:55:45 +0000

    the 2 classes has already been added by sebastien

--- securityservices/src/java/util/singleton/CollectionMonitorStatsImpl.java
+++ securityservices/src/java/util/singleton/CollectionMonitorStatsImpl.java
@@ -137,12 +137,6 @@ public class CollectionMonitorStatsImpl
   public void addStack(Stack s) {
     addElement(s, Stack.class);
   }
-  public void addLinkedHashMap(LinkedHashMap m) {
-    addElement(m, LinkedHashMap.class);
-  }
-  public void addLinkedHashSet(LinkedHashSet s) {
-    addElement(s, LinkedHashSet.class);
-  }
   public void addIdentityHashMap(IdentityHashMap m) {
     addElement(m, IdentityHashMap.class);
   }
