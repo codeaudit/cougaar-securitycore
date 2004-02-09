4a54031cf9d5f4f89711706d9aa074520142723d
tredmond <tredmond>
2004-02-09 03:21:38 +0000

    The web proxy now intercepts only the new owl web pages with the slightly different format:
             http://ontology.coginst.uwf.edu/... .daml
    
    is now
    
            http://ontology.ihmc.us/... .owl

--- securityservices/src/org/cougaar/core/security/policy/webproxy/DamlURLStreamHandler.java
+++ securityservices/src/org/cougaar/core/security/policy/webproxy/DamlURLStreamHandler.java
@@ -59,8 +59,8 @@ class DamlURLStreamHandler extends URLStreamHandler
     if (_log != null && _log.isDebugEnabled()) {
       _log.debug("Opening url " + u.toString());
     }
-    if (u.toString().endsWith(".daml") && 
-        u.toString().startsWith("http://ontology.coginst.uwf.edu")) {
+    if (u.toString().endsWith(".owl") && 
+        u.toString().startsWith("http://ontology.ihmc.us")) {
       if (_log != null && _log.isDebugEnabled()) {
         _log.debug("Using proxy");
         _log.debug("++++++++++++++++++++++++++++++++++++++++++++++++");
