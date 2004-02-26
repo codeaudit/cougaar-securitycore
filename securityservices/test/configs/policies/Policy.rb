ad2a8f83e594735ee9950370f98de912fa6a0d57
tredmond <tredmond>
2004-02-26 10:08:35 +0000

    *** empty log message ***

--- securityservices/test/configs/policies/Policy.rb
+++ securityservices/test/configs/policies/Policy.rb
@@ -45,65 +45,30 @@ Cougaar.new_experiment("Policy-Test").run(1) {
   # rule, which looks in the operator directory.  The code below 
   # will work in a stand-alone ACME setup. 
 
- # hosts_file = File.join(CIP, 'configs', 'ping', 'hosts.xml')
    hosts_file = "hosts.xml"
 
-#  hosts_file = nil 
-#  host = @hostname unless host 
-#  Dir.glob(File.join(".", "*hosts.xml")).each do |file| 
-#    ts = Cougaar::SocietyBuilder.from_xml_file(file).society 
-#    hosts_file = file 
-#  end 
  
   # transform the basic society to use our host-node layout 
   do_action "LayoutSociety", "#{layout_file}-layout.xml", hosts_file 
- 
- 
-  # load local rules (ping_env.rule) 
+
   do_action "TransformSociety", false, 
     ".",
-    "#{RULES}/isat/nameserver.rule",
-    "#{RULES}/isat/default_servlets.rule",
-    "#{RULES}/isat/root_mobility_plugin.rule",
-    "#{RULES}/isat/logging_config_servlet.rule",
-    "#{RULES}/isat/community_plugin.rule",
-    "#{RULES}/isat/show_jars.rule",
+    "#{RULES}/isat",
     "#{RULES}/security"
- 
-  # Build the communities.xml file 
-  do_action "TransformSociety", false, 
-    "#{RULES}/security/communities" 
  
   # optional: save the society to an XML file for easy debugging 
   do_action "SaveCurrentSociety", "mySociety.xml" 
   do_action "SaveCurrentCommunities", "myCommunities.xml" 
 
-  # Uncomment the following line if working in the CSI testbed
-  #  do_action "SetAcmeUser" 
 
   # start jabber 
   # 
   # replace the last parameter with your jabber server's host name  
+
   do_action "StartJabberCommunications"
-#  do_action "CleanupSociety"
-   do_action "VerifyHosts" 
-  do_action "ConnectOperatorService"
-#  do_action "ClearPersistenceAndLogs"
-#  do_action "GenericAction" do 
-#    `rm -rf #{CIP}/workspace/log4jlogs #{CIP}/workspace/P`
-#     `killall java`
-#     `rm -rf #{CIP}/workspace`
-#  end
+  do_action "VerifyHosts" 
+
   do_action "DeployCommunitiesFile" 
- 
-  # optional: print the cougaar events 
-  # 
-  # this will also print the ping statistics events 
-#  do_action "GenericAction" do |run| 
-#     run.comms.on_cougaar_event do |event| 
-#       puts event 
-#     end 
-#  end 
  
   do_action "StartSociety" 
 
