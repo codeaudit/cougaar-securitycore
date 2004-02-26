ae30c4d632a91a87bc9a9e218171a98fef6f2e23
srosset <srosset>
2004-02-26 02:11:02 +0000

    *** empty log message ***

---
+++ NetworkAnalysis/src/com/cougaarsoftware/nettool/LogParser.java
@@ -0,0 +1,24 @@
+/*
+ * Created on Feb 25, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.cougaarsoftware.nettool;
+
+import java.io.File;
+
+/**
+ * @author srosset
+ *
+ * Converts Cougaar network log files into Pajek or GraphML format.
+ */
+public class LogParser {
+
+	public void parseCougaarLogFiles(String []files) {
+		for (int i = 0 ; i < files.length ; i++) {
+			File f = new File(files[i]);
+			
+		}
+	}
+}
