468d21b959f6cbc08ccc2badb4bbb178f31042a9
srosset <srosset>
2004-04-09 17:17:43 +0000

    Updated script for new Cougaar

--- securityservices/test/bin/memory.sh
+++ securityservices/test/bin/memory.sh
@@ -2,11 +2,11 @@
 
 rm topOut
 while true; do
-#$CIP/operator/runallparallel 'ps -o pid,user,utime,size,vsize,cmd -C java --no-headers' | tee -a topOut
-#$CIP/operator/runallparallel 'top -b -n 1 | grep java | sort' | tee -a topOut
-$CIP/operator/runallparallel '${CIP}/operator/printProcessInfo.rb' | tee -a topOut
+  #$CIP/operator/runallparallel 'ps -o pid,user,utime,size,vsize,cmd -C java --no-headers' | tee -a topOut
+  #$CIP/operator/runallparallel 'top -b -n 1 | grep java | sort' | tee -a topOut
+  $CIP/operator/runall '${CIP}/operator/printProcessInfo.rb' | tee -a topOut
 
-#date | tee -a topOut
-echo "" | tee -a topOut
-sleep 10
+  #date | tee -a topOut
+  echo "" | tee -a topOut
+  sleep 10
 done
