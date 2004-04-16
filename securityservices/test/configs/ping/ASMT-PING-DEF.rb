5e2ca988d9cf849471a494854a685f3945e8a955
srosset <srosset>
2004-04-16 23:40:17 +0000

    Added event log dumper

--- securityservices/test/configs/ping/ASMT-PING-DEF.rb
+++ securityservices/test/configs/ping/ASMT-PING-DEF.rb
@@ -22,8 +22,8 @@ parameters:
     - $CIP/csmart/config/rules/security/testCollectData
    # ###
    # Redundant CA and persistence managers
-    #- $CIP/csmart/config/rules/security/redundancy
-    #- $CIP/csmart/config/rules/security/robustness
+#    - $CIP/csmart/config/rules/security/redundancy
+#    - $CIP/csmart/config/rules/security/robustness
    # ###
 # ######################################################
 #    - $CIP/csmart/config/rules/security/robustness
@@ -47,10 +47,13 @@ include_scripts:
   - script: $CIP/csmart/lib/security/scripts/revoke_agent_and_node_cert.rb
   - script: $CIP/csmart/lib/security/scripts/stress_security_uc1.rb
   - script: $CIP/csmart/lib/security/scripts/stress_security_uc3.rb
+#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc4.rb
   - script: $CIP/csmart/lib/security/scripts/stress_security_uc5.rb
   - script: $CIP/csmart/lib/security/scripts/threatcon_level_change.rb
   - script: $CIP/csmart/lib/security/scripts/invalid_community_request.rb
   - script: $CIP/csmart/lib/security/scripts/check_mop.rb
+  - script: $CIP/csmart/lib/security/scripts/parseResults.rb
+  - script: $CIP/csmart/lib/security/scripts/saveAcmeEvents.rb
 # ######################################################
 #  - script: setup_robustness.rb
 #  - script: network_shaping.rb
