37fdcae166983679b2a9febfd5553e2744f46b13
rliao1 <rliao1>
2004-04-19 21:58:34 +0000

    add trusted ca policy and certificate, for redundant root CA so that CAs will not complain about certificates from the other root

---
+++ securityservices/src/org/cougaar/core/security/crypto/TrustedCAConfigPlugin.java
@@ -0,0 +1,70 @@
+package org.cougaar.core.security.crypto;
+
+import org.cougaar.core.security.certauthority.ConfigPlugin;
+import org.cougaar.core.security.certauthority.servlet.CAInfo;
+import org.cougaar.core.security.policy.TrustedCaPolicy;
+import org.cougaar.core.security.certauthority.servlet.CAInfo;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+/*
+ * This class only add trusted policy and trusted CA certificate,
+ * it does not request certificate from the CA, this is used for 
+ * multiple root CA so that their subordinate CAs can trust each other.
+ */
+
+public class TrustedCAConfigPlugin extends ConfigPlugin {
+  Vector calist = new Vector();
+
+  public void setParameter(Object o) {
+    if (!(o instanceof List)) {
+      throw new IllegalArgumentException("Expecting a List argument to setParameter");
+    }
+    List l = (List) o;
+    if (l.size() == 0) {
+      System.out.println("No CA assigned!");
+    }
+    Iterator it = l.iterator();
+    while (it.hasNext()) {
+      String param = (String)it.next();
+      calist.addElement(param);
+    }
+
+  }
+
+  protected void execute() {
+    if (log.isDebugEnabled()) {
+      log.debug("executing ... ");
+    }
+    for (int i = 0; i < calist.size(); i++) {
+      addTrustedPolicy((String)calist.elementAt(i));
+    }
+  }
+
+  /** Need to synchronize because for normal node there could be multiple threads
+   */
+  protected synchronized void checkOrMakeIdentity(CAInfo info, String requestURL) {
+    // check whether already received the policy
+    TrustedCaPolicy [] tc = cryptoClientPolicy.getIssuerPolicy();
+    boolean newPolicy = true;
+    for (int i = 0; i < tc.length; i++) {
+      if (tc[i].caDN.equals(info.caPolicy.caDN)) {
+        newPolicy = false;
+        break;
+      }
+    }
+    if (newPolicy) {
+      setCAInfo(info, requestURL);
+    }
+    TrustedCaPolicy tcp = info.caPolicy;
+
+    // request certificates from the particular CA that has started
+    // the certificate may have been created
+    if (log.isDebugEnabled()) {
+      log.debug("CA " + info.caPolicy.caDN + " added as trusted CA.");
+    }
+
+  }
+}
