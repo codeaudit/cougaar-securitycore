b097a62f3d23aab3f5617881b429bcdd916152f6
rtripath <rtripath>
2004-04-22 21:40:22 +0000

    * modified script to set a boolean if it is a ping society

--- securityTests/security/scripts/setup_society_ping.rb
+++ securityTests/security/scripts/setup_society_ping.rb
@@ -1,7 +1,9 @@
 
 require 'security/lib/scripting'
 require 'security/actions/ping_active'
+require 'security/lib/security'
 
+PingSociety.setPingSociety
 insert_before :wait_for_initialization do
   wait_for  "PingActive", 20.minutes
 end
