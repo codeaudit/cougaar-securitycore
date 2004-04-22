6bd02996f08bd5fc441b627ed65eeef859bf2f8b
rtripath <rtripath>
2004-04-22 21:34:44 +0000

    * modified script to use saveAssertion

--- securityTests/security/lib/stresses/3c2.rb
+++ securityTests/security/lib/stresses/3c2.rb
@@ -6,238 +6,239 @@ require 'security/lib/rules'
 
 class Security3c2 < SecurityStressFramework
 
-   def initialize(run)
-     super(run)
-      @useIdmef = true
-      @revoked_node = nil
-      @revoked_agent = nil
-      @ssl_initiator_nodes = {}
-      @ssl_receiver_nodes = {}
-      @msg_nodes = {}
-      @src_agent = nil
-      @dest_agent = nil
-   end
+  def initialize(run)
+    super(run)
+    @useIdmef = true
+    @revoked_node = nil
+    @revoked_agent = nil
+    @ssl_initiator_nodes = {}
+    @ssl_receiver_nodes = {}
+    @msg_nodes = {}
+    @src_agent = nil
+    @dest_agent = nil
+  end
 
-   def getStressIds()
-     return ["Stress3b9", "Stress3c9", "Stress3c21", "Stress5k103", "Stress5k104", "Stress3c2", "Stress3c5"]
-   end
+  def getStressIds()
+    return ["Stress3b9", "Stress3c9", "Stress3c21", "Stress5k103", "Stress5k104", "Stress3c2", "Stress3c5"]
+  end
 
-   def postLoadSociety
-      @caDomains = CaDomains.instance
-      @caDomains.ensureExpectedEntities
+  def postLoadSociety
+    @caDomains = CaDomains.instance
+    @caDomains.ensureExpectedEntities
 
-      # Send servlet now installed through a rule
-      #installSendMessageServlet
-   end
+    # Send servlet now installed through a rule
+    #installSendMessageServlet
+  end
 
-   def postStartJabberCommunications
-     #printDotsOnCougaarEvents
-     if (@certRevocation == nil)
-       @certRevocation = CertRevocation.new
-     end
+  def postStartJabberCommunications
+    #printDotsOnCougaarEvents
+    if (@certRevocation == nil)
+      @certRevocation = CertRevocation.new
+    end
 
-     on_cougaar_event do |event|
-       begin
-	 # puts "event: #{event.event_type}, #{event.cluster_identifier}, #{event.component}, #{event.data.to_s}"
+    on_cougaar_event do |event|
+      begin
+        # puts "event: #{event.event_type}, #{event.cluster_identifier}, #{event.component}, #{event.data.to_s}"
 
-	 # message revoked Idmef message
-	 if event.event_type == 'STATUS' && event.component == 'IdmefEventPublisherPlugin' && event.data =~ /MESSAGE_FAILURE/
-	   #saveUnitTestResult('5k', "event: #{event.cluster_identifier}, #{event.component}, #{event.data}" )
-	   if event.data =~ /@revoked_agent/
-	     @msg_nodes[event.cluster_identifier] = @revoked_agent
-	   end
-	 end
+        # message revoked Idmef message
+        if event.event_type == 'STATUS' && event.component == 'IdmefEventPublisherPlugin' && event.data =~ /MESSAGE_FAILURE/
+          #saveUnitTestResult('5k', "event: #{event.cluster_identifier}, #{event.component}, #{event.data}" )
+          if event.data =~ /@revoked_agent/
+            @msg_nodes[event.cluster_identifier] = @revoked_agent
+          end
+        end
 
-	 # SSL revoked Idmef message
-	 msgPattern = /CertificateRevoked([^)]*)/m
-	 if event.event_type == 'STATUS' && event.data =~ /Revoked/
-	   #saveUnitTestResult('5k', "event: #{event.cluster_identifier}, #{event.component}, #{event.data.to_s}" )
+        # SSL revoked Idmef message
+        msgPattern = /CertificateRevoked([^)]*)/m
+        if event.event_type == 'STATUS' && event.data =~ /Revoked/
+          #saveUnitTestResult('5k', "event: #{event.cluster_identifier}, #{event.component}, #{event.data.to_s}" )
 
-	   name = event.data.scan(msgPattern)
-	   raise "wrong revocation message found #{event.data}" unless name != []
+          name = event.data.scan(msgPattern)
+          raise "wrong revocation message found #{event.data}" unless name != []
           
-	   agent_name = name[0].to_s.split('(')[1]
-	   if agent_name == @revoked_node.name
-	     if event.data =~ /ServerTrustManager/
-	       # summary "Confirmed SSL failure on revoked certificate for node #{agent_name} as initiator."
-	       @ssl_receiver_nodes[event.cluster_identifier] = @revoked_node.name
-	     elsif event.data =~ /ClientTrustManager/
-	       # summary "Confirmed SSL failure on revoked certificate for node #{agent_name} as receiver."
-	       @ssl_initiator_nodes[event.cluster_identifier] = @revoked_node.name
-	     end
-	   end # if agent_name
-	 end # if event.event_type
-       rescue => ex
-	 logInfoMsg "Exception while processing event: #{event.to_s} - #{ex}\n #{ex.backtrace.join('\n')}"
-       end
-     end # on_cougaar_events
-   end
-
-#   def postConditionalStartSociety
-#      sleep 5.minutes unless $WasRunning
-
-   def revokeNode(node)
-      # Give the agents time to retrieve their certificates
-      # user admin may not be started yet
-
-      if (node == nil)
-        saveResult("Stress5k103", "Error: could not find node to revoke")
+          agent_name = name[0].to_s.split('(')[1]
+          if agent_name == @revoked_node.name
+            if event.data =~ /ServerTrustManager/
+              # summary "Confirmed SSL failure on revoked certificate for node #{agent_name} as initiator."
+              @ssl_receiver_nodes[event.cluster_identifier] = @revoked_node.name
+            elsif event.data =~ /ClientTrustManager/
+              # summary "Confirmed SSL failure on revoked certificate for node #{agent_name} as receiver."
+              @ssl_initiator_nodes[event.cluster_identifier] = @revoked_node.name
+            end
+          end # if agent_name
+        end # if event.event_type
+      rescue => ex
+        logInfoMsg "Exception while processing event: #{event.to_s} - #{ex}\n #{ex.backtrace.join('\n')}"
       end
-      result = @certRevocation.revokeNode(node)
-      saveResult(result, "Stress5k103",
-         "revoke a node through administrator: #{node.name}")
+    end # on_cougaar_events
+  end
 
-      sleep(3.minutes)
+  #   def postConditionalStartSociety
+  #      sleep 5.minutes unless $WasRunning
 
-      summary "The following nodes received SSL connection request from revoked node:"
-      summary "#{@ssl_receiver_nodes.keys.to_s}"
-      result = false unless @ssl_receiver_nodes.keys.size != 0
-      saveResult(result, "Stress3c2", "SSL initiated by node with revoked certificate.")
+  def revokeNode(node)
+    # Give the agents time to retrieve their certificates
+    # user admin may not be started yet
 
-      summary "The following nodes initiated SSL connection request to revoked node:"
-      summary "#{@ssl_initiator_nodes.keys.to_s}"
-      result = false unless @ssl_initiator_nodes.keys.size != 0
-      saveResult(result, "Stress3c5", "SSL received by node with revoked certificate.")
-   end
+    if (node == nil)
+      saveResult("false","Stress5k103", "Error: could not find node to revoke")
+    end
+    result = @certRevocation.revokeNode(node)
+    saveResult(result, "Stress5k103",
+               "revoke a node through administrator: #{node.name}")
 
-   def revokeAgent(agent)
-     agent1 = @run.society.agents[agent]
-     if (agent1 == nil) 
-       raise "Unable to find source agent: #{agent}"
-     end
+    sleep(3.minutes)
 
-     # Preven CRL to reach revoked agent.
-     # That way, the revoked agent will succeed sending a message out,
-     # and we can test that the receiver is blocking the message.
-     url = "#{agent1.uri}/crlMessageBinderServlet?crlEnqueueMsg=true"
-     result, url = Cougaar::Communications::HTTP.get(url)
-     if !(result =~ /Success/)
-       saveUnitTestResult("Stress5k104", "Unable to block CRL msg at #{agent1.name}\nURL: #{url}\n#{result}")
-     end
+    summary "The following nodes received SSL connection request from revoked node:"
+    summary "#{@ssl_receiver_nodes.keys.to_s}"
+    result = false unless @ssl_receiver_nodes.keys.size != 0
+    saveResult(result, "Stress3c2", "SSL initiated by node with revoked certificate.")
 
-     # Now, revoke the agent.
-     result = @certRevocation.revokeAgent(agent)
-     saveResult(result, "Stress5k104",
-		"revoke an agent through administrator: #{agent}")
+    summary "The following nodes initiated SSL connection request to revoked node:"
+    summary "#{@ssl_initiator_nodes.keys.to_s}"
+    result = false unless @ssl_initiator_nodes.keys.size != 0
+    saveResult(result, "Stress3c5", "SSL received by node with revoked certificate.")
+  end
 
-     #@dest_agent = @certRevocation.selectAgent
-     @dest_agent = getValidDestAgent()
-     agent2 = @dest_agent
-     if (agent2 == nil) 
-       raise "Unable to find destination agent"
-     end
+  def revokeAgent(agent)
+    agent1 = @run.society.agents[agent]
+    if (agent1 == nil) 
+      raise "Unable to find source agent: #{agent}"
+    end
 
-     # Sleep a little bit until the receiver receives the CRL
-     sleep(5.minutes)
+    # Preven CRL to reach revoked agent.
+    # That way, the revoked agent will succeed sending a message out,
+    # and we can test that the receiver is blocking the message.
+    url = "#{agent1.uri}/crlMessageBinderServlet?crlEnqueueMsg=true"
+    result, url = Cougaar::Communications::HTTP.get(url)
+    if !(result =~ /Success/)
+      saveAssertion("Stress5k104", "Unable to block CRL msg at #{agent1.name}\nURL: #{url}\n#{result}")
+    end
 
-     saveUnitTestResult("Stress5k104",
-			"Sending msg from #{agent1.name} to #{agent2.name}... Sender does not have CRL")
-     testMessage(agent1, agent2, "Sender does not have CRL")      
+    # Now, revoke the agent.
+    result = @certRevocation.revokeAgent(agent)
+    saveResult(result, "Stress5k104",
+               "revoke an agent through administrator: #{agent}")
 
-     # Now, re-enable CRL to reach the revoked agent.
-     url = "#{agent1.uri}/crlMessageBinderServlet?crlEnqueueMsg=false"
-     result, url = Cougaar::Communications::HTTP.get(url)
-     if !(result =~ /Success/)
-       saveUnitTestResult("Stress5k104", "Unable to re-enable CRL msg at #{agent1.name}")
-     end
+    #@dest_agent = @certRevocation.selectAgent
+    @dest_agent = getValidDestAgent()
+    agent2 = @dest_agent
+    if (agent2 == nil) 
+      raise "Unable to find destination agent"
+    end
 
-     # Now that the sender has CRLs, try again.
-     # The sender should not even send the relay message.
-     saveUnitTestResult("Stress5k104", "Sending msg from #{agent1.name} to #{agent2.name}... Sender should have CRL")
-     testMessage(agent1, agent2, "Sender has CRL")
-   end
+    # Sleep a little bit until the receiver receives the CRL
+    sleep(5.minutes)
 
-   def getValidDestAgent
-     agentName = nil
-     run.society.each_agent(true) do |agent|
-       if !(agent.has_facet? "AgentAttacker")
-	 agentName = agent
-	 break
-       end
-     end
-     return agentName
-   end
+    saveAssertion("Stress5k104",
+                  "Sending msg from #{agent1.name} to #{agent2.name}... Sender does not have CRL")
+    testMessage(agent1, agent2, "Sender does not have CRL")      
 
-   def getAttackAgent
-     agentName = nil
-     run.society.each_agent(true) do |agent|
-       if agent.has_facet? "AgentAttacker"
-	 agentName = agent
-	 break
-       end
-     end
-     return agentName
-   end
+    # Now, re-enable CRL to reach the revoked agent.
+    url = "#{agent1.uri}/crlMessageBinderServlet?crlEnqueueMsg=false"
+    result, url = Cougaar::Communications::HTTP.get(url)
+    if !(result =~ /Success/)
+      saveAssertion("Stress5k104", "Unable to re-enable CRL msg at #{agent1.name}")
+    end
 
-   def getAttackNode
-     nodeName = nil
-     run.society.each_node do |node|
-       if node.has_facet? "NodeAttacker"
-	 nodeName = node
-	 break
-       end
-     end
-     return nodeName
-   end
+    # Now that the sender has CRLs, try again.
+    # The sender should not even send the relay message.
+    saveAssertion("Stress5k104", "Sending msg from #{agent1.name} to #{agent2.name}... Sender should have CRL")
+    testMessage(agent1, agent2, "Sender has CRL")
 
-   def revokeAgentAndNode
-     Thread.fork {
-       begin
-	 @revoked_agent = getAttackAgent()
-	 @revoked_node = getAttackNode()
-	 revokeAgent(@revoked_agent.name)
-	 sleep(10.minutes)
-	 revokeNode(@revoked_node)
+  end
 
-	 sleep(5.minutes)
-	 # Request new certificates for the node and agent
-	 begin
-	   logInfoMsg "Requesting new cert for #{@revoked_node.agent.name}"
-	   @certRevocation.requestNewCertificate(@revoked_node, @revoked_node.agent, "10 d")
-	   saveUnitTestResult('Stress3c2', "Requested new cert for #{@revoked_node}")
+  def getValidDestAgent
+    agentName = nil
+    run.society.each_agent(true) do |agent|
+      if !(agent.has_facet? "AgentAttacker")
+        agentName = agent
+        break
+      end
+    end
+    return agentName
+  end
 
-	   logInfoMsg "Requesting new cert for #{@revoked_agent.name}"
-	   @certRevocation.requestNewCertificate(@revoked_node, @revoked_agent, "10 d")
-	   saveUnitTestResult('Stress3c2', "Requested new cert for #{@revoked_agent}")
-	 rescue => ex2
-	   saveUnitTestResult('Stress3c2',
-              "Unable to request new cert: #{ex2}\n#{ex2.backtrace.join("\n")}")
-	 end
-       rescue => ex
-	 saveUnitTestResult('Stress3c2', "Unable to run test: #{ex}\n#{ex.backtrace.join("\n")}" )
-       end
-     }
-   end
+  def getAttackAgent
+    agentName = nil
+    run.society.each_agent(true) do |agent|
+      if agent.has_facet? "AgentAttacker"
+        agentName = agent
+        break
+      end
+    end
+    return agentName
+  end
 
-   def postConditionalGLSConnection
-     Thread.fork {
-       @revoked_node = @certRevocation.selectNode
-       revokeNode(@revoked_node)
+  def getAttackNode
+    nodeName = nil
+    run.society.each_node do |node|
+      if node.has_facet? "NodeAttacker"
+        nodeName = node
+        break
+      end
+    end
+    return nodeName
+  end
 
-       @revoked_agent = @certRevocation.selectAgent
-       revokeAgent(@revoked_agent)
-     }
-   end
+  def revokeAgentAndNode
+    Thread.fork {
+      begin
+        @revoked_agent = getAttackAgent()
+        @revoked_node = getAttackNode()
+        revokeAgent(@revoked_agent.name)
+        sleep(10.minutes)
+        revokeNode(@revoked_node)
 
-   def testMessage(agent1, agent2, comment)
+        sleep(5.minutes)
+        # Request new certificates for the node and agent
+        begin
+          logInfoMsg "Requesting new cert for #{@revoked_node.agent.name}"
+          @certRevocation.requestNewCertificate(@revoked_node, @revoked_node.agent, "10 d")
+          saveAssertion('Stress3c2', "Requested new cert for #{@revoked_node}")
+          logInfoMsg "Requesting new cert for #{@revoked_agent.name}"
+          @certRevocation.requestNewCertificate(@revoked_node, @revoked_agent, "10 d")
+          saveAssertion('Stress3c2', "Requested new cert for #{@revoked_agent}")
+        rescue => ex2
+          saveAssertion('Stress3c2',
+                        "Unable to request new cert: #{ex2}\n#{ex2.backtrace.join("\n")}")
+          
+        end
+      rescue => ex
+        saveAssertion('Stress3c2', "Unable to run test: #{ex}\n#{ex.backtrace.join("\n")}" )
+      end
+    }
+  end
+
+  def postConditionalGLSConnection
+    Thread.fork {
+      @revoked_node = @certRevocation.selectNode
+      revokeNode(@revoked_node)
+
+      @revoked_agent = @certRevocation.selectAgent
+      revokeAgent(@revoked_agent)
+    }
+  end
+
+  def testMessage(agent1, agent2, comment)
     if (@useIdmef)
       testMessageIdmef(agent1.name, agent2.name,
-        'Stress3c21',
-	"Send message with expired cert IDMEF - " + comment,
-        [ true, false, false, false ],
-        agent1.node.agent.name)
+                       'Stress3c21',
+                       "Send message with expired cert IDMEF - " + comment,
+                       [ true, false, false, false ],
+                       agent1.node.agent.name)
       testMessageIdmef(agent2.name, agent1.name,
-        'Stress3c21',
-	"Receive message with expired cert IDMEF - " + comment, 
-        [ true, false, false, false ],
-        agent1.node.agent.name)
+                       'Stress3c21',
+                       "Receive message with expired cert IDMEF - " + comment, 
+                       [ true, false, false, false ],
+                       agent1.node.agent.name)
     end
     testMessageFailure(agent1.name, agent2.name, 
-            'Stress3c9', "Send message with expired cert - " + comment, 
-             [ true, false, false, false ])
+                       'Stress3c9', "Send message with expired cert - " + comment, 
+                       [ true, false, false, false ])
     testMessageFailure(agent2.name, agent1.name, 
-            'Stress3b9', "Receive message with expired cert - " + comment, 
-            [ true, false, false, false ])
-   end
+                       'Stress3b9', "Receive message with expired cert - " + comment, 
+                       [ true, false, false, false ])
+  end
 
 end
