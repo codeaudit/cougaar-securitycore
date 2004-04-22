e98f67a82507afc53eaacb32419a96e90b461864
srosset <srosset>
2004-04-22 00:05:26 +0000

    Added stresses

--- securityservices/test/configs/acme_scripts/moas/ASMT-1000-UA-DEF.rb
+++ securityservices/test/configs/acme_scripts/moas/ASMT-1000-UA-DEF.rb
@@ -18,6 +18,7 @@ parameters:
 # ############################################################
 # Security rules
     - $CIP/csmart/config/rules/security
+#    - $CIP/csmart/config/rules/security/mop
     - $CIP/csmart/config/rules/security/testCollectData
    # ###
    # Redundant CA and persistence managers
@@ -39,9 +40,22 @@ include_scripts:
 # ############################################################
 # Security scripts
   - script: $CIP/csmart/lib/security/scripts/setup_scripting.rb
-#  - script: $CIP/csmart/lib/security/scripts/setup_society_1000_ua.rb
   - script: $CIP/csmart/lib/security/scripts/setup_userManagement.rb
+#  - script: $CIP/csmart/lib/security/scripts/setup_society_1000_ua.rb
 #  - script: $CIP/csmart/lib/security/scripts/check_wp.rb
+#  - script: $CIP/csmart/lib/security/scripts/revoke_agent_and_node_cert.rb
+#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc1.rb
+#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc3.rb
+#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc4.rb
+#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc5.rb
+#  - script: $CIP/csmart/lib/security/scripts/threatcon_level_change.rb
+#  - script: $CIP/csmart/lib/security/scripts/invalid_community_request.rb
+#  - script: $CIP/csmart/lib/security/scripts/check_mop.rb
+#  - script: $CIP/csmart/lib/security/scripts/parseResults.rb
+#  - script: $CIP/csmart/lib/security/scripts/saveAcmeEvents.rb
+  - script: $CIP/csmart/lib/security/scripts/security_archives.rb
+  - script: $CIP/csmart/lib/security/scripts/cleanup_society.rb
+
 # ############################################################
 #  - script: setup_robustness.rb
 #  - script: network_shaping.rb
