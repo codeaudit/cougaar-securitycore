c1e0b1888ce17f473e2b958e054afc8ce8a30d02
rliao1 <rliao1>
2004-04-23 00:19:49 +0000

    fix the issue where two certificates are created. monitor thread is creating the extra one when it takes too long to create the first one.

--- securityservices/src/org/cougaar/core/security/crypto/CertificateValidityMonitor.java
+++ securityservices/src/org/cougaar/core/security/crypto/CertificateValidityMonitor.java
@@ -35,11 +35,15 @@ import org.cougaar.core.service.LoggingService;
 import org.cougaar.core.component.ServiceAvailableEvent;
 import org.cougaar.core.component.ServiceAvailableListener;
 import org.cougaar.core.service.ThreadService;
+import org.cougaar.core.security.policy.TrustedCaPolicy;
+
+import sun.security.x509.X500Name;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Vector;
 import java.util.List;
+import java.util.Iterator;
 import java.util.ArrayList;
 
 public class CertificateValidityMonitor
@@ -49,6 +53,7 @@ public class CertificateValidityMonitor
   LoggingService log = null;
 
   static Hashtable certListeners = new Hashtable();
+  static Hashtable _certRequests = new Hashtable();
   static List validityListeners = new ArrayList();
   static List availListeners = new ArrayList();
   static List invalidatedNames = new ArrayList();
@@ -104,6 +109,20 @@ public class CertificateValidityMonitor
   public void startThread(ThreadService threadService) {
     threadService.getThread(this, new ValidityMonitor()).
       schedule(0, sleep_time);
+
+    // start another thread for certificate requests that fail
+    // this thread should be much faster
+    int waittime = 10000;
+      try {
+        String waitPoll = System.getProperty("org.cougaar.core.security.configpoll", "5000");
+        waittime = Integer.parseInt(waitPoll);
+      } catch (Exception ex) {
+        if (log.isWarnEnabled()) {
+          log.warn("Unable to parse configpoll property: " + ex.toString());
+        }
+      }
+    threadService.getThread(this, new CertRequestMonitor()).
+      schedule(0, waittime);
   }
 
 /*
@@ -134,7 +153,7 @@ public class CertificateValidityMonitor
       }
 
       Vector list = new Vector();
-      keyRing.checkExpiry(NodeInfo.getNodeName());
+      checkExpiry(NodeInfo.getNodeName());
       // node in priority
       // CA cert should not be in validity checking, otherwise CA cert should be
       // checked first
@@ -147,10 +166,72 @@ public class CertificateValidityMonitor
             log.warn(commonName + " has been revoked, not generating request to renew certificate");
             return;
           }
-          keyRing.checkExpiry(commonName);
+          checkExpiry(commonName);
         }
       }
     }
+  }
+
+  private void checkExpiry(String commonName) {
+    // if the first certificate is not generated yet, no need to checkExpiry
+    List list = keyRing.findCert(commonName, KeyRingService.LOOKUP_KEYSTORE, false);
+    if (list == null || list.size() == 0) {
+      if (log.isDebugEnabled()) {
+        log.debug("Certificate for " + commonName + " is not generated yet, not checking expiry.");
+      }
+      return;
+    }
+
+    keyRing.checkExpiry(commonName);
+  }
+
+  private class CertRequestMonitor implements Runnable {
+    public void run() {
+      Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+
+      if(log.isDebugEnabled()) {
+        log.debug("CertRequestor Thread running ******************");
+      }
+
+      synchronized (_certRequests) {
+        for (Iterator it = _certRequests.values().iterator(); it.hasNext(); ) {
+          CertRequestInfo info = (CertRequestInfo)it.next();
+          if (log.isDebugEnabled()) {
+            log.debug("processing " + info.m_dname);
+          }
+          keyRing.checkOrMakeCert(info.m_dname, info.m_isCA, info.m_trustPolicy);
+          List list = keyRing.findCert(info.m_dname, KeyRing.LOOKUP_KEYSTORE, true);
+          if (list != null && list.size() != 0) {
+            if (log.isDebugEnabled()) {
+              log.debug("Acquired " + info.m_dname);
+            }
+
+            keyRing.updateNS(info.m_dname);
+            it.remove();
+          }
+        }
+      }
+    }
+  }
+
+  public void addCertRequest(X500Name dname, boolean isCA, TrustedCaPolicy trustPolicy) {
+    if (log.isDebugEnabled()) {
+      log.debug("addCertRequest: " + dname + " policy " + trustPolicy);
+    }
+    _certRequests.put(dname.toString(),
+      new CertRequestInfo(dname, isCA, trustPolicy));
+  }
+
+  class CertRequestInfo {
+    X500Name m_dname;
+    boolean m_isCA;
+    TrustedCaPolicy m_trustPolicy;
+
+    CertRequestInfo(X500Name dname, boolean isCA, TrustedCaPolicy trustPolicy) {
+      m_dname = dname;
+      m_isCA = isCA;
+      m_trustPolicy = trustPolicy;
+    }  
   }
 
   /*
