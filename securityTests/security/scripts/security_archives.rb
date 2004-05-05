66c2a3fd87927c589ddde0b95e37bed9ba7d9c73
srosset <srosset>
2004-05-05 01:57:31 +0000

    Added stack trace files

--- securityTests/security/scripts/security_archives.rb
+++ securityTests/security/scripts/security_archives.rb
@@ -11,6 +11,7 @@ insert_before :setup_run do
 
   do_action "MarkForArchive", "#{CIP}/workspace/auditlogs", "*", "Tomcat audit files"
   do_action "MarkForArchive", "#{CIP}/workspace/test", "*", "Security stresses results"
+  do_action "MarkForArchive", "#{CIP}/workspace/test/stacktraces", "*.log", "Security stresses results"
   do_action "MarkForArchive", "#{CIP}/workspace/security/keystores", "*", "Keystore files"
   do_action "MarkForArchive", "#{CIP}/workspace/security/mopresults", "*", "Blackboard access control MOP"
 end
