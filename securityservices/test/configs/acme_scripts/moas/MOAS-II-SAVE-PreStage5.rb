06990116b8ad7aefaf065215915039894f168d2b
rliao1 <rliao1>
2004-05-06 18:03:50 +0000

    save moas ii

---
+++ securityservices/test/configs/acme_scripts/moas/MOAS-II-SAVE-PreStage5.rb
@@ -0,0 +1,87 @@
+=begin experiment
+
+name: MOAS-II-Save
+description: MOAS-II save pre-Stage5
+script: $CIP/csmart/lib/isat/OPBaselineTemplate-ExtOplan.rb
+parameters:
+  - run_count: 1
+  - society_file: $CIP/csmart/config/societies/ua/full-tc20-232a703v.plugins.rb
+  - layout_file: $CIP/operator/layouts/FULL-UA-MNGR-33H63N-layout.xml
+#  - layout_file: $CIP/operator/layouts/04-OP-layout.xml
+  - archive_dir: $CIP/Logs
+  
+  - rules:
+    - $CIP/csmart/config/rules/isat
+    - $CIP/csmart/config/rules/yp
+    - $CIP/csmart/config/rules/logistics
+#    - $CIP/csmart/config/rules/assessment
+
+# ############################################################
+# Security rules
+    - $CIP/csmart/config/rules/security
+#    - $CIP/csmart/lib/security/rules
+#    - $CIP/csmart/config/rules/security/mop
+#    - $CIP/csmart/config/rules/security/testCollectData
+   # ###
+   # Redundant CA and persistence managers
+#    - $CIP/csmart/config/rules/security/redundancy
+#    - $CIP/csmart/config/rules/security/robustness
+   # Run with only redundant PM
+#    - $CIP/csmart/config/rules/security/redundancy/add_redundant_pm_facet.rule
+#    - $CIP/csmart/config/rules/security/redundancy/adjust_memory.rule
+#    - $CIP/csmart/config/rules/security/robustness/redundant_persistence_mgrs.rule
+
+   # ###
+# ############################################################
+# Robustness rules
+#    - $CIP/csmart/config/rules/robustness/manager.rule
+#    - $CIP/csmart/config/rules/robustness/uc1/aruc1.rule
+#    - $CIP/csmart/config/rules/robustness/uc1/debug/mic.rule
+#    - $CIP/csmart/config/rules/robustness/uc1/tuning/collect_stats.rule
+#    - $CIP/csmart/config/rules/robustness/uc9/deconfliction.rule
+# ############################################################
+  - community_rules:
+    - $CIP/csmart/config/rules/security/communities
+#    - $CIP/csmart/config/rules/robustness/communities
+
+
+include_scripts:
+  - script: $CIP/csmart/lib/isat/clearPnLogs.rb
+# ############################################################
+# Security scripts
+  - script: $CIP/csmart/lib/security/scripts/setup_scripting.rb
+  - script: $CIP/csmart/lib/security/scripts/setup_userManagement.rb
+  - script: $CIP/csmart/lib/security/scripts/log_node_process_info.rb
+#  - script: $CIP/csmart/lib/security/scripts/setup_society_1000_ua.rb
+#  - script: $CIP/csmart/lib/security/scripts/check_wp.rb
+#  - script: $CIP/csmart/lib/security/scripts/revoke_agent_and_node_cert.rb
+#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc1.rb
+#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc3.rb
+#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc4.rb
+#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc5.rb
+#  - script: $CIP/csmart/lib/security/scripts/threatcon_level_change.rb
+#  - script: $CIP/csmart/lib/security/scripts/invalid_community_request.rb
+#  - script: $CIP/csmart/lib/security/scripts/check_mop.rb
+#  - script: $CIP/csmart/lib/security/scripts/parseResults.rb
+  - script: $CIP/csmart/lib/security/scripts/saveAcmeEvents.rb
+  - script: $CIP/csmart/lib/security/scripts/security_archives.rb
+  - script: $CIP/csmart/lib/security/scripts/cleanup_society.rb
+
+# ############################################################
+#  - script: setup_robustness.rb
+#  - script: $CIP/csmart/lib/isat/network_shaping.rb
+#  - script: $CIP/csmart/lib/isat/datagrabber_include.rb
+#  - script: $CIP/csmart/assessment/assess/inbound_aggagent_include.rb
+#  - script: $CIP/csmart/assessment/assess/outofbound_aggagent_include.rb
+#  - script: $CIP/csmart/assessment/assess/cnccalc_include.rb
+
+# ############################################################ 
+  - script: $CIP/csmart/lib/isat/save_snapshot.rb
+    parameters:
+      - snapshot_name: $CIP/SAVE-PreStage5.tgz
+      - snapshot_location: before_stage_5
+
+=end
+
+require 'cougaar/scripting'
+Cougaar::ExperimentDefinition.register(__FILE__)
