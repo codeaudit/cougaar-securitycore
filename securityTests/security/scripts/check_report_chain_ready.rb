2e7f37adbc3c03318bb407262ab4d47156989c0c
tredmond <tredmond>
2004-05-06 18:54:14 +0000

    Enter the check_report_chain_ready in the right place.

--- securityTests/security/scripts/check_report_chain_ready.rb
+++ securityTests/security/scripts/check_report_chain_ready.rb
@@ -2,10 +2,11 @@
 require 'security/lib/scripting'
 require 'security/lib/stresses/reportChainReady'
 
-insert_after :setup_run do
+insert_before :setup_run do
    do_action "InjectStress", "TestReportChainReady", "beforeStartedSociety"
 end
 
-insert_after :society_running do
+insert_after :wait_for_initialization do
+   do_action "Sleep", 30.minutes
    do_action "InjectStress", "TestReportChainReady", "afterReportChainReady"
 end
