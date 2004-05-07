b3e84cc3bce6bae7a1e13f10a4cdad0d72292d4f
tredmond <tredmond>
2004-05-07 23:28:14 +0000

    Putting the second action after wait for initialization.  This action now sleeps for 5 minute intervals and grabs the state after each one.

--- securityTests/security/scripts/check_report_chain_ready.rb
+++ securityTests/security/scripts/check_report_chain_ready.rb
@@ -6,6 +6,6 @@ insert_before :setup_run do
    do_action "InjectStress", "TestReportChainReady", "beforeStartedSociety"
 end
 
-insert_before :society_running do
+insert_after :wait_for_initialization do
    do_action "InjectStress", "TestReportChainReady", "afterReportChainReady"
 end
