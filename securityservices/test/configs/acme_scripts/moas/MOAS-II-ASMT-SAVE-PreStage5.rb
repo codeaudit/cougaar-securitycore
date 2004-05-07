53fb13412d209780bd35496ec6752c56e272b653
srosset <srosset>
2004-05-07 23:35:38 +0000

    Enabling security stresses by default

--- securityservices/test/configs/acme_scripts/moas/MOAS-II-ASMT-SAVE-PreStage5.rb
+++ securityservices/test/configs/acme_scripts/moas/MOAS-II-ASMT-SAVE-PreStage5.rb
@@ -21,8 +21,8 @@ parameters:
 # ############################################################
 # Security rules
     - $CIP/csmart/config/rules/security
-#    - $CIP/csmart/lib/security/rules
-#    - $CIP/csmart/config/rules/security/mop
+    - $CIP/csmart/lib/security/rules
+    - $CIP/csmart/config/rules/security/mop
     - $CIP/csmart/config/rules/security/testCollectData/ServiceContractPlugin.rule
    # ###
    # Redundant CA and persistence managers
@@ -59,15 +59,15 @@ include_scripts:
 #  - script: $CIP/csmart/lib/security/scripts/setup_society_1000_ua.rb
   - script: $CIP/csmart/lib/security/scripts/check_wp.rb
   - script: $CIP/csmart/lib/security/scripts/check_report_chain_ready.rb
-#  - script: $CIP/csmart/lib/security/scripts/revoke_agent_and_node_cert.rb
-#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc1.rb
-#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc3.rb
-#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc4.rb
-#  - script: $CIP/csmart/lib/security/scripts/stress_security_uc5.rb
-#  - script: $CIP/csmart/lib/security/scripts/threatcon_level_change.rb
-#  - script: $CIP/csmart/lib/security/scripts/invalid_community_request.rb
-#  - script: $CIP/csmart/lib/security/scripts/check_mop.rb
-#  - script: $CIP/csmart/lib/security/scripts/parseResults.rb
+  - script: $CIP/csmart/lib/security/scripts/revoke_agent_and_node_cert.rb
+  - script: $CIP/csmart/lib/security/scripts/stress_security_uc1.rb
+  - script: $CIP/csmart/lib/security/scripts/stress_security_uc3.rb
+  - script: $CIP/csmart/lib/security/scripts/stress_security_uc4.rb
+  - script: $CIP/csmart/lib/security/scripts/stress_security_uc5.rb
+  - script: $CIP/csmart/lib/security/scripts/threatcon_level_change.rb
+  - script: $CIP/csmart/lib/security/scripts/invalid_community_request.rb
+  - script: $CIP/csmart/lib/security/scripts/check_mop.rb
+  - script: $CIP/csmart/lib/security/scripts/parseResults.rb
   - script: $CIP/csmart/lib/security/scripts/saveAcmeEvents.rb
   - script: $CIP/csmart/lib/security/scripts/security_archives.rb
   - script: $CIP/csmart/lib/security/scripts/cleanup_society.rb
