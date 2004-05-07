6b16e611202a0b5a0f0aec292340b6a5e57e020a
rliao1 <rliao1>
2004-05-07 21:42:28 +0000

    converted to new framework

--- securityTests/security/lib/stresses/4a1.rb
+++ securityTests/security/lib/stresses/4a1.rb
@@ -4,7 +4,8 @@ require 'security/lib/dataProtection'
 require 'security/lib/caDomain'
 
 #
-#
+# rehydration failure with revoked agent does not work on current code
+# currently CRL information is not stored, so there is no CRL information during rehydration
 #
 class Security4a1Experiment < SecurityStressFramework
    def initialize(run)
@@ -26,16 +27,18 @@ class Security4a1Experiment < SecurityStressFramework
        if event.event_type == 'STATUS' && event.component == 'IdmefEventPublisherPlugin' && event.data =~ /DATA_FAILURE/
 
          if event.data =~ /DATA_FAILURE_REASON,No certificates/
-       puts "event: #{event.cluster_identifier}, #{event.data}"
+#       puts "event: #{event.cluster_identifier}, #{event.data}"
            if event.data =~ /#{@revokedAgent}/
              summary "Detected rehydration failure event for revoked agent #{@revokedAgent}"
              @revokeDetection = true    
              @revokedIdmef = true
            end
          elsif event.data =~ /DATA_FAILURE_REASON:Verify digest failure/
-       puts "event: #{event.cluster_identifier}, #{event.data}"
+#       puts "event: #{event.cluster_identifier}, #{event.data}"
            if event.data =~ /#{@modifiedAgent}/
-             summary "Detected rehydration failure event for agent #{@modifiedAgent} with persistent data modified"
+             if @tamperDetection == false
+               summary "Detected rehydration failure event for agent #{@modifiedAgent} with persistent data modified"
+             end
              @tamperDetection = true
              @tamperIdmef = true
            end # if event
@@ -44,46 +47,60 @@ class Security4a1Experiment < SecurityStressFramework
      end # do event
    end
 
-#   def postConditionalStartSociety
-   def postConditionalGLSConnection
-#   def postConditionalPublishNextStage
+#   def postConditionalGLSConnection
+   def postPublishNextStage
         CaDomains.instance.ensureExpectedEntities
 
 # 4A2, revoke an agent and verify rehydration failure
 #        sleep 10.minutes
  
-        puts "Test rehydration for revoked and tampered agents"
+#        puts "Test rehydration for revoked and tampered agents"
         certRevocation = CertRevocation.new
-        ra_node = certRevocation.selectNode
-        @revokedAgent = certRevocation.selectAgentFromNode(ra_node)
+#        ra_node = certRevocation.selectNode
+#        @revokedAgent = certRevocation.selectAgentFromNode(ra_node)
 #        certRevocation.revokeAgent(@revokedAgent)
 
-        ma_node = certRevocation.selectNode
+        ma_node = nil
+        run.society.each_node do |node|
+          node.each_facet do |facet|
+            if facet[:NodeAttacker] == 'true'
+              ma_node = node
+            end
+          end
+        end
+#        ma_node = certRevocation.selectNode
         @modifiedAgent = certRevocation.selectAgentFromNode(ma_node)
 
 # give time to receive CRL
-        sleep 10.minutes
+#        sleep 10.minutes
 
 # 4A1, modify one agent snapshot to verify signature      
         DataProtection.modifyPersistence(@modifiedAgent)
 
 # now check rehydration
-      run.do_action "KillNodes", ra_node.name, ma_node.name
+#      run.do_action "KillNodes", ra_node.name
+      run.do_action "KillNodes", ma_node.name
       run.do_action "Sleep", 2.minutes
 
-      run.do_action "RestartNodes", ra_node.name, ma_node.name
+#      run.do_action "RestartNodes", ra_node.name
+      run.do_action "RestartNodes", ma_node.name
 
 Thread.fork {
-  sleep 5.minutes
+  begin
+
+    sleep 5.minutes
 #  summary "Failure to detect revoked agent rehydration" unless @revokeDetection
-  summary "Failure to detect tampered agent rehydration" unless @tamperDetection
+    summary "Failure to detect tampered agent rehydration" unless @tamperDetection
 #  saveResult(@revokeDetection, "4a1", "Rehydration failure due to revoked certificate")
 #  saveResult(@revokeIdmef, "4a20", "Idmef detection for 4a1")
 
-  saveResult(@tamperDetection, "4a2", "Rehydration failure due to tampered persisted data")   
-  saveResult(@tamperIdmef, "4a21", "Idmef detection for 4a2")
+    saveResult(@tamperDetection, "4a2", "Rehydration failure due to tampered persisted data")   
+    saveResult(@tamperIdmef, "4a21", "Idmef detection for 4a2")
 
 # exit 0   # keep the society running so that we can re-run this.
+  rescue => ex
+    saveAssertion('Stress4a1', "Unable to run test: #{ex}\n#{ex.backtrace.join("\n")}" )      end
+
 }
    end
 
