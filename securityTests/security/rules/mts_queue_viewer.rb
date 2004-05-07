9b1158fe5af844adcbaf0204fa126f1cd6d9288d
srosset <srosset>
2004-05-07 20:37:33 +0000

    Fixed rule problem

--- securityTests/security/rules/mts_queue_viewer.rb
+++ securityTests/security/rules/mts_queue_viewer.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/ruby
 
 society.each_node do |node|
-  node.add_component do |c|
-    c.insertionpoint="Node.AgentManager.Agent.MessageTransport.Component"
+  node.agent.add_component do |c|
+    c.name = "org.cougaar.mts.std.DestinationQueueMonitorPlugin"
+    c.insertionpoint = "Node.AgentManager.Agent.MessageTransport.Component"
     c.classname = "org.cougaar.mts.std.DestinationQueueMonitorPlugin"
   end
 end
