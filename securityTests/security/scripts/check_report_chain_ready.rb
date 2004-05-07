ac2ac7b4f6d32bd9553fc675f8800ca242daeb2e
tredmond <tredmond>
2004-05-07 02:51:17 +0000

    Put this before society_running.  My thinking is that if there si a problem then this will get called after report chain ready fails? Maybe this is not true because the society shts down.

--- securityTests/security/scripts/check_report_chain_ready.rb
+++ securityTests/security/scripts/check_report_chain_ready.rb
@@ -6,6 +6,6 @@ insert_before :setup_run do
    do_action "InjectStress", "TestReportChainReady", "beforeStartedSociety"
 end
 
-insert_after :wait_for_initialization do
+insert_before :society_running do
    do_action "InjectStress", "TestReportChainReady", "afterReportChainReady"
 end
