a88bef4682a8785c5da76b59187ed9964a6c2b86
tredmond <tredmond>
2004-05-10 14:01:14 +0000

    An experimental version of checkTrace that handles transitory errors.

---
+++ securityservices/test/bin/checkTrace-Experimental.rb
@@ -0,0 +1,330 @@
+#!/usr/bin/ruby
+
+CIP = ENV['CIP']
+$stdout.sync = true
+$stderr.sync = true
+
+# Add extra arguments to the grep function. For example, "-c" will report a count only
+#$extraargs = ARGV
+
+# The directory where the log files are located
+$logFileDirectory = "#{CIP}/workspace/log4jlogs"
+#$logFileDirectory = "#{CIP}/../srosset/log4jlogs"
+
+if ARGV.size > 0 &&  ARGV[0] != nil
+  $logFileDirectory = ARGV[0]
+end
+puts "Checking files under #{$logFileDirectory}"
+# True if performing a check for transient issues (e.g. issues that should disappear after a while)
+$searchTransientIssues=true
+
+def getStartupTime(fileName)
+  aFile = File.new(fileName, "r")
+  firstLine = aFile.readline
+  aFile.close
+  return getTimeFromEvent(firstLine)
+end
+
+def getTimeFromEvent(line)
+  startuptime = nil
+  message = nil
+  line.scan(/^([0-9]*)-([0-9]*)-([0-9]*) ([0-9]*):([0-9]*):([0-9]*),([0-9]*) (.*)/) { |x|
+    time = x[0]
+    #    puts x[0] , x[1], x[2], x[3], x[4],x[5]
+    # Time looks like this:
+    # 2004-04-12 20:51:22,234
+    startuptime = Time.local(x[0], x[1], x[2], x[3], x[4], x[5])
+    message = x[7]
+  }
+  return startuptime
+end
+
+# Search for exceptions and log4j errors, warnings, fatals
+$badMessages = [
+  "Exception",
+  "WARN",
+  "ERROR",
+  "FATAL"
+]
+
+# Except for the following cases:
+# This array contains pairs of (timeout, string)
+# The string is a log message at warn or above, but it is actually ok in some
+# circumstances.
+# The timeout is a time after which we should not see the message. For example, there
+# might be transient issues at startup, but after a while we should not see those issues.
+
+$startupDelay = 10 * 60
+
+$okMessages = [
+  # ############################################################### 
+  # Cougaar core messages
+  # ############################################################### 
+  # A message is sent out and a "+" is printed on standard out.
+  # standard out is logged at ERROR level.
+  [nil, "STDOUT - +"],
+  # A message is received out and a "-" is printed on standard out.
+  # standard out is logged at ERROR level.
+  [nil, "STDOUT - -"],
+
+  # ############################################################### 
+  # Security messages
+  # ############################################################### 
+  # An agent is unable to send a PKCS certificate signing request to the CA.
+  # This may happen at startup if the CA agent is not up yet or is too busy.
+  [$startupDelay, "Unable to send PKCS"],
+  # A Jar file was not signed properly.
+  # This may happen at startup. Ruby is still in the middle of generating jar files,
+  # but some nodes have already started and they are checking the signatures of all the jar files.
+  [$startupDelay, "xml\.jar cannot be trusted"],
+  [$startupDelay, "ConfigFinder - Unable to add entry"],
+  [$startupDelay, "Caused by: java.lang.Exception: Jar file does not have any certificate"],
+  # The MTS was unable to send a message out.
+  # This may happen at startup if the agent does not have a certificate, or if 
+  # the remote agent does not have a certificate.
+  # This applies to all four messages below.
+  [$startupDelay, "DestinationQueueImpl - Failure in communication"],
+  [$startupDelay, "MarshalException: error marshalling arguments"],
+  [$startupDelay, "error during JRMP connection"],
+  [$startupDelay, "java\.rmi\.ServerException: RemoteException occurred in server thread"],
+  [$startupDelay, "java\.rmi\.UnmarshalException: error unmarshalling arguments"],
+
+  # Jena is not happy when reading files with relative paths.
+  # See http://groups.yahoo.com/group/jena-dev/message/5266
+  # TODO: This should really be fixed.
+  [$startupDelay, "{W130} Base URI is \"\""],
+  # TODO: what is this? Probably Kaos
+  [$startupDelay, "{W101} Unqualified use of rdf:ID has been deprecated"],
+  [$startupDelay, "{W101} Unqualified use of rdf:resource has been deprecated"],
+  # A message has been received in the clear.
+  # In this specific circumstance, this is ok because RMI over SSL was used.
+  [$startupDelay, "Could not use protection level: SecureMethodParam: PLAIN null "],
+  # Ignorable KAoS warning
+  [2*$startupDelay, "Guard is already set"],
+  # A node is trying to communicate with the CA in order to submit a certificate
+  # signing request (CSR). The CSR is submitted through https.
+  # However, the SSL server-side certificate is not valid yet, most likely because it
+  # has not been signed by a root CA yet.
+  [$startupDelay, "Host cert has not been signed by CA yet"],
+  # A node is not able to find the certificate among its local identities, i.e., not a local agent.
+  # This is not a correct warning, it warns about certificate from other nodes, which does not have
+  # a local identity.
+  # Complete message is:
+  #   NameServerCertificateComponent - Fail to update node certificate for <node-name>
+  [$startupDelay, "Fail to update node certificate for"],
+  # The adaptivity engine complains that no plugin has published a condition.
+  # However, there is no guarantee that the M&R plugin will be started before the
+  # adaptivity engine. Eventually, the plugin will be loaded and publish the condition
+  [$startupDelay, "No Condition named"],
+  # ############################################################### 
+  # Warnings generated by Security stresses
+  # ############################################################### 
+  [$startupDelay, "Stress4a50.txt.jar cannot be trusted"],
+  [$startupDelay, "Stress4a51.txt.jar cannot be trusted"],
+  [$startupDelay, "Stress4a52.txt.jar cannot be trusted"],
+  [$startupDelay, "SecureConfigFinder.readExceptionList"],
+  [$startupDelay, "/unauthorizedTest write"],
+  [$startupDelay, "Found cert in cert directory which has been revoked ! CN=MessageAttacker"],
+  [$startupDelay, "blackboardOMSubscription.getChangedCollection"],
+  [$startupDelay, "blackboardOMPSubscription.getChangedCollection"],
+  [$startupDelay, "Stress4a53.txt.jar!/Stress4a53.txt"],
+  [$startupDelay, "LOGIN_FAILURE_REASON:WRONG_PASSWORD"],
+  [$startupDelay, "Caused by: java.security.cert.CertificateExpiredException"],
+ 
+  # ############################################################### 
+  # Robustness messages
+  # ############################################################### 
+  # The message timeout aspect complains that a message cannot be sent in a reasonnable
+  # amount of time.
+  # This may happen at startup when messages stay in the queue for a long time because
+  # agents do not have a certificate yet.
+  [$startupDelay, "MessageTimeoutAspect"],
+  # ############################################################### 
+  # Logistics warnings
+  # ############################################################### 
+  # Logistics: the scenario time is set.
+  # Not sure why this is logged at WARN level
+  [nil, "Starting Time set to"],
+  [nil, "Expanding an already disposed task"],
+  [nil, "which is before this orgs arrival time"],
+=begin
+  "SocketException",
+  "No CertificateEntry in naming",
+  "Error writing to server",
+  "CommunityServiceUtil",
+  "ComplainingLP - Warning",
+  "RogueThreadDetector - Schedulable",
+  "Host cert has not been signed",
+  "PersistenceManager cert not found",
+  "Unable to get Certifificate entry for DN",
+  "pAssoc has null object",
+  "ConnectException: Connection refused",
+  "error unmarshalling arguments",
+  "RemoteException occurred in server thread",
+  "Error unmarshaling return",
+  "BlackboardServiceProxy - QUERY DENIED",
+  "No execution context available",
+
+=end
+]
+
+def buildBadPattern()
+  # Build grep pattern
+  badPattern = "-nE \""
+  while (x = $badMessages.shift) != nil
+    badPattern << x
+    if $badMessages.size > 0
+      badPattern << "|"
+    end
+  end
+  badPattern << "\""
+  #puts badPattern
+  return badPattern;
+end
+
+def buildOkPattern()
+  okPattern = "-Ev \""
+  $okMessages.each do |x|
+#  while (x = $okMessages.shift) != nil
+    if (x[0] == nil)
+      okPattern << x[1]
+    end
+#    if $okMessages.size > 0
+#      okPattern << "|"
+#    end
+  end
+  okPattern.chop!()
+  okPattern << "\""
+  #puts okPattern
+  return okPattern
+end
+
+def getLongestTransient()
+  delay = 0
+  $okMessages.each do |x|
+    if x[0] != nil && x[0] > delay then
+      delay = x[0]
+    end
+  end  
+  return delay
+end
+
+def getLogResultsFileName(transient)
+  if ($logResultsFileName == nil) 
+    $logResultsFileName = "log4j-issues-#{Time.new.strftime("%m-%d-%Y_%H:%M:%S")}"
+  end
+  if transient
+    return $logResultsFileName + ".log";
+  else
+    return $logResultsFileName + "-notransient.log"
+  end
+end
+
+def searchIssues(removeOkPattern)
+  $startupTime = 0
+  badPattern = buildBadPattern()
+  okPattern = buildOkPattern()
+  logResultsFileName = getLogResultsFileName(true)
+  puts "Log4j issues saved to #{logResultsFileName}"
+  Dir.new($logFileDirectory).each do |filename|
+    next if !( filename =~ /\.log/ )
+    filepath = "#{$logFileDirectory}/#{filename}"
+    command = "echo ++ Checking #{filepath} | tee -a #{logResultsFileName} 2>&1"
+    system(command)
+
+    time = getStartupTime(filepath)
+    #    puts time
+    if ((time <=> $startupTime) == -1) || $startupTime == 0
+      $startupTime = time
+      #puts "Set startup time: " + startupDelay.to_s
+    end
+    if (removeOkPattern) 
+      #command="grep #{badPattern} #{filepath} | grep #{$extraargs} #{okPattern}"
+      command="grep #{badPattern} #{filepath} | grep #{okPattern}"
+    else
+      command="grep #{badPattern} #{filepath}"
+    end
+#    command << " | tee -a #{logResultsFileName} 2>&1"
+    command << " >> #{logResultsFileName} 2>&1"
+    #puts command
+    system(command)
+  end
+  return $startupTime
+end
+
+def removeTransientIssues()
+  #puts "entering remove transient issues"
+  File.open(getLogResultsFileName(false), "w") do |reducedLogs|
+    # puts("++ Checking #{reducedLogs.path()}")
+    IO.foreach(getLogResultsFileName(true)) do |log|
+      match = nil
+      # puts "Line = #{log}"
+      if (!((match = log.scan(/^\+\+ Checking (.*)/)).empty?))
+        # puts("first match")
+        puts(log)
+        reducedLogs.puts(log)
+      elsif (!((match = log.scan(/^([0-9]*)\:(.*)/)).empty?)) then
+        # puts("second match")
+        lineno = match[0][0]
+        message = match[0][1]
+        # puts "lineno = #{lineno}, message = #{message}"
+        time = getTimeFromEvent(message)
+        #
+        # time == nil when the line is not an initial log line (hopefully)
+        #
+        if (time != nil) then
+          # puts("Doing conservative comparison")
+          # puts(time)
+          # puts($startupTime + getLongestTransient())
+          # puts(time <=> ($startupTime + getLongestTransient()))
+          if ((time <=> ($startupTime + getLongestTransient())) == -1) then
+            msgok = false
+            $okMessages.each do |x|
+              if (x[0] != nil) then
+                # puts("Doing tighter comparison")
+                # puts(time)
+                # puts($startupTime + x[0])
+                # puts(time <=> ($startupTime + x[0]))
+                if (((time <=> ($startupTime + x[0])) == -1) &&
+                    (message.index(x[1]) != nil)) then
+                  msgok = true
+                  break
+                end
+              end
+            end
+            if (!msgok) then 
+              puts("#{lineno}:#{message}")
+              reducedLogs.puts("#{lineno}:#{message}")
+            end
+          else
+            puts("#{lineno}:#{message}")
+            reducedLogs.puts("#{lineno}:#{message}")
+          end
+        end
+      else 
+        # puts("no match")
+      end
+    end
+  end
+end
+
+#  resultsDir = "#{CIP}/workspace/logAnalysis"
+#  startupDelay = searchIssues(false, resultsDir)
+#  Dir.new(resultsDir).each do |filename|
+#    parseTimeRelatedEvents(filename, startupDelay)
+#  end
+#end
+
+def parseTimeRelatedEvents(fileName, startupDelay)
+  aFile = File.new(fileName, "r")
+  aFile.each_line { |line|
+    time = getTimeFromEvent(line)
+    if ( (time <=> ($startupTime + getLongestTransient())) == -1)
+      # This might be a transient issue
+    end
+  }
+  aFile.close
+end
+
+searchIssues(true)
+removeTransientIssues()
