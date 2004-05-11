83f9ef53662dbb41da669c83b868b061aa8aa562
srosset <srosset>
2004-05-11 14:14:29 +0000

    Added comments. Added redundant CA by default

--- securityservices/test/configs/acme_scripts/moas/MOAS-II-OP-ASMT-SAVE-PreStage5.rb
+++ securityservices/test/configs/acme_scripts/moas/MOAS-II-OP-ASMT-SAVE-PreStage5.rb
@@ -6,17 +6,36 @@ script: $CIP/csmart/scripts/definitions/BaselineTemplate-ExtOplan.rb
 parameters:
   - run_count: 1
   - society_file: $CIP/csmart/config/societies/ua/full-tc20-232a703v.plugins.rb
-# for 2 rack
   - layout_file: $CIP/operator/layouts/04-OP-layout.xml
   - archive_dir: $CIP/Logs
-  
+
   - rules:
     - $CIP/csmart/config/rules/isat
     - $CIP/csmart/config/rules/isat/uc3_nosec
     - $CIP/csmart/config/rules/yp
     - $CIP/csmart/config/rules/logistics
     - $CIP/csmart/config/rules/assessment
-# Robustnes
+###########################################
+# Security rules
+# Rules to enable the security framework.
+    - $CIP/csmart/config/rules/security
+# Rules to add components that apply security stresses
+    - $CIP/csmart/lib/security/rules
+# Rules that add components to compute the MOP values
+# In particular, the rules add the blackboard access control components
+    - $CIP/csmart/config/rules/security/mop
+# Rule used to check that all agents are reporting for duty
+    - $CIP/csmart/config/rules/security/testCollectData/ServiceContractPlugin.rule
+# Rules to enable the redundant CA and redundant persistence manager
+    - $CIP/csmart/config/rules/security/robustness
+# Enable the rules below if the layout does not include the redundant CA and PM facets
+#    - $CIP/csmart/config/rules/security/redundancy
+# Enable the rules below to enable the redundant PMs but not the redundant CAs
+#    - $CIP/csmart/config/rules/security/redundancy/add_redundant_pm_facet.rule
+#    - $CIP/csmart/config/rules/security/redundancy/adjust_memory.rule
+#    - $CIP/csmart/config/rules/security/robustness/redundant_persistence_mgrs.rule
+###########################################
+# Robustness rules
 #    - $CIP/csmart/config/rules/robustness/manager.rule
 #    - $CIP/csmart/config/rules/robustness/uc1
 #    - $CIP/csmart/config/rules/robustness/uc7
@@ -27,23 +46,6 @@ parameters:
 #    - $CIP/csmart/config/rules/metrics/serialization/metrics-only-serialization.rule
 #    - $CIP/csmart/config/rules/metrics/rss/tic
 
-# Security rules
-    - $CIP/csmart/config/rules/security
-    - $CIP/csmart/lib/security/rules
-    - $CIP/csmart/config/rules/security/mop
-    - $CIP/csmart/config/rules/security/testCollectData/ServiceContractPlugin.rule
-   # Redundant CA and persistence managers
-    - $CIP/csmart/config/rules/security/robustness
-#    Enable the rules below only when the layout does not include the
-#     redundant CA and PM facets
-#    - $CIP/csmart/config/rules/security/redundancy
-   # Enable the rules below when the redundant PMs are enabled but not the redundant CAs
-#    - $CIP/csmart/config/rules/security/redundancy/add_redundant_pm_facet.rule
-#    - $CIP/csmart/config/rules/security/redundancy/adjust_memory.rule
-#    - $CIP/csmart/config/rules/security/robustness/redundant_persistence_mgrs.rule
-
-# Robustness rules
-#    - $CIP/csmart/config/rules/robustness/manager.rule
 #    - $CIP/csmart/config/rules/robustness/uc1/aruc1.rule
 #    - $CIP/csmart/config/rules/robustness/uc1/debug/mic.rule
 #    - $CIP/csmart/config/rules/robustness/uc1/tuning/collect_stats.rule
@@ -55,14 +57,8 @@ parameters:
 
 include_scripts:
   - script: $CIP/csmart/lib/isat/clearPnLogs.rb
-  - script: $CIP/csmart/lib/isat/network_shaping.rb
-  - script: $CIP/csmart/lib/isat/klink_reporting.rb
-  - script: $CIP/csmart/lib/robustness/objs/deconfliction.rb
-  - script: $CIP/csmart/lib/isat/datagrabber_include.rb
-  - script: $CIP/csmart/assessment/assess/inbound_aggagent_include.rb
-  - script: $CIP/csmart/assessment/assess/outofbound_aggagent_include.rb
-
-# Security scripts
+###########################################
+# Security include scripts
   - script: $CIP/csmart/lib/security/scripts/setup_scripting.rb
   - script: $CIP/csmart/lib/security/scripts/setup_userManagement.rb
   - script: $CIP/csmart/lib/security/scripts/log_node_process_info.rb
@@ -81,19 +77,23 @@ include_scripts:
   - script: $CIP/csmart/lib/security/scripts/saveAcmeEvents.rb
   - script: $CIP/csmart/lib/security/scripts/security_archives.rb
   - script: $CIP/csmart/lib/security/scripts/cleanup_society.rb
-
-#  - script: setup_robustness.rb
-#  - script: $CIP/csmart/lib/isat/network_shaping.rb
-#  - script: $CIP/csmart/lib/isat/datagrabber_include.rb
-#  - script: $CIP/csmart/assessment/assess/inbound_aggagent_include.rb
-#  - script: $CIP/csmart/assessment/assess/outofbound_aggagent_include.rb
+#
+###########################################
+# Robustness include scripts
+  - script: $CIP/csmart/lib/isat/network_shaping.rb
+  - script: $CIP/csmart/lib/isat/klink_reporting.rb
+#  - script: $CIP/csmart/lib/robustness/objs/deconfliction.rb
+###########################################
+# Assessment include scripts
+  - script: $CIP/csmart/lib/isat/datagrabber_include.rb
+  - script: $CIP/csmart/assessment/assess/inbound_aggagent_include.rb
+  - script: $CIP/csmart/assessment/assess/outofbound_aggagent_include.rb
 #  - script: $CIP/csmart/assessment/assess/cnccalc_include.rb
-
+###########################################
   - script: $CIP/csmart/lib/isat/save_snapshot.rb
     parameters:
       - snapshot_name: $CIP/SAVE-PreStage5.tgz
       - snapshot_location: before_stage_5
-
 =end
 
 require 'cougaar/scripting'
