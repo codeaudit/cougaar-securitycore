542994b2dd0f9d88929ed3e640f6d2bba28586bd
tredmond <tredmond>
2004-05-11 18:10:13 +0000

    for now, this removes the files in workspace/test Needs to be done better.

--- securityTests/security/scripts/cleanup_society.rb
+++ securityTests/security/scripts/cleanup_society.rb
@@ -9,4 +9,7 @@ require 'security/lib/stresses/jar_files.rb'
 
 insert_before :transformed_society do
   do_action "InjectStress", "CleanupJarFiles", "cleanupJarFiles"
+  do_action "GenericAction" do |run|
+    `rm -f #{CIP}/workspace/test/*`
+  end
 end
