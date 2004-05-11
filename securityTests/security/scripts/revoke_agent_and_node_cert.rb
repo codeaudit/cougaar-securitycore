96d5cc452e70d8a4e45447920c1d7d9fd9fdab9e
rliao1 <rliao1>
2004-05-11 21:35:28 +0000

    run postLoadSociety after ReportChainReady.
    There is a problem with scripts using caDomains.rb, which goes through society and set information on fields, and will cause NilClass exception before nodes are started

--- securityTests/security/scripts/revoke_agent_and_node_cert.rb
+++ securityTests/security/scripts/revoke_agent_and_node_cert.rb
@@ -11,9 +11,10 @@ insert_before :setup_run do
   do_action  "InjectStress", "Security3c2", "postStartJabberCommunications"
 end
 
-insert_before :wait_for_initialization do
+insert_after :during_stage_1 do
   # Revoke a node
   # Revoke an agent
+  # in 1k society we have the problem of society still loading at initialization stage
   do_action  "InjectStress", "Security3c2", "postLoadSociety"
 end
 
