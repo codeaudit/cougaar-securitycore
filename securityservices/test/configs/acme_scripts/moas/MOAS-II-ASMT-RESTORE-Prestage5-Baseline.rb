4ed1af7148e0d4b4d3fc7a4eb2fd28cb869e474c
srosset <srosset>
2004-05-13 22:50:07 +0000

    TIC modifications

--- securityservices/test/configs/acme_scripts/moas/MOAS-II-ASMT-RESTORE-Prestage5-Baseline.rb
+++ securityservices/test/configs/acme_scripts/moas/MOAS-II-ASMT-RESTORE-Prestage5-Baseline.rb
@@ -1,29 +1,38 @@
 =begin experiment
 
-name: MOAS-II-AS-RESTORE-Basline
-description: MOAS-II-AS-preStage5-Restore-Baseline
-script: OPRestoreTemplate.rb
+name: MOAS-II-RESTORE-Basline
+description: MOAS-II-preStage5-Restore-Baseline
+script: $CIP/csmart/scripts/definitions/RestoreTemplate.rb
 parameters:
   - run_count: 1
   - snapshot_name: $CIP/SAVE-PreStage5.tgz
   - archive_dir: $CIP/Logs
   - stages:
     - 5
-    - 6
   
 include_scripts:
   - script: $CIP/csmart/lib/isat/clearLogs.rb
-#  - script: $CIP/csmart/lib/isat/datagrabber_include.rb
-#  - script: $CIP/csmart/lib/isat/network_shaping.rb
-#  - script: $CIP/csmart/assessment/assess/inbound_aggagent_include.rb
-#  - script: $CIP/csmart/assessment/assess/outofbound_aggagent_include.rb
-#  - script: $CIP/csmart/assessment/assess/cnccalc_include.rb
+###########################################
+# Security include scripts
+  - script: $CIP/csmart/lib/security/scripts/setup_scripting.rb
+  - script: $CIP/csmart/lib/security/scripts/setup_userManagement.rb
+  - script: $CIP/csmart/lib/security/scripts/log_node_process_info.rb
+  - script: $CIP/csmart/lib/security/scripts/parseResults.rb
+  - script: $CIP/csmart/lib/security/scripts/security_archives.rb
+  - script: $CIP/csmart/lib/security/scripts/cleanup_society.rb
+###########################################
+  - script: $CIP/csmart/lib/isat/datagrabber_include.rb
+  - script: $CIP/csmart/lib/isat/network_shaping.rb
+  - script: $CIP/csmart/lib/robustness/objs/deconfliction.rb
+  - script: $CIP/csmart/assessment/assess/inbound_aggagent_include.rb
+  - script: $CIP/csmart/assessment/assess/outofbound_aggagent_include.rb
+  - script: $CIP/csmart/assessment/assess/cnccalc_include.rb
     parameters:
       - run_type: base
-      - description: Stage 56 Baseline
+      - description: Stage 5 Baseline
   - script: $CIP/csmart/assessment/assess/analysis_baseline_cmds.rb
     parameters:
-      - only_analyze: "moe1"
+      - only_analyze: "moe1,moe3"
       - baseline_name: base2
 
 =end
