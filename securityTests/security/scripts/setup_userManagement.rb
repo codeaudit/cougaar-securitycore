cf9d06fc3bf145495fd4b4532d242b445d1a8ecb
srosset <srosset>
2004-05-13 23:10:23 +0000

    Changed the insertion point, so that it can work in the RESTORE experiment. Not tested for the SAVE experiment.

--- securityTests/security/scripts/setup_userManagement.rb
+++ securityTests/security/scripts/setup_userManagement.rb
@@ -8,7 +8,7 @@ description: special initialization for security
 
 require 'security/lib/scripting'
 
-insert_after :wait_for_initialization do
+insert_after :society_running do
   # We set the agent name parameter to "nil", which instructs the
   # action to look for the agent that has "org_id" facet == "OSD.GOV"
   # This makes it easier to run the stresses with the PING society.
