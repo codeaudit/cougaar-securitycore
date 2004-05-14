83be9a3ee2cef77fcff6455982c6270c7d05001a
tredmond <tredmond>
2004-05-14 15:45:27 +0000

    removed the network shaping.

--- securityservices/test/configs/acme_scripts/moas/MOAS-II-OP-ASMT-RESTORE-Prestage5-Baseline.rb
+++ securityservices/test/configs/acme_scripts/moas/MOAS-II-OP-ASMT-RESTORE-Prestage5-Baseline.rb
@@ -22,7 +22,7 @@ include_scripts:
   - script: $CIP/csmart/lib/security/scripts/cleanup_societyRestore.rb
 ###########################################
   - script: $CIP/csmart/lib/isat/datagrabber_include.rb
-  - script: $CIP/csmart/lib/isat/network_shaping.rb
+#  - script: $CIP/csmart/lib/isat/network_shaping.rb
   - script: $CIP/csmart/assessment/assess/inbound_aggagent_include.rb
   - script: $CIP/csmart/assessment/assess/outofbound_aggagent_include.rb
   - script: $CIP/csmart/assessment/assess/cnccalc_include.rb
