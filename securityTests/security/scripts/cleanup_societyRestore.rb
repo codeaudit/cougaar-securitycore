98551536b2b3b527b99158abce71952a545a16cb
srosset <srosset>
2004-05-14 13:38:50 +0000

    *** empty log message ***

---
+++ securityTests/security/scripts/cleanup_societyRestore.rb
@@ -0,0 +1,15 @@
+
+#
+# Perform any cleanup before starting the society
+#
+#
+
+require 'security/lib/scripting'
+require 'security/lib/stresses/jar_files.rb'
+
+insert_before :snaphot_restored do
+  do_action "InjectStress", "CleanupJarFiles", "cleanupJarFiles"
+  do_action "GenericAction" do |run|
+    `rm -f #{CIP}/workspace/test/*`
+  end
+end
