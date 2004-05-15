68d9a1684a37043274d4815fd1d110f37b53f58c
tredmond <tredmond>
2004-05-15 23:46:32 +0000

    updates to the policy society

--- securityservices/test/configs/policies/society/Policy.rb
+++ securityservices/test/configs/policies/society/Policy.rb
@@ -8,22 +8,25 @@ $:.unshift File.join(CIP, 'csmart', 'acme_service', 'src', 'redist')
 $:.unshift File.join(CIP, 'csmart', 'config', 'lib') 
 $:.unshift File.join(CIP, 'csmart', 'lib')
 
-
 require 'cougaar/scripting' 
-require 'cougaar/experiment'
+
 require 'cougaar/communities' 
-require 'ultralog/scripting'
-require 'ultralog/services' 
+require 'cougaar/experiment'
 
 require 'security/lib/cougaarMods'
 require 'security/actions/configFiles'
 require 'security/actions/cond_policy'
+require 'security/actions/saveEvents'
 require 'security/lib/misc'
- 
-require 'socket' 
-require 'rexml/document' 
 
-require 'policyInitDM.rb'
+
+require 'ultralog/scripting'
+require 'ultralog/services' 
+ 
+require 'rexml/document' 
+require 'socket' 
+
+require 'policyTests'
  
 Cougaar::ExperimentMonitor.enable_stdout 
 Cougaar::ExperimentMonitor.enable_logging 
@@ -74,10 +77,13 @@ Cougaar.new_experiment("Policy-Test").run(1) {
 
   do_action "BuildConfigJarFiles"
   do_action "DeployCommunitiesFile" 
-  do_action "StartSociety" 
 
+  do_action "SaveAcmeEvents"
+
+  do_action "StartSociety" 
   do_action "Sleep", 30.seconds 
-  do_action "WaitForUserManagerReady"
-#  do_action "InitDM"
+#  do_action "SetPoliciesTest"
+  do_action "InitDM"
+  do_action "Sleep", 40.seconds
 }
 
