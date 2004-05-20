1e896d07601a345ae8c578bb666a6435f77e08b0
srosset <srosset>
2004-05-20 18:25:51 +0000

    Enable MOPs by default

--- securityTests/security/scripts/check_mop.rb
+++ securityTests/security/scripts/check_mop.rb
@@ -15,10 +15,10 @@ require 'security/lib/SecurityMop2_6'
 
 insert_before :setup_run do
   # Required when using InjectStress for security MOPs:
-#  do_action "StoreMopsInRunHashTable"
+  do_action "StoreMopsInRunHashTable"
 
   # MOP 2.3: encrypted messages
-#  do_action "StartTcpCapture", ["AgentA", "AgentB"]
+  do_action "StartTcpCapture", ["AgentA", "AgentB"]
 end
 
 insert_after :society_running do
@@ -26,8 +26,8 @@ insert_after :society_running do
 #  do_action  "InjectStress", "SecurityMop21", "setup"
 
   # MOP 2.4: unauthorized user actions
-#  do_action  "InjectStress", "SecurityMop2_4", "setup"
-#  do_action  "InjectStress", "SecurityMop2_4", "perform"
+  do_action  "InjectStress", "SecurityMop2_4", "setup"
+  do_action  "InjectStress", "SecurityMop2_4", "perform"
 end
 
 insert_after :after_stage_1 do
@@ -35,29 +35,29 @@ insert_after :after_stage_1 do
 #  do_action  "InjectStress", "SecurityMop21", "shutdown"
 
   # MOP 2.3: encrypted messages
-#  do_action  "InjectStress", "SecurityMop23", "shutdown"
-#  do_action  "Sleep", 1.minute
+  do_action  "InjectStress", "SecurityMop23", "shutdown"
+  do_action  "Sleep", 1.minute
 
   # MOP 2.1: blackboard access control
 #  do_action  "InjectStress", "SecurityMop21", "calculate"
 
   # MOP 2.2: encrypted persistence files
-#  do_action  "InjectStress", "SecurityMop22", "calculate"
+  do_action  "InjectStress", "SecurityMop22", "calculate"
 
   # MOP 2.3: encrypted messages
-#  do_action  "InjectStress", "SecurityMop23", "calculate"
+  do_action  "InjectStress", "SecurityMop23", "calculate"
 
   # MOP 2.4: unauthorized user actions
-#  do_action  "InjectStress", "SecurityMop2_4", "calculate"
+  do_action  "InjectStress", "SecurityMop2_4", "calculate"
 
   # MOP 2.5: user action audit
-#  do_action  "InjectStress", "SecurityMop2_5", "calculate"
+  do_action  "InjectStress", "SecurityMop2_5", "calculate"
 
   # MOP 2.6: IDMEF events
-#  do_action  "InjectStress", "SecurityMop2_6", "calculate"
+  do_action  "InjectStress", "SecurityMop2_6", "calculate"
 
   # MOP 2.3: encrypted messages
-#  do_action "WaitForCalculationCompletion"
-#  do_action "InjectStress", "SecurityMop23", "postCalculate"
+  do_action "WaitForCalculationCompletion"
+  do_action "InjectStress", "SecurityMop23", "postCalculate"
 
 end
