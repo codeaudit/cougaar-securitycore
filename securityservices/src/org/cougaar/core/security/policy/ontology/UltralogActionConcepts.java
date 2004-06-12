b83cc788f933e006fe2da989e55b66d0bf1e64b0
tredmond <tredmond>
2004-06-12 06:45:08 +0000

    Rebuilt for community policy changes.

--- securityservices/src/org/cougaar/core/security/policy/ontology/UltralogActionConcepts.java
+++ securityservices/src/org/cougaar/core/security/policy/ontology/UltralogActionConcepts.java
@@ -16,41 +16,53 @@ final public class UltralogActionConcepts
 	}
 
 	// Concepts
+    public static final String CommunityAction = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "CommunityAction");
     public static final String WPForward = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "WPForward");
     public static final String WPUpdateSelf = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "WPUpdateSelf");
+    public static final String CommunityActionSelf = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "CommunityActionSelf");
     public static final String WPUpdateDelegate = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "WPUpdateDelegate");
     public static final String BlackBoardAccess = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "BlackBoardAccess");
     public static final String WPUpdate = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "WPUpdate");
     public static final String ServletAccess = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "ServletAccess");
+    public static final String CommunityActionDelegate = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "CommunityActionDelegate");
     public static final String WPLookup = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "WPLookup");
 
+    public static String CommunityAction() { return CommunityAction; };
     public static String WPForward() { return WPForward; };
     public static String WPUpdateSelf() { return WPUpdateSelf; };
+    public static String CommunityActionSelf() { return CommunityActionSelf; };
     public static String WPUpdateDelegate() { return WPUpdateDelegate; };
     public static String BlackBoardAccess() { return BlackBoardAccess; };
     public static String WPUpdate() { return WPUpdate; };
     public static String ServletAccess() { return ServletAccess; };
+    public static String CommunityActionDelegate() { return CommunityActionDelegate; };
     public static String WPLookup() { return WPLookup; };
 
 	// Properties
     public static final String usedAuditLevel = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "usedAuditLevel");
     public static final String forwardTo = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "forwardTo");
     public static final String usedProtectionLevel = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "usedProtectionLevel");
+    public static final String communityTarget = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "communityTarget");
     public static final String hasSubject = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "hasSubject");
+    public static final String communityActionType = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "communityActionType");
     public static final String blackBoardAccessObject = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "blackBoardAccessObject");
     public static final String usedAuthenticationLevel = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "usedAuthenticationLevel");
     public static final String accessedServlet = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "accessedServlet");
     public static final String wpAgentEntry = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "wpAgentEntry");
+    public static final String community = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "community");
     public static final String blackBoardAccessMode = OntologyLanguageTagSelector.selectLanguageForTag(UltralogActionOwlURL + "blackBoardAccessMode");
 
     public static String usedAuditLevel() { return usedAuditLevel; };
     public static String forwardTo() { return forwardTo; };
     public static String usedProtectionLevel() { return usedProtectionLevel; };
+    public static String communityTarget() { return communityTarget; };
     public static String hasSubject() { return hasSubject; };
+    public static String communityActionType() { return communityActionType; };
     public static String blackBoardAccessObject() { return blackBoardAccessObject; };
     public static String usedAuthenticationLevel() { return usedAuthenticationLevel; };
     public static String accessedServlet() { return accessedServlet; };
     public static String wpAgentEntry() { return wpAgentEntry; };
+    public static String community() { return community; };
     public static String blackBoardAccessMode() { return blackBoardAccessMode; };
 
 	// Instances
--- securityservices/src/org/cougaar/core/security/policy/ontology/UltralogEntityConcepts.java
+++ securityservices/src/org/cougaar/core/security/policy/ontology/UltralogEntityConcepts.java
@@ -16,9 +16,11 @@ final public class UltralogEntityConcepts
 	}
 
 	// Concepts
+    public static final String Community = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "Community");
     public static final String PlugInRoles = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "PlugInRoles");
     public static final String WPAccessType = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "WPAccessType");
     public static final String BlackBoardAccessMode = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "BlackBoardAccessMode");
+    public static final String CommunityActionType = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "CommunityActionType");
     public static final String ProtectionLevel = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "ProtectionLevel");
     public static final String UltralogEntity = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "UltralogEntity");
     public static final String Servlet = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "Servlet");
@@ -27,9 +29,11 @@ final public class UltralogEntityConcepts
     public static final String AuthenticationLevel = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "AuthenticationLevel");
     public static final String ULContentValue = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "ULContentValue");
 
+    public static String Community() { return Community; };
     public static String PlugInRoles() { return PlugInRoles; };
     public static String WPAccessType() { return WPAccessType; };
     public static String BlackBoardAccessMode() { return BlackBoardAccessMode; };
+    public static String CommunityActionType() { return CommunityActionType; };
     public static String ProtectionLevel() { return ProtectionLevel; };
     public static String UltralogEntity() { return UltralogEntity; };
     public static String Servlet() { return Servlet; };
@@ -42,11 +46,25 @@ final public class UltralogEntityConcepts
 
 
 	// Instances
+    public static final String testCommunityTwo = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "testCommunityTwo");
     public static final String WPAdd = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "WPAdd");
+    public static final String ModifyCommunityAttributes = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "ModifyCommunityAttributes");
     public static final String WPChange = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "WPChange");
+    public static final String JoinCommunity = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "JoinCommunity");
+    public static final String ListCommunities = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "ListCommunities");
     public static final String WPRemove = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "WPRemove");
+    public static final String GetCommunityDescriptor = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "GetCommunityDescriptor");
+    public static final String LeaveCommunity = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "LeaveCommunity");
+    public static final String testCommunityOne = OntologyLanguageTagSelector.selectLanguageForTag(UltralogEntityOwlURL + "testCommunityOne");
 
+    public static String testCommunityTwo() { return testCommunityTwo; };
     public static String WPAdd() { return WPAdd; };
+    public static String ModifyCommunityAttributes() { return ModifyCommunityAttributes; };
     public static String WPChange() { return WPChange; };
+    public static String JoinCommunity() { return JoinCommunity; };
+    public static String ListCommunities() { return ListCommunities; };
     public static String WPRemove() { return WPRemove; };
+    public static String GetCommunityDescriptor() { return GetCommunityDescriptor; };
+    public static String LeaveCommunity() { return LeaveCommunity; };
+    public static String testCommunityOne() { return testCommunityOne; };
 }
