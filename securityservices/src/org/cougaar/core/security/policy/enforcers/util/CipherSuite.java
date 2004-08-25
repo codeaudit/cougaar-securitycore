0086380612607f354e85cc0821edf0ec0d4326d9
tredmond <tredmond>
2004-08-25 04:24:08 +0000

    CipherSuites now has support for the connection between two agents having intrinsic security (e.g. the agents are on the same LAN or are connected by a secure VPN tunnel).

--- securityservices/src/org/cougaar/core/security/policy/enforcers/util/CipherSuite.java
+++ securityservices/src/org/cougaar/core/security/policy/enforcers/util/CipherSuite.java
@@ -26,7 +26,10 @@
 
 package org.cougaar.core.security.policy.enforcers.util;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -36,9 +39,13 @@ import java.util.Set;
 
 
 public class CipherSuite {
+  private String _name;
   private Set _symmetric;
   private Set _asymmetric;
   private Set _signature;
+  private Map _conditionMap;
+
+  public final static int PROTECTED_LAN = 1;
 
   /**
    * Construct a Cipher Suite.
@@ -48,34 +55,23 @@ public class CipherSuite {
    * symmetric, etc.
    *
    */
-  public CipherSuite(Set symmetric,
-                     Set asymmetric,
-                     Set signature) {
-    _symmetric  = new LinkedHashSet();
-    _asymmetric = new LinkedHashSet();
-    _signature   = new LinkedHashSet();
-    if (symmetric != null) {
-      _symmetric.addAll(symmetric);
-    }
-    if (asymmetric != null) {
-      _asymmetric.addAll(asymmetric);
-    }
-    if (signature != null) {
-      _signature.addAll(signature);
-    }
+  public CipherSuite(String name) {
+    _name         = name;
+    init();
   }
+
 
   public CipherSuite() {
-    this(null, null, null);
+    _name = null;
+    init();
   }
 
-  /**
-   * returns if there are any cipher suites that are valid
-   */
-  public boolean isCipherAvailable() {
-    return !_symmetric.isEmpty() && 
-      !_signature.isEmpty() && 
-      !_asymmetric.isEmpty();
+  private void init()
+  {
+    _symmetric    = new LinkedHashSet();
+    _asymmetric   = new LinkedHashSet();
+    _signature    = new LinkedHashSet();
+    _conditionMap = new HashMap();
   }
 
   /**
@@ -83,34 +79,127 @@ public class CipherSuite {
    * vector of strings.
    */
   public Set getSymmetric()   { return _symmetric; }
+
+
+  /**
+   * Returns the vector of symmetric algorithms in the suite as a
+   * vector of strings based on network environment such as protected lan or
+   * vpn connection
+   */
+  public Set getSymmetric(int condition)   
+  { 
+    Object o = _conditionMap.get(new Integer(condition));
+    if (o == null) {
+      return _symmetric;
+    } else {
+      return ((CipherSuite) o)._symmetric;
+    }
+  }
+
+
+  public void addSymmetric(String symm) {
+    _symmetric.add(symm);
+  }
+
+  public void addSymmetric(int condition, String symm) {
+    CipherSuite cs = getConditionalCipherSuite(condition);
+    cs._symmetric.add(symm);
+  }
+
+
   /**
    * Returns the vector of assymmetric algorithms in the suite as a
    * vector of strings.
    */
   public Set getAsymmetric() { return _asymmetric; }
-  /**
-   * Returns the vector of checksum algorithms in the suite as a
-   * vector of strings.
-   */
-  public Set getSignature()    { return _signature;   }
 
-  public void addSymmetric(String symm) {
-    _symmetric.add(symm);
+  /**
+   * Returns the vector of assymmetric algorithms in the suite as a
+   * vector of strings based on network environment such as protected lan or
+   * vpn connection
+   */
+  public Set getAsymmetric(int condition) 
+  { 
+    Object o = _conditionMap.get(new Integer(condition));
+    if (o == null) {
+      return _asymmetric;
+    } else {
+      return ((CipherSuite) o)._asymmetric;
+    }
   }
 
   public void addAsymmetric(String asymm) {
     _asymmetric.add(asymm);
   }
 
+  public void addAsymmetric(int condition, String asymm) 
+  {
+    CipherSuite cs = getConditionalCipherSuite(condition);
+    cs._asymmetric.add(asymm);
+  }
+
+  /**
+   * Returns the vector of checksum algorithms in the suite as a
+   * vector of strings.
+   */
+  public Set getSignature()    { return _signature;   }
+
+
+  /**
+   * Returns the vector of signature algorithms in the suite as a
+   * vector of strings based on network environment such as protected lan or
+   * vpn connection
+   */
+  public Set getSignature(int condition) 
+  { 
+    Object o = _conditionMap.get(new Integer(condition));
+    if (o == null) {
+      return _signature;
+    } else {
+      return ((CipherSuite) o)._signature;
+    }
+  }
+
+
   public void addSignature(String sig) {
     _signature.add(sig);
   }
 
-  public void addAll(CipherSuite cs) {
+  public void addSignature(int condition, String sig) {
+    CipherSuite cs = getConditionalCipherSuite(condition);
+    cs._signature.add(sig);
+  }
+
+
+  public void addAll(CipherSuite cs)
+  {
     _symmetric.addAll(cs._symmetric);
     _asymmetric.addAll(cs._asymmetric);
     _signature.addAll(cs._signature);
+    for (Iterator conditions = cs._conditionMap.keySet().iterator(); 
+         conditions.hasNext();) {
+      Integer conditionType = (Integer) conditions.next();
+      CipherSuite mycs = getConditionalCipherSuite(conditionType.intValue());
+      CipherSuite othercs = (CipherSuite) cs._conditionMap.get(conditionType);
+      mycs._symmetric.addAll(othercs._symmetric);
+      mycs._asymmetric.addAll(othercs._asymmetric);
+      mycs._signature.addAll(othercs._signature);
+    }
   }
+
+  private CipherSuite getConditionalCipherSuite(int condition)
+  {
+    Integer cond = new Integer(condition);
+    Object o = _conditionMap.get(cond);
+    if (o == null) {
+      CipherSuite cs = new CipherSuite();
+      _conditionMap.put(cond, (Object) cs);
+      return cs;
+    } else {
+      return (CipherSuite) o;
+    }
+  }
+
 
   public String toString() {
     return "CipherSuite: symmetric = " + _symmetric + 
