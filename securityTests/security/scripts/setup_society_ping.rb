faa0bdfba74a87c37d5b673d7ab926ceb3e330ed
srosset <srosset>
2004-08-27 23:01:37 +0000

    Added action to copy communities.xml.jar to $CIP/configs/common.

--- securityTests/security/scripts/setup_society_ping.rb
+++ securityTests/security/scripts/setup_society_ping.rb
@@ -3,6 +3,7 @@ require 'security/lib/scripting'
 require 'security/actions/ping_active'
 require 'security/lib/security'
 require 'security/actions/buildHostFile'
+require 'security/actions/build_config_files'
 require 'security/actions/resetCsiAcme'
 
 insert_before "build_host_file" do
@@ -14,4 +15,9 @@ end
 PingSociety.setPingSociety
 insert_before :wait_for_initialization do
   wait_for  "PingActive", 20.minutes
+end
+
+insert_before :setup_run do
+  # Replacing DeployCommunitiesFile which does not work at CSI's testbed
+  do_action "BuildSignedCommunityJarFile", "myCommunity.xml", "#{CIP}/configs/common"
 end
