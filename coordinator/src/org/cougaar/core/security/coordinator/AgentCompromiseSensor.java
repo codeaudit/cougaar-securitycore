e6a65230ff2048a5fa9c3ea819d54050ec58ff07
rliao1 <rliao1>
2004-09-13 17:48:10 +0000

    coordinator stubs

---
+++ coordinator/src/org/cougaar/core/security/coordinator/AgentCompromiseSensor.java
@@ -0,0 +1,134 @@
+/* 
+ * <copyright> 
+ *  Copyright 1999-2004 Cougaar Software, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects 
+ *  Agency (DARPA). 
+ *  
+ *  You can redistribute this software and/or modify it under the
+ *  terms of the Cougaar Open Source License as published on the
+ *  Cougaar Open Source Website (www.cougaar.org).  
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ * </copyright> 
+ */
+
+package org.cougaar.core.security.coordinator;
+
+import java.util.Collection;
+import java.util.Iterator;
+import org.cougaar.coordinator.*;
+import org.cougaar.coordinator.techspec.TechSpecNotFoundException;
+import org.cougaar.core.blackboard.IncrementalSubscription;
+import org.cougaar.core.component.ServiceBroker;
+import org.cougaar.core.plugin.ComponentPlugin;
+import org.cougaar.core.service.LoggingService;
+import org.cougaar.util.UnaryPredicate;
+import org.cougaar.core.adaptivity.InterAgentOperatingMode;
+
+public class AgentCompromiseSensor extends ComponentPlugin
+{
+    private LoggingService log;
+    private AgentCompromiseDiagnosis diagnosis;
+    private ServiceBroker sb;
+    private boolean start = true; 
+
+    private IncrementalSubscription _subscription;
+    private final UnaryPredicate INTER_AGENT_OPERATING_MODE =
+      new UnaryPredicate() {
+        public boolean execute(Object o) {
+          if (o instanceof InterAgentOperatingMode) {
+            return true;
+          }    
+          return false;
+        }
+      };
+
+    public void load() {
+        super.load();
+        sb = getServiceBroker();
+        log = (LoggingService)sb.getService(this, LoggingService.class, null);
+
+    }
+
+    public synchronized void unload() {
+        sb.releaseService(this, LoggingService.class, log);
+        super.unload();
+    }
+
+  protected void setupSubscriptions() {
+    _subscription = (IncrementalSubscription)
+      blackboard.subscribe(INTER_AGENT_OPERATING_MODE);
+  }
+
+    int tsLookupCnt = 0;
+    public synchronized void execute() {
+
+      if (start) {
+        initAgentCompromiseDiagnosis();
+        if (start) {
+          return;
+        }
+      }
+
+      // get the threatcon level change object, publish change
+      if (_subscription.hasChanged()) {
+      // notify all the agents in a particular enclave/security community 
+        // leave to the old value, set or add will change it
+          //removePolicies(_subscription.getRemovedCollection());
+          changeLevel(_subscription.getAddedCollection());
+          changeLevel(_subscription.getChangedCollection());
+      }
+
+
+    }
+
+    private void changeLevel(Collection c) {
+      Iterator i = c.iterator();
+      while(i.hasNext()) {
+        InterAgentOperatingMode iaom = (InterAgentOperatingMode)i.next();
+        // value - Comparable
+        // set value
+        try {
+          diagnosis.setValue(iaom.getValue());
+          blackboard.publishChange(diagnosis);
+          if (log.isDebugEnabled()) {
+            log.debug(diagnosis + " changed.");
+            log.debug(diagnosis.dump());
+          }
+        } catch (IllegalValueException e) { 
+          log.error("Illegal value = "+iaom.getValue(), e);
+        }
+      }
+        
+    }
+
+    private void initAgentCompromiseDiagnosis() {
+      try {
+        diagnosis = new AgentCompromiseDiagnosis(agentId.toString(), sb);
+        blackboard.publishAdd(diagnosis);
+        start = false;
+        if (log.isDebugEnabled()) {
+          log.debug(diagnosis + " added.");
+        }
+      }
+      catch (TechSpecNotFoundException e) {
+                if (tsLookupCnt > 10) {
+                    log.warn("TechSpec not found for AgentCompromiseDiagnosis.  Will retry.", e);
+                    tsLookupCnt = 0;
+                }
+                blackboard.signalClientActivity();
+      }
+    }
+}
+
---
+++ coordinator/src/org/cougaar/core/security/coordinator/ThreatConActuator.java
@@ -0,0 +1,134 @@
+/*
+ * <copyright>
+ *  Copyright 1999-2004 Cougaar Software, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA)
+ *  and the Defense Logistics Agency (DLA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+*/
+
+package org.cougaar.core.security.coordinator;
+
+import org.cougaar.coordinator.*;
+import org.cougaar.coordinator.techspec.TechSpecNotFoundException;
+
+import org.cougaar.core.plugin.ComponentPlugin;
+import org.cougaar.core.component.ServiceBroker;
+import org.cougaar.core.service.LoggingService;
+import org.cougaar.core.blackboard.IncrementalSubscription;
+import org.cougaar.util.UnaryPredicate;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class ThreatConActuator extends ComponentPlugin
+{
+  private LoggingService log;
+  private ThreatConAction action;
+  private ServiceBroker sb;
+  private IncrementalSubscription actionSub;
+
+  private UnaryPredicate actionPred = new UnaryPredicate() {
+            public boolean execute(Object o) {
+                return (o instanceof ThreatConAction);}};
+
+  public void load() {
+    super.load();
+
+    sb = getServiceBroker();
+    log = (LoggingService)
+            sb.getService(this, LoggingService.class, null);
+
+  }
+
+  public synchronized void unload() {
+  }
+
+  public void setupSubscriptions()
+  {
+    actionSub = (IncrementalSubscription)blackboard.subscribe(actionPred);
+  }
+
+  boolean start = true;
+  int tsLookupCnt = 0;
+  public synchronized void execute() {
+    if (start) { 
+      try {
+        Set values = new HashSet();
+        values.add(ThreatConAction.RMI);
+        values.add(ThreatConAction.RMISSL);
+        values.add(ThreatConAction.NORMI);
+        ThreatConAction action = new ThreatConAction(agentId.toString(), values, sb);
+        blackboard.publishAdd(action);
+        if (log.isDebugEnabled()) log.debug(action+" added.");
+        start = false;
+      } catch (TechSpecNotFoundException e) {
+        if (tsLookupCnt > 10) {
+          log.warn("TechSpec not found for SampleAction.  Will retry.", e);
+          tsLookupCnt = 0;
+        }
+        blackboard.signalClientActivity();
+      } catch (IllegalValueException ive) {
+        log.error("Exception for initialization: " + ive);
+      }
+      
+    }
+
+    Iterator iter = actionSub.getChangedCollection().iterator();
+    while (iter.hasNext()) {
+      ThreatConAction action = (ThreatConAction)iter.next();
+      if (action != null) {
+        if (log.isDebugEnabled()) {
+          log.debug("received action: " + action);
+        }
+
+        Set newPV = action.getPermittedValues(); 
+        if (newPV != null) {
+          if (newPV.size() != 1) {
+            log.warn("More than one possible action value. Action will not be performed");
+            break;
+          }
+
+          Iterator values = newPV.iterator();
+          String value = (String)values.next();
+          if (value.equals(ThreatConAction.RMI)) {
+            try {
+              action.start(value);
+              blackboard.publishChange(action);
+                        if (log.isDebugEnabled()) 
+                            log.debug(action + " started.");
+              // TODO do the switching here
+              action.stop();
+              blackboard.publishChange(action);
+                        if (log.isDebugEnabled()) 
+                            log.debug(action + " stopped.");
+            } catch (IllegalValueException e) {
+              log.error("Illegal actionValue = "+value,e);
+              break;
+            } catch (NoStartedActionException nsae) {} // not going to happen
+            
+          }
+          else if (value.equals(ThreatConAction.RMISSL)) {
+          }
+          else if (value.equals(ThreatConAction.NORMI)) {
+          }
+        }
+      }
+    }
+  }
+}
+
