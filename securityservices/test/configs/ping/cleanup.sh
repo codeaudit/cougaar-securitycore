e83111c238d72132524f87210ee010e98f31fedd
srosset <srosset>
2004-09-14 18:37:16 +0000

    Added script to cleanup workspace directory.

---
+++ securityservices/test/configs/ping/cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm -rf $CIP/workspace/log4jlogs/*
+rm -rf $CIP/workspace/P
+rm -rf $CIP/workspace/BootPolicies*
+rm -rf $CIP/workspace/auditlogs/*
+rm -rf $CIP/workspace/jarfiles/*
+rm -rf $CIP/workspace/nodelogs/*
+rm -rf $CIP/workspace/security
+rm -rf $CIP/workspace/test
+rm -rf $CIP/log/bootstrap/*
