db6919b9175b21c6bb24903567a918559783f843
tredmond <tredmond>
2004-09-27 02:27:26 +0000

    Outline of the new policy scripts which invoke the ur policy actions.

---
+++ securityTests/security/scripts/ur_initialize_policy.rb
@@ -0,0 +1,11 @@
+
+
+require 'lib/security/actions/policyGeneration.rb'
+
+insert_before :setup_run do
+  do_action "BuildURPolicies"
+end
+
+insert_after :wait_for_initialization do
+  do_action "InstallURPolicies"
+end
---
+++ securityTests/security/scripts/ur_migrate_policy.rb
@@ -0,0 +1,7 @@
+
+
+require 'lib/security/actions/policyGeneration.rb'
+
+insert_before parameters[:migrate_location] do
+  do_action "MigratePolicies", parameters[:node_name],parameters[:target_network]
+end
