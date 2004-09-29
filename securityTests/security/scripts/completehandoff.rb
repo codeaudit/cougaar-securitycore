c487b55419c7f972a0b8c2acf456427d6474da00
srosset <srosset>
2004-09-29 23:50:06 +0000

    Changed from insert_before to insert_after to make it consistent with the other ISAT scripts.

--- securityTests/security/scripts/completehandoff.rb
+++ securityTests/security/scripts/completehandoff.rb
@@ -1,6 +1,6 @@
 require 'security/lib/scripting'
 require 'security/actions/completesecurityhandoff'
 
-insert_before parameters[:start_label] do
+insert_after parameters[:start_label] do
   do_action "CompleteSecurityHandOff",parameters[:nodename],parameters[:enclave]
 end
--- securityTests/security/scripts/starthandoff.rb
+++ securityTests/security/scripts/starthandoff.rb
@@ -2,9 +2,9 @@ require 'security/lib/scripting'
 require 'security/actions/startsecurityhandoff'
 require 'security/actions/policyGeneration'
 
-insert_before parameters[:start_label] do
+insert_after parameters[:start_label] do
 
-  do_action "MigratePolicies",parameters[:nodename],parameters[:enclave]
+  #do_action "MigratePolicies",parameters[:nodename],parameters[:enclave]
 
   do_action "StartSecurityHandOff",parameters[:nodename]
 
