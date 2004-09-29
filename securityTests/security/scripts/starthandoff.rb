daf0123567bacf5a8df5da63bea26156b1257484
rtripath <rtripath>
2004-09-29 21:18:18 +0000

    * Added a do action to MigratePolicies

--- securityTests/security/scripts/starthandoff.rb
+++ securityTests/security/scripts/starthandoff.rb
@@ -1,6 +1,11 @@
 require 'security/lib/scripting'
 require 'security/actions/startsecurityhandoff'
+require 'security/actions/policyGeneration'
 
 insert_before parameters[:start_label] do
+
+  do_action "MigratePolicies",parameters[:nodename],parameters[:enclave]
+
   do_action "StartSecurityHandOff",parameters[:nodename]
+
 end
