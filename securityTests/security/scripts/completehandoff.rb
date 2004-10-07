163b9a49e8e469305386700b1381c3fed5f9e63c
tredmond <tredmond>
2004-10-07 03:50:47 +0000

    Added the MoveNodeGuard action to the complete handoff.

--- securityTests/security/scripts/completehandoff.rb
+++ securityTests/security/scripts/completehandoff.rb
@@ -1,6 +1,9 @@
 require 'security/lib/scripting'
 require 'security/actions/completesecurityhandoff'
+require 'security/actions/policyGeneration'
 
 insert_after parameters[:start_label] do
+  do_action "MoveNodeGuard", parameters[:nodename], parameters[:enclave]
+
   do_action "CompleteSecurityHandOff",parameters[:nodename],parameters[:enclave]
 end
