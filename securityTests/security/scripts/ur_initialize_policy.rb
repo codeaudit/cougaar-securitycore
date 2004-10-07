68fb18c8ea2b4b6d80db5aceb33f287bfd2ed193
tredmond <tredmond>
2004-10-07 03:53:20 +0000

    Added parameters for the insertion point and for the database.

--- securityTests/security/scripts/ur_initialize_policy.rb
+++ securityTests/security/scripts/ur_initialize_policy.rb
@@ -3,9 +3,13 @@
 require 'security/actions/policyGeneration.rb'
 
 insert_before :setup_run do
-  do_action "BuildURPolicies"
+  do_action "BuildURPolicies",
+               parameters[:dbUser], 
+               parameters[:dbHost],
+               parameters[:dbPassword],
+               parameters[:db]
 end
 
-insert_after :wait_for_initialization do
-  do_action "InstallURPolicies"
+insert_after parameters[:insertionPoint] do
+  do_action "InstallURPolicies", parameters[:wait]
 end
