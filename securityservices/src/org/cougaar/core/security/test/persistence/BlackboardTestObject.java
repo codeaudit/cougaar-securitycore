dc715ff5354d5cf2c9fa8d3a34945eae4bb0831b
rliao1 <rliao1>
2004-10-08 23:11:13 +0000

    put array list in here

---
+++ securityservices/src/org/cougaar/core/security/test/persistence/BlackboardTestObject.java
@@ -0,0 +1,7 @@
+package org.cougaar.core.security.test.persistence;
+
+import java.util.ArrayList;
+import java.io.Serializable;
+
+public class BlackboardTestObject extends ArrayList implements Serializable {
+}
