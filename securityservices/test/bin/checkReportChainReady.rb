1bff11c1e7a9fc783e971892f014031a5c41a1b3
tredmond <tredmond>
2004-10-12 23:43:43 +0000

    Added some explanation of errors.

--- securityservices/test/bin/checkReportChainReady.rb
+++ securityservices/test/bin/checkReportChainReady.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/ruby
 
-CIP=ENV['CIP']
+if ! defined? CIP then
+  CIP=ENV['CIP']
+end
 
 $:.unshift File.join(CIP, 'csmart', 'lib')
 
@@ -10,7 +12,22 @@ require "security/lib/scripting"
 require "security/lib/security"
 require "security/lib/stresses/reportChainReady"
 
-require File.join(CIP, "workspace", "test", "subordinates.rb")
+subordinatesFile = File.join(CIP, "workspace", "test", "subordinates.rb")
+subordinatesFound=false
+begin
+  if File.stat(subordinatesFile).readable?() then
+# strange inconsistency - require doesn't throw exceptions?
+    require subordinatesFile
+    subordinatesFound = true
+  end
+rescue
+  puts("#{$!} #{$!.backtrace.join("\n")}")
+  puts("------------------------")
+  puts("Error reading subordinates.rb")
+  puts("The check_report_chain_ready.rb" +
+            "script probably wasn't loaded")
+  exit(-1)
+end
 
 x = TestReportChainReady.new(nil)
 installExpectedRelations(x)
