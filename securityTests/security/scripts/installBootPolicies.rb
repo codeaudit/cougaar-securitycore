63df6287332f683e05f0de39c917bcdbd0e8edf1
tredmond <tredmond>
2004-10-15 23:24:48 +0000

    Didn't work.

--- securityTests/security/scripts/installBootPolicies.rb
+++
@@ -1,6 +0,0 @@
-require 'security/lib/scripting'
-require 'security/actions/policyGeneration'
-
-insert_before parameters[:start_label] do
-  do_action "InstallBootPolicies", parameters[:wait_flag]
-end
