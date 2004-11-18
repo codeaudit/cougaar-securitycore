2fd192f65289941326d13f4e96b1c0809e949dcc
srosset <srosset>
2004-11-18 23:04:57 +0000

    Removed unused import

--- securityTests/regress/test/org/cougaar/core/security/pingtest/PingLog4jParser.java
+++ securityTests/regress/test/org/cougaar/core/security/pingtest/PingLog4jParser.java
@@ -26,7 +26,6 @@ package org.cougaar.core.security.pingtest;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
