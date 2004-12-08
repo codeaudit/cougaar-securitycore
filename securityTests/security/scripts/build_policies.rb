ab7443785923de5e10630345d616fbeb9b9fe0a1
srosset <srosset>
2004-12-08 21:39:40 +0000

    Typos, which prevented the Java policy to be modified as expected.

--- securityTests/security/scripts/build_policies.rb
+++ securityTests/security/scripts/build_policies.rb
@@ -9,4 +9,12 @@ require 'security/lib/scripting'
 
 insert_before :setup_run do
   do_action "BuildPolicies"
+  do_action "GenericAction" do |run|
+    # First, extract the Java policy from the JAR file to make
+    # sure we are working from the original file.
+    `cd #{$CIP}/configs/security ; jar xvf configs_secure_bootstrapper.jar security/Cougaar_Java.policy`
+    # Rebuild Java policy to accept both
+    # old signers and new signers.
+    `java -classpath #{$CIP}/lib/secure_bootstrapper.jar org.cougaar.core.security.securebootstrap.PolicyParserTool #{$CIP}/configs/security/security/Cougaar_Java.policy #{$CIP}/configs/security/Cougaar_Java.policy`
+  end
 end
