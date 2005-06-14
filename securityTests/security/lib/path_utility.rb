903d6821ba857dbe57800405b6d587326a386bb3
srosset <srosset>
2005-06-14 00:06:03 +0000

    On windows, convert relative paths to absolute paths. People have reported problems when using relative paths.

--- securityTests/security/lib/path_utility.rb
+++ securityTests/security/lib/path_utility.rb
@@ -21,7 +21,7 @@ class PathUtility
   def PathUtility.fixPath(arg)
     PathUtility.init
     if @@isWindows
-      arg = `cygpath -m #{arg}`.strip
+      arg = `cygpath -m -a #{arg}`.strip
       #puts "Converted: #{arg}"
     end
     arg
